9ba7c98f5ea9aa31919f13722e64b8dadabc1190
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-09-20 15:48:13 +0300

    Add BitMonoContext

---
+++ BitMono/BitMono.Core/BitMonoContext.cs
@@ -0,0 +1,25 @@
+﻿namespace BitMono.Core
+{
+    public class BitMonoContext
+    {
+        public BitMonoContext(string moduleFile, string baseDirectory, string outputDirectory, bool fileWatermark = true)
+        {
+            ModuleFile = moduleFile;
+            BaseDirectory = baseDirectory;
+            OutputDirectory = outputDirectory;
+            FileWatermark = fileWatermark;
+        }
+        public BitMonoContext(string baseDirectory, string outputDirectory, bool fileWatermark = true) 
+            : this(null, baseDirectory, outputDirectory, fileWatermark)
+        {
+        }
+        public BitMonoContext()
+        {
+        }
+
+        public string ModuleFile { get; set; }
+        public string BaseDirectory { get; set; }
+        public string OutputDirectory { get; set; }
+        public bool FileWatermark { get; set; }
+    }
+}
