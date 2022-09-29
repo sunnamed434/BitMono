fbd3d169715cd3901c1ad14c794fc103a8d817d6
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-09-29 22:40:17 +0300

    Add new critical analyzers

---
+++ BitMono/BitMono.Core/Protecting/Analyzing/TypeDefModelCriticalAnalyzer.cs
@@ -0,0 +1,27 @@
+﻿using Autofac.Util;
+using BitMono.API.Protecting;
+using BitMono.API.Protecting.Analyzing;
+using dnlib.DotNet;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace BitMono.Core.Protecting.Analyzing
+{
+    public class TypeDefModelCriticalAnalyzer : ICriticalAnalyzer<TypeDef>
+    {
+        public bool NotCriticalToMakeChanges(ProtectionContext context, TypeDef typeDef)
+        {
+            var assemblyTypes = context.TargetAssembly.GetLoadableTypes();
+            var type = assemblyTypes.FirstOrDefault(t => t.Name.Equals(typeDef.Name));
+            if (type != null)
+            {
+                if (type.GetCustomAttribute<SerializableAttribute>(false) != null)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
