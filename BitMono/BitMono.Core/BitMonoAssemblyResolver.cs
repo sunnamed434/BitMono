6ef5c03a88bc9290c88448ce4151b154128f0cf1
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-10-02 23:08:08 +0300

    Add BitMonoAssemblyResolver

---
+++ BitMono/BitMono.Core/BitMonoAssemblyResolver.cs
@@ -0,0 +1,52 @@
+﻿using BitMono.API.Protecting;
+using dnlib.DotNet;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using ILogger = BitMono.Core.Logging.ILogger;
+
+namespace BitMono.Core
+{
+    public class BitMonoAssemblyResolver : IAsyncDisposable
+    {
+        private readonly ProtectionContext m_ProtectionContext;
+        private readonly ILogger m_Logger;
+
+        public BitMonoAssemblyResolver(ProtectionContext protectionContext, ILogger logger)
+        {
+            m_ProtectionContext = protectionContext;
+            m_Logger = logger;
+        }
+
+        public bool ResolvingFailed { get; private set; }
+
+
+        public Task ResolveAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (AssemblyRef assemblyRef in m_ProtectionContext.ModuleDefMD.GetAssemblyRefs())
+            {
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    return Task.CompletedTask;
+                }
+
+                try
+                {
+                    m_ProtectionContext.ModuleCreationOptions.Context.AssemblyResolver.ResolveThrow(assemblyRef, m_ProtectionContext.ModuleDefMD);
+                }
+                catch (AssemblyResolveException)
+                {
+                    ResolvingFailed = true;
+                    m_Logger.Warn("Failed to resolve dependency of " + assemblyRef.FullName);
+                }
+            }
+            return Task.CompletedTask;
+        }
+
+        public ValueTask DisposeAsync()
+        {
+            ResolvingFailed = false;
+            return new ValueTask();
+        }
+    }
+}
