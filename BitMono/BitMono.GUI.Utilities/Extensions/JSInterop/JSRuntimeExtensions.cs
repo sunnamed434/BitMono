4b6febd16544c397d94763f82208d4668cecefe3
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-10-15 00:19:10 +0300

    New JS extension methods for JSRuntimeExtensions

--- BitMono/BitMono.GUI.Utilities/Extensions/JSInterop/JSRuntimeExtensions.cs
+++ BitMono/BitMono.GUI.Utilities/Extensions/JSInterop/JSRuntimeExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.JSInterop;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
 namespace BitMono.GUI.Utilities.Extensions.JSInterop
 {
@@ -15,6 +16,18 @@ namespace BitMono.GUI.Utilities.Extensions.JSInterop
         public static async Task HideModalAsync(this IJSRuntime source, string modalId)
         {
             await source.InvokeVoidAsync("HideModal", modalId);
+        }
+        public static async Task ScrollToEndAsync(this IJSRuntime source, ElementReference textAreaReference)
+        {
+            await source.InvokeVoidAsync("scrollToEnd", textAreaReference);
+        }
+        public static async Task CopyTextToClipboardAsync(this IJSRuntime source, string content)
+        {
+            await source.InvokeVoidAsync("navigator.clipboard.writeText", content);
+        }
+        public static async Task AddTooltipsAsync(this IJSRuntime source)
+        {
+            await source.InvokeVoidAsync("addTooltips");
         }
     }
 }
