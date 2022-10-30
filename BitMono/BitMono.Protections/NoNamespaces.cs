df58953778d965d8b3788d68313455cad92b4901
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-10-30 19:24:50 +0200

    Update NoNamespaces.cs

--- BitMono/BitMono.Protections/NoNamespaces.cs
+++ BitMono/BitMono.Protections/NoNamespaces.cs
@@ -1,5 +1,7 @@
 ﻿using BitMono.API.Protecting;
-using BitMono.Core.Protecting.Analyzing;
+using BitMono.API.Protecting.Contexts;
+using BitMono.Core.Protecting.Analyzing.DnlibDefs;
+using BitMono.Core.Protecting.Analyzing.Naming;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -8,12 +10,12 @@ namespace BitMono.Protections
 {
     public class NoNamespaces : IProtection
     {
-        private readonly TypeDefCriticalAnalyzer m_TypeDefCriticalAnalyzer;
+        private readonly DnlibDefCriticalAnalyzer m_DnlibDefCriticalAnalyzer;
         private readonly NameCriticalAnalyzer m_NameCriticalAnalyzer;
 
-        public NoNamespaces(TypeDefCriticalAnalyzer typeDefCriticalAnalyzer, NameCriticalAnalyzer nameCriticalAnalyzer)
+        public NoNamespaces(DnlibDefCriticalAnalyzer typeDefCriticalAnalyzer, NameCriticalAnalyzer nameCriticalAnalyzer)
         {
-            m_TypeDefCriticalAnalyzer = typeDefCriticalAnalyzer;
+            m_DnlibDefCriticalAnalyzer = typeDefCriticalAnalyzer;
             m_NameCriticalAnalyzer = nameCriticalAnalyzer;
         }
 
@@ -23,7 +25,7 @@ namespace BitMono.Protections
             foreach (var typeDef in context.ModuleDefMD.GetTypes().ToArray())
             {
                 if (typeDef.IsGlobalModuleType == false
-                    && m_TypeDefCriticalAnalyzer.NotCriticalToMakeChanges(context, typeDef)
+                    && m_DnlibDefCriticalAnalyzer.NotCriticalToMakeChanges(context, typeDef)
                     && typeDef.Namespace?.Data != null)
                 {
                     if (m_NameCriticalAnalyzer.NotCriticalToMakeChanges(context, typeDef.Namespace))
