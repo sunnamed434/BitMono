ddc98fc0375f737a54f71b34b1928bf2a22fa32f
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-10-30 19:19:49 +0200

    Create BitMonoContext.cs

---
+++ BitMono/BitMono.API/Protecting/Context/BitMonoContext.cs
@@ -0,0 +1,11 @@
+﻿namespace BitMono.API.Protecting.Contexts
+{
+    public class BitMonoContext
+    {
+        public string ModuleFile { get; set; }
+        public string BaseDirectory { get; set; }
+        public string OutputDirectory { get; set; }
+        public string OutputModuleFile { get; set; }
+        public bool Watermark { get; set; }
+    }
+}
