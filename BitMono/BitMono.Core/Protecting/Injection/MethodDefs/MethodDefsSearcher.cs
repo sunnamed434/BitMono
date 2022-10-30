afa02d3fc651ec426655d7542b428df0afba1c25
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-10-30 19:22:00 +0200

    Create MethodDefsSearcher.cs

---
+++ BitMono/BitMono.Core/Protecting/Injection/MethodDefs/MethodDefsSearcher.cs
@@ -0,0 +1,43 @@
+﻿using BitMono.API.Protecting.Injection.MethodDefs;
+using dnlib.DotNet;
+using NullGuard;
+
+namespace BitMono.Core.Protecting.Injection.MethodDefs
+{
+    public class MethodDefsSearcher : IMethodDefSearcher
+    {
+        [return: AllowNull]
+        public MethodDef Find(string name, ModuleDefMD moduleDefMD)
+        {
+            foreach (var typeDef in moduleDefMD.Types)
+            {
+                foreach (var methodDef in typeDef.Methods)
+                {
+                    if (methodDef.Name.Equals(name))
+                    {
+                        return methodDef;
+                    }
+                }
+            }
+            return null;
+        }
+        [return: AllowNull]
+        public MethodDef FindInGlobalNestedMethods(string name, ModuleDefMD moduleDefMD)
+        {
+            foreach (var typeDef in moduleDefMD.GlobalType.NestedTypes)
+            {
+                if (typeDef.HasMethods)
+                {
+                    foreach (var methodDef in typeDef.Methods)
+                    {
+                        if (methodDef.Name.Equals(name))
+                        {
+                            return methodDef;
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
