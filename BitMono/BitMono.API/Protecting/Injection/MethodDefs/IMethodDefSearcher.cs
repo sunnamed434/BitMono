b816d9f3f73ee5965a4ccc9c12e075271286497a
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-10-30 19:20:00 +0200

    Create IMethodDefSearcher.cs

---
+++ BitMono/BitMono.API/Protecting/Injection/MethodDefs/IMethodDefSearcher.cs
@@ -0,0 +1,10 @@
+﻿using dnlib.DotNet;
+
+namespace BitMono.API.Protecting.Injection.MethodDefs
+{
+    public interface IMethodDefSearcher
+    {
+        MethodDef Find(string name, ModuleDefMD moduleDefMD);
+        MethodDef FindInGlobalNestedMethods(string name, ModuleDefMD moduleDefMD);
+    }
+}
