2ee5ca63dba94b8ec10bea5a150a6cd3e93a2b51
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-10-30 19:24:55 +0200

    Update StringsEncryption.cs

--- BitMono/BitMono.Protections/StringsEncryption.cs
+++ BitMono/BitMono.Protections/StringsEncryption.cs
@@ -1,11 +1,12 @@
 ﻿using BitMono.API.Protecting;
+using BitMono.API.Protecting.Contexts;
 using BitMono.API.Protecting.Injection;
-using BitMono.API.Protecting.Injection.Fields;
-using BitMono.API.Protecting.Injection.Methods;
+using BitMono.API.Protecting.Injection.FieldDefs;
+using BitMono.API.Protecting.Injection.MethodDefs;
 using BitMono.API.Protecting.Renaming;
-using BitMono.Core.Protecting.Analyzing;
+using BitMono.Core.Protecting.Analyzing.DnlibDefs;
 using BitMono.Encryption;
-using BitMono.Utilities.Extensions.Dnlib;
+using BitMono.Utilities.Extensions.dnlib;
 using dnlib.DotNet;
 using dnlib.DotNet.Emit;
 using System.Linq;
@@ -17,25 +18,24 @@ namespace BitMono.Protections
     public class StringsEncryption : IProtection
     {
         private readonly IInjector m_Injector;
-        private readonly IRenamer m_Renamer;
         private readonly IFieldSearcher m_FieldSearcher;
-        private readonly IMethodSearcher m_MethodSearcher;
-        private readonly MethodDefCriticalAnalyzer m_MethodDefCriticalAnalyzer;
+        private readonly IMethodDefSearcher m_MethodSearcher;
+        private readonly DnlibDefCriticalAnalyzer m_DnlibDefCriticalAnalyzer;
+        private readonly IRenamer m_Renamer;
 
         public StringsEncryption(
             IInjector injector,
-            IRenamer renamer,
             IFieldSearcher fieldSearcher,
-            IMethodSearcher methodSearcher,
-            MethodDefCriticalAnalyzer methodDefCriticalAnalyzer)
+            IMethodDefSearcher methodSearcher,
+            DnlibDefCriticalAnalyzer dnlibDefCriticalAnalyzer,
+            IRenamer renamer)
         {
             m_Injector = injector;
-            m_Renamer = renamer;
             m_FieldSearcher = fieldSearcher;
             m_MethodSearcher = methodSearcher;
-            m_MethodDefCriticalAnalyzer = methodDefCriticalAnalyzer;
+            m_DnlibDefCriticalAnalyzer = dnlibDefCriticalAnalyzer;
+            m_Renamer = renamer;
         }
-
 
         public Task ExecuteAsync(ProtectionContext context, CancellationToken cancellationToken = default)
         {
@@ -46,7 +46,7 @@ namespace BitMono.Protections
             var cryptKeyBytes = new byte[] { 0x1, 0x3, 0x10, 0x15, 0x20, 0x50, 0x5, 0x10, 0x10 };
             var saltBytesFieldDef = m_Injector.InjectArrayInGlobalNestedTypes(context.ModuleDefMD, saltBytes, "saltBytes");
             var cryptKeyBytesFieldDef = m_Injector.InjectArrayInGlobalNestedTypes(context.ModuleDefMD, cryptKeyBytes, "cryptKeyBytes");
-            
+
             var decryptMethodDefFromEncryptionModule = m_MethodSearcher.Find("Decrypt", context.EncryptionModuleDefMD);
             var decryptorMethodDef = new MethodDefUser("Decrypt", decryptMethodDefFromEncryptionModule.MethodSig, MethodAttributes.Static | MethodAttributes.Assembly);
             decryptorMethodDef.Body = decryptMethodDefFromEncryptionModule.Body;
@@ -89,7 +89,7 @@ namespace BitMono.Protections
                 {
                     foreach (var methodDef in typeDef.Methods.ToArray())
                     {
-                        if (methodDef.HasBody && m_MethodDefCriticalAnalyzer.NotCriticalToMakeChanges(context, methodDef))
+                        if (methodDef.HasBody && m_DnlibDefCriticalAnalyzer.NotCriticalToMakeChanges(context, methodDef))
                         {
                             for (int i = 0; i < methodDef.Body.Instructions.Count(); i++)
                             {
