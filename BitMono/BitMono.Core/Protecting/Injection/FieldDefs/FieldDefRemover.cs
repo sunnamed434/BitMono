1e9226536856729cbcc78bf1cc1e0952eea4007d
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-10-30 19:21:49 +0200

    Create FieldDefRemover.cs

---
+++ BitMono/BitMono.Core/Protecting/Injection/FieldDefs/FieldDefRemover.cs
@@ -0,0 +1,24 @@
+﻿using BitMono.API.Protecting.Injection.FieldDefs;
+using dnlib.DotNet;
+
+namespace BitMono.Core.Protecting.Injection.FieldDefs
+{
+    public class FieldDefRemover : IFieldRemover
+    {
+        public bool Remove(string name, ModuleDefMD module)
+        {
+            foreach (TypeDef typeDef in module.Types)
+            {
+                foreach (var fieldDef in typeDef.Fields)
+                {
+                    if (fieldDef.Name.Equals(name))
+                    {
+                        typeDef.Fields.Remove(fieldDef);
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
