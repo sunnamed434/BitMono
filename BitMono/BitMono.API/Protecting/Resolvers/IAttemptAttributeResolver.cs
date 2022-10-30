8ec5f8b8182a30dd8582524761a9213e14808375
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-10-30 19:20:39 +0200

    Create IAttemptAttributeResolver.cs

---
+++ BitMono/BitMono.API/Protecting/Resolvers/IAttemptAttributeResolver.cs
@@ -0,0 +1,12 @@
+﻿using BitMono.API.Protecting.Contexts;
+using dnlib.DotNet;
+using System;
+
+namespace BitMono.API.Protecting.Resolvers
+{
+    public interface IAttemptAttributeResolver
+    {
+        bool TryResolve<TAttribute>(ProtectionContext context, IDnlibDef dnlibDef, Predicate<TAttribute> review, Func<TAttribute, bool> predicate, out TAttribute attribute, bool inherit = false)
+            where TAttribute : Attribute;
+    }
+}
