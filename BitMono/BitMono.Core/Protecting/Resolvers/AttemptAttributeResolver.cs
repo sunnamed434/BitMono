cb3bfef7f54bf94b97445e5f3869accf8ccce021
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-10-30 19:22:40 +0200

    Create AttemptAttributeResolver.cs

---
+++ BitMono/BitMono.Core/Protecting/Resolvers/AttemptAttributeResolver.cs
@@ -0,0 +1,33 @@
+﻿using BitMono.API.Protecting.Contexts;
+using BitMono.API.Protecting.Resolvers;
+using dnlib.DotNet;
+using NullGuard;
+using System;
+
+namespace BitMono.Core.Protecting.Resolvers
+{
+    public class AttemptAttributeResolver : IAttemptAttributeResolver
+    {
+        private readonly IDnlibDefAttributeResolver m_DnlibDefAttributeResolver;
+
+        public AttemptAttributeResolver(IDnlibDefAttributeResolver dnlibDefAttributeResolver)
+        {
+            m_DnlibDefAttributeResolver = dnlibDefAttributeResolver;
+        }
+
+        public bool TryResolve<TAttribute>(ProtectionContext context, IDnlibDef dnlibDef, [AllowNull] Predicate<TAttribute> review, [AllowNull] Func<TAttribute, bool> predicate, [AllowNull] out TAttribute attribute, bool inherit = false)
+            where TAttribute : Attribute
+        {
+            attribute = m_DnlibDefAttributeResolver.ResolveOrDefault(context, dnlibDef, predicate, inherit);
+            if (attribute == null)
+            {
+                return false;
+            }
+            if (review != null)
+            {
+                return review.Invoke(attribute);
+            }
+            return true;
+        }
+    }
+}
