d546accca34631dd6b5a9984751cead9bb9d17eb
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-11-05 23:58:21 +0200

    Update IAttemptAttributeResolver.cs

--- BitMono/BitMono.API/Protecting/Resolvers/IAttemptAttributeResolver.cs
+++ BitMono/BitMono.API/Protecting/Resolvers/IAttemptAttributeResolver.cs
@@ -5,7 +5,7 @@ namespace BitMono.API.Protecting.Resolvers
 {
     public interface IAttemptAttributeResolver
     {
-        bool TryResolve<TAttribute>(IHasCustomAttribute from, Predicate<TAttribute> review, Func<TAttribute, bool> predicate, out TAttribute attribute)
+        bool TryResolve<TAttribute>(IHasCustomAttribute from, Predicate<TAttribute> review, Func<TAttribute, bool> predicate, Func<TAttribute, bool> strip, out TAttribute attribute)
             where TAttribute : Attribute;
     }
 }
