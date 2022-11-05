74f0e2072f34a0c371ebad2df51ebfba3e10ec1f
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-11-05 21:34:21 +0200

    Update StringsEncryption.cs

--- BitMono/BitMono.Protections/StringsEncryption.cs
+++ BitMono/BitMono.Protections/StringsEncryption.cs
@@ -1,40 +1,50 @@
 ﻿using BitMono.API.Protecting;
-using BitMono.API.Protecting.Contexts;
+using BitMono.API.Protecting.Context;
 using BitMono.API.Protecting.Injection;
 using BitMono.API.Protecting.Injection.FieldDefs;
 using BitMono.API.Protecting.Injection.MethodDefs;
 using BitMono.API.Protecting.Renaming;
+using BitMono.API.Protecting.Resolvers;
 using BitMono.Core.Protecting.Analyzing.DnlibDefs;
 using BitMono.Encryption;
 using BitMono.Utilities.Extensions.dnlib;
 using dnlib.DotNet;
 using dnlib.DotNet.Emit;
 using System.Linq;
+using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
+using ILogger = Serilog.ILogger;
+using MethodAttributes = dnlib.DotNet.MethodAttributes;
 
 namespace BitMono.Protections
 {
     public class StringsEncryption : IProtection
     {
         private readonly IInjector m_Injector;
-        private readonly IFieldSearcher m_FieldSearcher;
         private readonly IMethodDefSearcher m_MethodSearcher;
+        private readonly IFieldSearcher m_FieldSearcher;
+        private readonly IObfuscationAttributeExcludingResolver m_ObfuscationAttributeExcludingResolver;
         private readonly DnlibDefCriticalAnalyzer m_DnlibDefCriticalAnalyzer;
         private readonly IRenamer m_Renamer;
+        private readonly ILogger m_Logger;
 
         public StringsEncryption(
             IInjector injector,
             IFieldSearcher fieldSearcher,
+            IObfuscationAttributeExcludingResolver obfuscationAttributeExcludingResolver,
             IMethodDefSearcher methodSearcher,
             DnlibDefCriticalAnalyzer dnlibDefCriticalAnalyzer,
-            IRenamer renamer)
+            IRenamer renamer,
+            ILogger logger)
         {
             m_Injector = injector;
-            m_FieldSearcher = fieldSearcher;
             m_MethodSearcher = methodSearcher;
+            m_FieldSearcher = fieldSearcher;
+            m_ObfuscationAttributeExcludingResolver = obfuscationAttributeExcludingResolver;
             m_DnlibDefCriticalAnalyzer = dnlibDefCriticalAnalyzer;
             m_Renamer = renamer;
+            m_Logger = logger.ForContext<StringsEncryption>();
         }
 
         public Task ExecuteAsync(ProtectionContext context, CancellationToken cancellationToken = default)
@@ -85,18 +95,38 @@ namespace BitMono.Protections
 
             foreach (var typeDef in context.ModuleDefMD.GetTypes().ToArray())
             {
+                if (m_ObfuscationAttributeExcludingResolver.TryResolve(typeDef, feature: nameof(StringsEncryption),
+                    out ObfuscationAttribute typeDefObfuscationAttribute))
+                {
+                    if (typeDefObfuscationAttribute.Exclude)
+                    {
+                        m_Logger.Information("Found {0}, skipping.", nameof(ObfuscationAttribute));
+                        continue;
+                    }
+                }
+
                 if (typeDef.HasMethods)
                 {
                     foreach (var methodDef in typeDef.Methods.ToArray())
                     {
+                        if (m_ObfuscationAttributeExcludingResolver.TryResolve(methodDef, feature: nameof(StringsEncryption),
+                            out ObfuscationAttribute methodDefObfuscationAttribute))
+                        {
+                            if (methodDefObfuscationAttribute.Exclude)
+                            {
+                                m_Logger.Information("Found {0}, skipping.", nameof(ObfuscationAttribute));
+                                continue;
+                            }
+                        }
+
                         if (methodDef.HasBody && m_DnlibDefCriticalAnalyzer.NotCriticalToMakeChanges(context, methodDef))
                         {
                             for (int i = 0; i < methodDef.Body.Instructions.Count(); i++)
                             {
                                 if (methodDef.Body.Instructions[i].OpCode == OpCodes.Ldstr
-                                    && methodDef.Body.Instructions[i].Operand is string stringContent)
+                                    && methodDef.Body.Instructions[i].Operand is string content)
                                 {
-                                    var encryptedContentBytes = Encryptor.EncryptContent(stringContent, saltBytes, cryptKeyBytes);
+                                    var encryptedContentBytes = Encryptor.EncryptContent(content, saltBytes, cryptKeyBytes);
                                     var injectedEncryptedArrayBytes = m_Injector.InjectArrayInGlobalNestedTypes(context.ModuleDefMD, encryptedContentBytes, m_Renamer.RenameUnsafely());
 
                                     methodDef.Body.Instructions[i].OpCode = OpCodes.Nop;
