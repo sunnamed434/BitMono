a0ee29fe9ec6ce50f8591257d5f912bc42aee730
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-11-05 21:34:26 +0200

    Update NoNamespaces.cs

--- BitMono/BitMono.Protections/NoNamespaces.cs
+++ BitMono/BitMono.Protections/NoNamespaces.cs
@@ -1,32 +1,53 @@
 ﻿using BitMono.API.Protecting;
-using BitMono.API.Protecting.Contexts;
+using BitMono.API.Protecting.Context;
+using BitMono.API.Protecting.Resolvers;
 using BitMono.Core.Protecting.Analyzing.DnlibDefs;
 using BitMono.Core.Protecting.Analyzing.Naming;
+using dnlib.DotNet;
 using System.Linq;
+using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
+using ILogger = Serilog.ILogger;
 
 namespace BitMono.Protections
 {
     public class NoNamespaces : IProtection
     {
+        private readonly IObfuscationAttributeExcludingResolver m_ObfuscationAttributeExcludingResolver;
         private readonly DnlibDefCriticalAnalyzer m_DnlibDefCriticalAnalyzer;
         private readonly NameCriticalAnalyzer m_NameCriticalAnalyzer;
+        private readonly ILogger m_Logger;
 
-        public NoNamespaces(DnlibDefCriticalAnalyzer typeDefCriticalAnalyzer, NameCriticalAnalyzer nameCriticalAnalyzer)
+        public NoNamespaces(
+            IObfuscationAttributeExcludingResolver obfuscationAttributeExcludingResolver, 
+            DnlibDefCriticalAnalyzer typeDefCriticalAnalyzer, 
+            NameCriticalAnalyzer nameCriticalAnalyzer,
+            ILogger logger)
         {
+            m_ObfuscationAttributeExcludingResolver = obfuscationAttributeExcludingResolver;
             m_DnlibDefCriticalAnalyzer = typeDefCriticalAnalyzer;
             m_NameCriticalAnalyzer = nameCriticalAnalyzer;
+            m_Logger = logger.ForContext<NoNamespaces>();
         }
-
 
         public Task ExecuteAsync(ProtectionContext context, CancellationToken cancellationToken = default)
         {
             foreach (var typeDef in context.ModuleDefMD.GetTypes().ToArray())
             {
+                if (m_ObfuscationAttributeExcludingResolver.TryResolve(typeDef, feature: nameof(NoNamespaces),
+                    out ObfuscationAttribute typeDefObfuscationAttribute))
+                {
+                    if (typeDefObfuscationAttribute.Exclude)
+                    {
+                        m_Logger.Information("Found {0}, skipping.", nameof(ObfuscationAttribute));
+                        continue;
+                    }
+                }
+
                 if (typeDef.IsGlobalModuleType == false
                     && m_DnlibDefCriticalAnalyzer.NotCriticalToMakeChanges(context, typeDef)
-                    && typeDef.Namespace?.Data != null)
+                    && UTF8String.IsNullOrEmpty(typeDef.Namespace) == false)
                 {
                     if (m_NameCriticalAnalyzer.NotCriticalToMakeChanges(context, typeDef.Namespace))
                     {
