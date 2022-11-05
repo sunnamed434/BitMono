d315a46fb72a72903d97e9340f221a520222ab1c
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-11-05 21:31:31 +0200

    Create ICustomAttributesResolver.cs

---
+++ BitMono/BitMono.API/Protecting/Resolvers/ICustomAttributesResolver.cs
@@ -0,0 +1,11 @@
+﻿using dnlib.DotNet;
+using System;
+using System.Collections.Generic;
+
+namespace BitMono.API.Protecting.Resolvers
+{
+    public interface ICustomAttributesResolver
+    {
+        IEnumerable<TAttribute> Resolve<TAttribute>(IHasCustomAttribute from) where TAttribute : Attribute;
+    }
+}
