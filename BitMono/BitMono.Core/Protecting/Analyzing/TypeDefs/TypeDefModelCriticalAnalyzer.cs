98ce3fcc9c109901311b9cb0c64053da3701ba58
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-11-06 13:31:59 +0200

    Update TypeDefModelCriticalAnalyzer.cs

--- BitMono/BitMono.Core/Protecting/Analyzing/TypeDefs/TypeDefModelCriticalAnalyzer.cs
+++ BitMono/BitMono.Core/Protecting/Analyzing/TypeDefs/TypeDefModelCriticalAnalyzer.cs
@@ -1,58 +1,41 @@
-﻿using Autofac.Util;
-using BitMono.API.Protecting.Analyzing;
+﻿using BitMono.API.Protecting.Analyzing;
 using BitMono.API.Protecting.Context;
+using BitMono.API.Protecting.Resolvers;
 using dnlib.DotNet;
 using Newtonsoft.Json;
 using System;
-using System.Linq;
-using System.Reflection;
 using System.Xml.Serialization;
 
 namespace BitMono.Core.Protecting.Analyzing.TypeDefs
 {
-    public class TypeDefModelCriticalAnalyzer : ICriticalAnalyzer<TypeDef>
+    public class TypeDefModelCriticalAnalyzer : ICriticalAnalyzer<IHasCustomAttribute>
     {
-        private readonly Type[] m_CriticalTypeAttributes = new Type[]
-        {
-            typeof(SerializableAttribute),
-        };
-        private readonly Type[] m_CriticalFieldPropertyAttributes = new Type[]
-        {
-            typeof(XmlAttributeAttribute),
-            typeof(XmlArrayItemAttribute),
-            typeof(JsonPropertyAttribute),
-        };
+        private readonly IAttemptAttributeResolver m_AttemptAttributeResolver;
 
-        public bool NotCriticalToMakeChanges(ProtectionContext context, TypeDef typeDef)
+        public TypeDefModelCriticalAnalyzer(IAttemptAttributeResolver attemptAttributeResolver)
         {
-            var assemblyTypes = context.Assembly.GetLoadableTypes();
-            var type = assemblyTypes.FirstOrDefault(t => t.Name.Equals(typeDef.ReflectionName));
-            if (type != null)
+            m_AttemptAttributeResolver = attemptAttributeResolver;
+        }
+
+        public bool NotCriticalToMakeChanges(ProtectionContext context, IHasCustomAttribute from)
+        {
+            if (m_AttemptAttributeResolver.TryResolve<SerializableAttribute>(from, null, null, null, out _))
             {
-                if (m_CriticalTypeAttributes.Any(c => type.GetCustomAttribute(c, false) != null))
-                {
-                    return false;
-                }
-
-                if (typeDef.HasFields)
-                {
-                    var field = type.GetFields().FirstOrDefault(f => m_CriticalFieldPropertyAttributes.Any(c => f.GetCustomAttribute(c, false) != null));
-                    if (field != null)
-                    {
-                        return false;
-                    }
-                }
-
-                if (typeDef.HasProperties)
-                {
-                    var field = type.GetProperties().FirstOrDefault(f => m_CriticalFieldPropertyAttributes.Any(c => f.GetCustomAttribute(c, false) != null));
-                    if (field != null)
-                    {
-                        return false;
-                    }
-                }
+                return false;
             }
-            return true;
+            if (m_AttemptAttributeResolver.TryResolve<XmlAttributeAttribute>(from, null, null, null, out _))
+            {
+                return false;
+            }
+            if (m_AttemptAttributeResolver.TryResolve<XmlArrayItemAttribute>(from, null, null, null, out _))
+            {
+                return false;
+            }
+            if (m_AttemptAttributeResolver.TryResolve<JsonPropertyAttribute>(from, null, null, null, out _))
+            {
+                return false;
+            }
+            return false;
         }
     }
 }
