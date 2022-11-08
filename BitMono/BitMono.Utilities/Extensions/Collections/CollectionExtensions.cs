55a1fceb55b9105567ba8c46a063ad0803c2692e
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-11-08 16:25:49 +0200

    Create CollectionExtensions.cs

---
+++ BitMono/BitMono.Utilities/Extensions/Collections/CollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace BitMono.Utilities.Extensions.Collections
+{
+    public static class CollectionExtensions
+	{
+        public static IList<T> Swap<T>(this IList<T> source, int index1, int index2)
+        {
+            T temp = source[index1];
+            source[index1] = source[index2];
+            source[index2] = temp;
+            return source;
+        }
+    }
+}
