cf3784adb85a73185dd86739b610b0a1cbfb59fa
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-11-08 16:24:06 +0200

    Create BitMonoObfuscationConfiguration.cs

---
+++ BitMono/BitMono.Host/Configuration/BitMonoObfuscationConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using BitMono.API.Configuration;
+
+namespace BitMono.Host.Configuration
+{
+    public class BitMonoObfuscationConfiguration : JsonConfigurationAccessor, IBitMonoObfuscationConfiguration
+    {
+        public BitMonoObfuscationConfiguration() : base(file: "obfuscation.json")
+        {
+        }
+    }
+}
