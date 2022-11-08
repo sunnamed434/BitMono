263ec1e0907e804b0b2aedeec0ad4b716725c318
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-11-08 16:22:01 +0200

    Create IConfigurationAccessor.cs

---
+++ BitMono/BitMono.API/Configuration/IConfigurationAccessor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace BitMono.API.Configuration
+{
+    public interface IConfigurationAccessor
+    {
+        IConfiguration Configuration { get; }
+    }
+}
