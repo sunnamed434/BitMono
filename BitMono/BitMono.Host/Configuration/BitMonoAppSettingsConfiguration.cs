738bbee42b60605f4cff11656fb4738c8256e15e
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-11-08 16:24:02 +0200

    Create BitMonoAppSettingsConfiguration.cs

---
+++ BitMono/BitMono.Host/Configuration/BitMonoAppSettingsConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using BitMono.API.Configuration;
+
+namespace BitMono.Host.Configuration
+{
+    public class BitMonoAppSettingsConfiguration : JsonConfigurationAccessor, IBitMonoAppSettingsConfiguration
+    {
+        public BitMonoAppSettingsConfiguration() : base(file: "appsettings.json")
+        {
+        }
+    }
+}
