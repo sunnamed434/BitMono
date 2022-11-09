3e63ca231c26d1c8f504475bab062d5d157b7c65
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-11-09 18:07:10 +0200

    Update DotNetHook.cs

--- BitMono/BitMono.Protections/DotNetHook.cs
+++ BitMono/BitMono.Protections/DotNetHook.cs
@@ -162,42 +162,4 @@ namespace BitMono.Protections
             return Task.CompletedTask;
         }
     }
-
-    public class DotNetHookPhase : IPhaseProtection
-    {
-        private readonly IList<(MethodDef, MethodDef, int)> m_InstructionsToBeTokensUpdated;
-        private readonly IMethodDefSearcher m_MethodDefSearcher;
-
-        public DotNetHookPhase(IList<(MethodDef, MethodDef, int)> instructionsToBeTokensUpdated, IMethodDefSearcher methodDefSearcher)
-        {
-            m_InstructionsToBeTokensUpdated = instructionsToBeTokensUpdated;
-            m_MethodDefSearcher = methodDefSearcher;
-        }
-
-        public Task ExecuteAsync(ProtectionContext context, CancellationToken cancellationToken = default)
-        {
-            var moduleDefMD = ModuleDefMD.Load(context.BitMonoContext.OutputModuleFile, context.ModuleCreationOptions);
-            foreach (var tuple in m_InstructionsToBeTokensUpdated)
-            {
-                var callerMethodDef = m_MethodDefSearcher.Find(tuple.Item1.Name, moduleDefMD);
-                var targetMethodDef = m_MethodDefSearcher.Find(tuple.Item2.Name, moduleDefMD);
-                if (callerMethodDef != null && targetMethodDef != null)
-                {
-                    callerMethodDef.Body.Instructions.Insert(tuple.Item3, new Instruction(OpCodes.Ldc_I4, targetMethodDef.MDToken.ToInt32()));
-                }
-            }
-
-            var moduleWriterOptions = new ModuleWriterOptions(moduleDefMD);
-            moduleWriterOptions.Logger = DummyLogger.NoThrowInstance;
-            moduleWriterOptions.MetadataOptions.Flags |= MetadataFlags.KeepOldMaxStack;
-
-            using (moduleDefMD)
-            using (var fileStream = File.Open(context.BitMonoContext.OutputModuleFile, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite))
-            {
-                moduleDefMD.Write(fileStream, moduleWriterOptions);
-            }
-            context.ModuleDefMD = moduleDefMD;
-            return Task.CompletedTask;
-        }
-    }
 }
