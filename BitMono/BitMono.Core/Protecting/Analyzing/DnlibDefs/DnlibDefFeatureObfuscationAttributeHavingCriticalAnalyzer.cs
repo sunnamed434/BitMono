2e780c5efb1be2e3ea06c869cbeeed9dea514345
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-11-10 19:44:29 +0200

    Create DnlibDefFeatureObfuscationAttributeHavingCriticalAnalyzer.cs

---
+++ BitMono/BitMono.Core/Protecting/Analyzing/DnlibDefs/DnlibDefFeatureObfuscationAttributeHavingCriticalAnalyzer.cs
@@ -0,0 +1,32 @@
+﻿using BitMono.API.Protecting;
+using BitMono.API.Protecting.Analyzing;
+using BitMono.API.Protecting.Resolvers;
+using dnlib.DotNet;
+using System.Reflection;
+
+namespace BitMono.Core.Protecting.Analyzing.DnlibDefs
+{
+    public class DnlibDefFeatureObfuscationAttributeHavingCriticalAnalyzer<TFeature> : ICriticalAnalyzer<IDnlibDef>
+        where TFeature : IProtection
+    {
+        private readonly IObfuscationAttributeExcludingResolver m_ObfuscationAttributeExcludingResolver;
+
+        public DnlibDefFeatureObfuscationAttributeHavingCriticalAnalyzer(IObfuscationAttributeExcludingResolver obfuscationAttributeExcludingResolver)
+        {
+            m_ObfuscationAttributeExcludingResolver = obfuscationAttributeExcludingResolver;
+        }
+
+        public bool NotCriticalToMakeChanges(IDnlibDef dnlibDef)
+        {
+            if (m_ObfuscationAttributeExcludingResolver.TryResolve(dnlibDef, feature: typeof(TFeature).Name,
+                out ObfuscationAttribute typeDefObfuscationAttribute))
+            {
+                if (typeDefObfuscationAttribute.Exclude)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
