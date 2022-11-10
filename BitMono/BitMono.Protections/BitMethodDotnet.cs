21d44c7dd6427f767cb6657f5496e0ba2d5d5468
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-11-10 19:44:56 +0200

    Update BitMethodDotnet.cs

--- BitMono/BitMono.Protections/BitMethodDotnet.cs
+++ BitMono/BitMono.Protections/BitMethodDotnet.cs
@@ -2,6 +2,7 @@
 using BitMono.API.Protecting.Context;
 using BitMono.API.Protecting.Resolvers;
 using BitMono.Core.Protecting.Analyzing.DnlibDefs;
+using BitMono.Core.Protecting.Resolvers;
 using BitMono.Utilities.Extensions.dnlib;
 using dnlib.DotNet.Emit;
 using Serilog;
@@ -15,17 +16,20 @@ namespace BitMono.Protections
 {
     public class BitMethodDotnet : IProtection
     {
-        private readonly IObfuscationAttributeExcludingResolver m_ObfuscationAttributeExcludingResolver;
+        private readonly DnlibDefFeatureObfuscationAttributeHavingCriticalAnalyzer<BitMethodDotnet> m_DnlibDefFeatureObfuscationAttributeHavingCriticalAnalyzer;
+        private readonly DnlibDefSpecificNamespaceHavingCriticalAnalyzer m_DnlibDefSpecificNamespaceHavingCriticalAnalyzer;
         private readonly DnlibDefCriticalAnalyzer m_DnlibDefCriticalAnalyzer;
         private readonly ILogger m_Logger;
         private readonly Random m_Random;
 
         public BitMethodDotnet(
-            IObfuscationAttributeExcludingResolver obfuscationAttributeExcludingResolver,
+            DnlibDefFeatureObfuscationAttributeHavingCriticalAnalyzer<BitMethodDotnet> dnlibDefFeatureObfuscationAttributeHavingCriticalAnalyzer,
+            DnlibDefSpecificNamespaceHavingCriticalAnalyzer dnlibDefSpecificNamespaceHavingCriticalAnalyzer,
             DnlibDefCriticalAnalyzer dnlibDefCriticalAnalyzer,
             ILogger logger)
         {
-            m_ObfuscationAttributeExcludingResolver = obfuscationAttributeExcludingResolver;
+            m_DnlibDefFeatureObfuscationAttributeHavingCriticalAnalyzer = dnlibDefFeatureObfuscationAttributeHavingCriticalAnalyzer;
+            m_DnlibDefSpecificNamespaceHavingCriticalAnalyzer = dnlibDefSpecificNamespaceHavingCriticalAnalyzer;
             m_DnlibDefCriticalAnalyzer = dnlibDefCriticalAnalyzer;
             m_Logger = logger.ForContext<BitMethodDotnet>();
             m_Random = new Random();
@@ -35,28 +39,32 @@ namespace BitMono.Protections
         {
             foreach (var typeDef in context.ModuleDefMD.GetTypes().ToArray())
             {
-                if (m_ObfuscationAttributeExcludingResolver.TryResolve(typeDef, feature: nameof(BitMethodDotnet),
-                    out ObfuscationAttribute typeDefObfuscationAttribute))
+                if (m_DnlibDefFeatureObfuscationAttributeHavingCriticalAnalyzer.NotCriticalToMakeChanges(typeDef) == false)
                 {
-                    if (typeDefObfuscationAttribute.Exclude)
-                    {
-                        m_Logger.Debug("Found {0}, that applyed to members of type, skipping type.", nameof(ObfuscationAttribute));
-                        continue;
-                    }
+                    m_Logger.Debug("Found {0}, skipping.", nameof(ObfuscationAttribute));
+                    continue;
+                }
+
+                if (m_DnlibDefSpecificNamespaceHavingCriticalAnalyzer.NotCriticalToMakeChanges(typeDef) == false)
+                {
+                    m_Logger.Debug("Not able to make changes because of specific namespace was found, skipping.");
+                    continue;
                 }
 
                 if (typeDef.HasMethods)
                 {
                     foreach (var methodDef in typeDef.Methods)
                     {
-                        if (m_ObfuscationAttributeExcludingResolver.TryResolve(methodDef, feature: nameof(BitMethodDotnet),
-                            out ObfuscationAttribute methodDefObfuscationAttribute))
+                        if (m_DnlibDefFeatureObfuscationAttributeHavingCriticalAnalyzer.NotCriticalToMakeChanges(methodDef) == false)
                         {
-                            if (methodDefObfuscationAttribute.Exclude)
-                            {
-                                m_Logger.Debug("Found {0}, that applyed to method, skipping it.", nameof(ObfuscationAttribute));
-                                continue;
-                            }
+                            m_Logger.Debug("Found {0}, skipping.", nameof(ObfuscationAttribute));
+                            continue;
+                        }
+
+                        if (m_DnlibDefSpecificNamespaceHavingCriticalAnalyzer.NotCriticalToMakeChanges(methodDef) == false)
+                        {
+                            m_Logger.Debug("Not able to make changes because of specific namespace was found, skipping.");
+                            continue;
                         }
 
                         if (methodDef.HasBody && methodDef.IsConstructor == false
