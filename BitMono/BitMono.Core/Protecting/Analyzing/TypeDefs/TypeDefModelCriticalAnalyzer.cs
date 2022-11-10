9ec2cf5b8b3b98c64cd242fb5d9f72bfc118fb88
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-11-10 19:44:35 +0200

    Update TypeDefModelCriticalAnalyzer.cs

--- BitMono/BitMono.Core/Protecting/Analyzing/TypeDefs/TypeDefModelCriticalAnalyzer.cs
+++ BitMono/BitMono.Core/Protecting/Analyzing/TypeDefs/TypeDefModelCriticalAnalyzer.cs
@@ -19,19 +19,19 @@ namespace BitMono.Core.Protecting.Analyzing.TypeDefs
 
         public bool NotCriticalToMakeChanges(IHasCustomAttribute from)
         {
-            if (m_AttemptAttributeResolver.TryResolve<SerializableAttribute>(from, null, null, null, out _))
+            if (m_AttemptAttributeResolver.TryResolve<SerializableAttribute>(from, null, null, out _))
             {
                 return false;
             }
-            if (m_AttemptAttributeResolver.TryResolve<XmlAttributeAttribute>(from, null, null, null, out _))
+            if (m_AttemptAttributeResolver.TryResolve<XmlAttributeAttribute>(from, null, null, out _))
             {
                 return false;
             }
-            if (m_AttemptAttributeResolver.TryResolve<XmlArrayItemAttribute>(from, null, null, null, out _))
+            if (m_AttemptAttributeResolver.TryResolve<XmlArrayItemAttribute>(from, null, null, out _))
             {
                 return false;
             }
-            if (m_AttemptAttributeResolver.TryResolve<JsonPropertyAttribute>(from, null, null, null, out _))
+            if (m_AttemptAttributeResolver.TryResolve<JsonPropertyAttribute>(from, null, null, out _))
             {
                 return false;
             }
