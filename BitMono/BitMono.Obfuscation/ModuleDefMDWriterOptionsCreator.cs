51b0766538008d4bfa1df227d100adeeb91ddb7b
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-11-11 15:29:26 +0200

    Create ModuleDefMDWriterOptionsCreator.cs

---
+++ BitMono/BitMono.Obfuscation/ModuleDefMDWriterOptionsCreator.cs
@@ -0,0 +1,19 @@
+﻿using dnlib.DotNet;
+using dnlib.DotNet.MD;
+using dnlib.DotNet.Writer;
+using System.Threading.Tasks;
+
+namespace BitMono.Obfuscation
+{
+    public class ModuleDefMDWriterOptionsCreator
+    {
+        public Task<ModuleWriterOptions> CreateAsync(ModuleDefMD moduleDefMD)
+        {
+            var moduleDefMDWriterOptions = new ModuleWriterOptions(moduleDefMD);
+            moduleDefMDWriterOptions.MetadataLogger = DummyLogger.NoThrowInstance;
+            moduleDefMDWriterOptions.MetadataOptions.Flags |= MetadataFlags.KeepOldMaxStack | MetadataFlags.PreserveAll;
+            moduleDefMDWriterOptions.Cor20HeaderOptions.Flags = ComImageFlags.ILOnly;
+            return Task.FromResult(moduleDefMDWriterOptions);
+        }
+    }
+}
