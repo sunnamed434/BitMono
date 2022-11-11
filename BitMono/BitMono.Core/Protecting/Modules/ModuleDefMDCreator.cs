53d8ad7d35e5723b410c5347a1da4fff52b9be0b
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-11-11 10:23:07 +0200

    Create ModuleDefMDCreator.cs

---
+++ BitMono/BitMono.Core/Protecting/Modules/ModuleDefMDCreator.cs
@@ -0,0 +1,31 @@
+﻿using dnlib.DotNet;
+using dnlib.DotNet.MD;
+using dnlib.DotNet.Writer;
+using System.Threading.Tasks;
+
+namespace BitMono.Core.Protecting.Modules
+{
+    public class ModuleDefMDCreator
+    {
+        public Task<ModuleDefMDCreationResult> CreateAsync(string moduleFile)
+        {
+            var assemblyResolver = new AssemblyResolver();
+            var moduleContext = new ModuleContext(assemblyResolver);
+            assemblyResolver.DefaultModuleContext = moduleContext;
+            var moduleCreationOptions = new ModuleCreationOptions(assemblyResolver.DefaultModuleContext, CLRRuntimeReaderKind.Mono);
+            var moduleDefMD = ModuleDefMD.Load(moduleFile, moduleCreationOptions);
+            var moduleWriterOptions = new ModuleWriterOptions(moduleDefMD);
+            moduleWriterOptions.MetadataLogger = DummyLogger.NoThrowInstance;
+            moduleWriterOptions.MetadataOptions.Flags |= MetadataFlags.KeepOldMaxStack | MetadataFlags.PreserveAll;
+            moduleWriterOptions.Cor20HeaderOptions.Flags = ComImageFlags.ILOnly;
+            return Task.FromResult(new ModuleDefMDCreationResult
+            {
+                AssemblyResolver = assemblyResolver,
+                ModuleContext = moduleContext,
+                ModuleCreationOptions = moduleCreationOptions,
+                ModuleDefMD = moduleDefMD,
+                ModuleWriterOptions = moduleWriterOptions,
+            });
+        }
+    }
+}
