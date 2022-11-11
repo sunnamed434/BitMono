c54db2ca91b6d308eba3851cb5d575934e9acd33
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-11-11 10:22:45 +0200

    Create IEngine.cs

---
+++ BitMono/BitMono.API/Protecting/Modules/IEngine.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace BitMono.API.Protecting.Modules
+{
+    public interface IEngine
+    {
+        Task StartAsync(CancellationToken cancellationToken = default);
+    }
+}
