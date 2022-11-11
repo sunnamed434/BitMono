5c2ef7a1011b0dacff35a6387be585db4fdc13f4
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-11-11 15:06:08 +0200

    Create TipsNotifier.cs

---
+++ BitMono/BitMono.Obfuscation/TipsNotifier.cs
@@ -0,0 +1,31 @@
+﻿using BitMono.API.Configuration;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BitMono.Obfuscation
+{
+    public class TipsNotifier
+    {
+        private readonly IConfiguration m_Configuration;
+        private readonly ILogger m_Logger;
+
+        public TipsNotifier(IBitMonoAppSettingsConfiguration configuration, ILogger logger)
+        {
+            m_Configuration = configuration.Configuration;
+            m_Logger = logger.ForContext<TipsNotifier>();
+        }
+
+        public Task NotifyAsync()
+        {
+            var tips = m_Configuration.GetSection("Tips").Get<string[]>();
+            Random random = new Random();
+            var tip = tips.Reverse().ToArray()[random.Next(0, tips.Length)];
+            m_Logger.Information("Today is your day! Generating helpful tip for you - see it a bit down!");
+            m_Logger.Information(tip);
+            return Task.CompletedTask;
+        }
+    }
+}
