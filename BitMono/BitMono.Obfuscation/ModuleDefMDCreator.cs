1b6bc30bfc6146065d671eb0bd7deeb169d665db
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-11-11 15:29:28 +0200

    Update ModuleDefMDCreator.cs

--- BitMono/BitMono.Obfuscation/ModuleDefMDCreator.cs
+++ BitMono/BitMono.Obfuscation/ModuleDefMDCreator.cs
@@ -1,31 +1,27 @@
 ﻿using dnlib.DotNet;
-using dnlib.DotNet.MD;
-using dnlib.DotNet.Writer;
 using System.Threading.Tasks;
 
 namespace BitMono.Obfuscation
 {
     public class ModuleDefMDCreator
     {
-        public Task<ModuleDefMDCreationResult> CreateAsync(string moduleFile)
+        public async Task<ModuleDefMDCreationResult> CreateAsync(string moduleFile)
         {
             var assemblyResolver = new AssemblyResolver();
             var moduleContext = new ModuleContext(assemblyResolver);
             assemblyResolver.DefaultModuleContext = moduleContext;
             var moduleCreationOptions = new ModuleCreationOptions(assemblyResolver.DefaultModuleContext, CLRRuntimeReaderKind.Mono);
             var moduleDefMD = ModuleDefMD.Load(moduleFile, moduleCreationOptions);
-            var moduleWriterOptions = new ModuleWriterOptions(moduleDefMD);
-            moduleWriterOptions.MetadataLogger = DummyLogger.NoThrowInstance;
-            moduleWriterOptions.MetadataOptions.Flags |= MetadataFlags.KeepOldMaxStack | MetadataFlags.PreserveAll;
-            moduleWriterOptions.Cor20HeaderOptions.Flags = ComImageFlags.ILOnly;
-            return Task.FromResult(new ModuleDefMDCreationResult
+
+            var moduleDefMDWriterOptions = await new ModuleDefMDWriterOptionsCreator().CreateAsync(moduleDefMD);
+            return new ModuleDefMDCreationResult
             {
                 AssemblyResolver = assemblyResolver,
                 ModuleContext = moduleContext,
                 ModuleCreationOptions = moduleCreationOptions,
                 ModuleDefMD = moduleDefMD,
-                ModuleWriterOptions = moduleWriterOptions,
-            });
+                ModuleWriterOptions = moduleDefMDWriterOptions,
+            };
         }
     }
 }
