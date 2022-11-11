b12c2e5fa333ac9e28422a32435022de3d142105
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-11-11 15:04:49 +0200

    Create IBitMonoModuleFileResolver.cs

---
+++ BitMono/BitMono.API/Protecting/Resolvers/IBitMonoModuleFileResolver.cs
@@ -0,0 +1,10 @@
+﻿using BitMono.API.Protecting.Context;
+using System.Threading.Tasks;
+
+namespace BitMono.API.Protecting.Resolvers
+{
+    public interface IBitMonoModuleFileResolver
+    {
+        Task ResolveAsync(BitMonoContext context);
+    }
+}
