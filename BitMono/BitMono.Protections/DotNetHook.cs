29e205471ea56b1308586ad44e5cac1ef05653f6
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-11-11 15:21:49 +0200

    Update DotNetHook.cs

--- BitMono/BitMono.Protections/DotNetHook.cs
+++ BitMono/BitMono.Protections/DotNetHook.cs
@@ -2,10 +2,8 @@
 using BitMono.API.Protecting.Injection.MethodDefs;
 using BitMono.API.Protecting.Pipeline;
 using BitMono.API.Protecting.Renaming;
-using BitMono.API.Protecting.Resolvers;
 using BitMono.Core.Protecting.Analyzing.DnlibDefs;
 using BitMono.ExternalComponents;
-using BitMono.Utilities.Extensions.dnlib;
 using dnlib.DotNet;
 using dnlib.DotNet.Emit;
 using dnlib.DotNet.Writer;
