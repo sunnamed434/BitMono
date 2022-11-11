36f78a957237b975de67e33b78da410edde6843a
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-11-11 22:15:26 +0200

    Update ModuleDefMDCreator.cs

--- BitMono/BitMono.Obfuscation/ModuleDefMDCreator.cs
+++ BitMono/BitMono.Obfuscation/ModuleDefMDCreator.cs
@@ -1,17 +1,25 @@
-﻿using dnlib.DotNet;
+﻿using BitMono.Obfuscation.API;
+using dnlib.DotNet;
 using System.Threading.Tasks;
 
 namespace BitMono.Obfuscation
 {
-    public class ModuleDefMDCreator
+    public class ModuleDefMDCreator : IModuleDefMDCreator
     {
-        public async Task<ModuleDefMDCreationResult> CreateAsync(string moduleFile)
+        private readonly byte[] m_ModuleBytes;
+
+        public ModuleDefMDCreator(byte[] moduleBytes)
+        {
+            m_ModuleBytes = moduleBytes;
+        }
+
+        public async Task<ModuleDefMDCreationResult> CreateAsync()
         {
             var assemblyResolver = new AssemblyResolver();
             var moduleContext = new ModuleContext(assemblyResolver);
             assemblyResolver.DefaultModuleContext = moduleContext;
             var moduleCreationOptions = new ModuleCreationOptions(assemblyResolver.DefaultModuleContext, CLRRuntimeReaderKind.Mono);
-            var moduleDefMD = ModuleDefMD.Load(moduleFile, moduleCreationOptions);
+            var moduleDefMD = ModuleDefMD.Load(m_ModuleBytes, moduleCreationOptions);
 
             var moduleDefMDWriterOptions = await new ModuleDefMDWriterOptionsCreator().CreateAsync(moduleDefMD);
             return new ModuleDefMDCreationResult
