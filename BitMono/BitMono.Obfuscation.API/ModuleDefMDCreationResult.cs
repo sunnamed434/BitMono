9c6c723108aa99619427209fefe9e5b7b68b0fd0
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-11-11 22:15:51 +0200

    Create ModuleDefMDCreationResult.cs

---
+++ BitMono/BitMono.Obfuscation.API/ModuleDefMDCreationResult.cs
@@ -0,0 +1,14 @@
+﻿using dnlib.DotNet;
+using dnlib.DotNet.Writer;
+
+namespace BitMono.Obfuscation
+{
+    public class ModuleDefMDCreationResult
+    {
+        public AssemblyResolver AssemblyResolver { get; set; }
+        public ModuleContext ModuleContext { get; set; }
+        public ModuleCreationOptions ModuleCreationOptions { get; set; }
+        public ModuleDefMD ModuleDefMD { get; set; }
+        public ModuleWriterOptions ModuleWriterOptions { get; set; }
+    }
+}
