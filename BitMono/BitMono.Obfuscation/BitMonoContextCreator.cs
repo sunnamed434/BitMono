768d944cf9e4e5cf86b4b4fc88996f472109a718
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-11-11 22:15:34 +0200

    Update BitMonoContextCreator.cs

--- BitMono/BitMono.Obfuscation/BitMonoContextCreator.cs
+++ BitMono/BitMono.Obfuscation/BitMonoContextCreator.cs
@@ -1,7 +1,7 @@
 ﻿using BitMono.API.Configuration;
 using BitMono.API.Protecting.Context;
 using Microsoft.Extensions.Configuration;
-using System.IO;
+using NullGuard;
 using System.Threading.Tasks;
 
 namespace BitMono.Obfuscation
@@ -15,16 +15,12 @@ namespace BitMono.Obfuscation
             m_Configuration = configuration.Configuration;
         }
 
-        public Task<BitMonoContext> CreateAsync(string currentAssemblyDirectory, string baseDirectoryName, string outputDirectoryName)
+        public Task<BitMonoContext> CreateAsync(string outputDirectoryName, [AllowNull] string dependenciesDirectoryName = null)
         {
-            Directory.CreateDirectory(baseDirectoryName);
-            Directory.CreateDirectory(outputDirectoryName);
-            var baseDirectoryPath = Path.Combine(currentAssemblyDirectory, baseDirectoryName);
-            var outputDirectoryPath = Path.Combine(currentAssemblyDirectory, outputDirectoryName);
             var bitMonoContext = new BitMonoContext
             {
-                BaseDirectory = baseDirectoryPath,
-                OutputDirectory = outputDirectoryPath,
+                OutputPath = outputDirectoryName,
+                DependenciesDirectoryName = dependenciesDirectoryName,
                 Watermark = m_Configuration.GetValue<bool>(nameof(Shared.Models.Obfuscation.Watermark)),
             };
             return Task.FromResult(bitMonoContext);
