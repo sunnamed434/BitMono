849536d619114a9570a1e795c890fa37d4dbca23
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-11-14 20:26:45 +0200

    Update ProtectionsSortingResult.cs

--- BitMono/BitMono.Obfuscation/ProtectionsSortingResult.cs
+++ BitMono/BitMono.Obfuscation/ProtectionsSortingResult.cs
@@ -7,6 +7,7 @@ namespace BitMono.Obfuscation
     public class ProtectionsSortingResult
     {
         public ICollection<IProtection> Protections { get; set; }
+        public IEnumerable<IProtection> DeprecatedProtections { get; set; }
         public ICollection<string> Skipped { get; set; }
         public IEnumerable<IStageProtection> StageProtections { get; set; }
         public IEnumerable<IPipelineProtection> PipelineProtections { get; set; }
