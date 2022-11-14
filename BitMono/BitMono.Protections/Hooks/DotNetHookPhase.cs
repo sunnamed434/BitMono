38eda58801b871bd25b121e5e92e2d2a50a8eb30
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-11-14 18:21:24 +0200

    Update DotNetHookPhase.cs

--- BitMono/BitMono.Protections/Hooks/DotNetHookPhase.cs
+++ BitMono/BitMono.Protections/Hooks/DotNetHookPhase.cs
@@ -3,7 +3,6 @@ using BitMono.API.Protecting.Injection.MethodDefs;
 using BitMono.API.Protecting.Pipeline;
 using dnlib.DotNet;
 using dnlib.DotNet.Emit;
-using dnlib.DotNet.Writer;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading;
@@ -25,29 +24,25 @@ namespace BitMono.Protections.Hooks
         public Task ExecuteAsync(ProtectionContext context, CancellationToken cancellationToken = default)
         {
             var moduleDefMD = ModuleDefMD.Load(context.BitMonoContext.OutputModuleFile, context.ModuleCreationOptions);
+            context.ModuleDefMD = moduleDefMD;
             foreach (var instructionTokensUpdate in m_InstructionsToBeTokensUpdated)
             {
-                var initializatorMethodDef = m_MethodDefSearcher.Find(instructionTokensUpdate.InitializatorMethodDef.Name, moduleDefMD);
-                var fromMethodDef = m_MethodDefSearcher.Find(instructionTokensUpdate.FromMethodDef.Name, moduleDefMD);
-                var toMethodDef = m_MethodDefSearcher.Find(instructionTokensUpdate.ToMethodDef.Name, moduleDefMD);
+                var initializatorMethodDef = m_MethodDefSearcher.Find(instructionTokensUpdate.InitializatorMethodDef.Name, context.ModuleDefMD);
+                var fromMethodDef = m_MethodDefSearcher.Find(instructionTokensUpdate.FromMethodDef.Name, context.ModuleDefMD);
+                var toMethodDef = m_MethodDefSearcher.Find(instructionTokensUpdate.ToMethodDef.Name, context.ModuleDefMD);
 
                 if (initializatorMethodDef != null && fromMethodDef != null && toMethodDef != null)
                 {
                     initializatorMethodDef.Body.Instructions.Insert(instructionTokensUpdate.Index, new Instruction(OpCodes.Ldc_I4, fromMethodDef.MDToken.ToInt32()));
-                    initializatorMethodDef.Body.Instructions.Insert(instructionTokensUpdate.Index, new Instruction(OpCodes.Ldc_I4, toMethodDef.MDToken.ToInt32()));
+                    initializatorMethodDef.Body.Instructions.Insert(instructionTokensUpdate.Index + 1, new Instruction(OpCodes.Ldc_I4, toMethodDef.MDToken.ToInt32()));
                 }
             }
 
-            var moduleWriterOptions = new ModuleWriterOptions(moduleDefMD);
-            moduleWriterOptions.Logger = DummyLogger.NoThrowInstance;
-            moduleWriterOptions.MetadataOptions.Flags |= MetadataFlags.KeepOldMaxStack | MetadataFlags.PreserveAll;
-
-            using (moduleDefMD)
+            using (context.ModuleDefMD)
             using (var fileStream = File.Open(context.BitMonoContext.OutputModuleFile, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite))
             {
-                moduleDefMD.Write(fileStream, moduleWriterOptions);
+                context.ModuleDefMD.Write(fileStream, context.ModuleWriterOptions);
             }
-            context.ModuleDefMD = moduleDefMD;
             return Task.CompletedTask;
         }
     }
