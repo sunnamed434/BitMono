7449aa15e3163557ba80697f2589c7db1ea05aed
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-11-18 19:38:04 +0200

    Update IModuleDefMDCreator.cs

--- BitMono/BitMono.Obfuscation.API/IModuleDefMDCreator.cs
+++ BitMono/BitMono.Obfuscation.API/IModuleDefMDCreator.cs
@@ -1,9 +1,7 @@
-﻿using System.Threading.Tasks;
-
-namespace BitMono.Obfuscation.API
+﻿namespace BitMono.Obfuscation.API
 {
     public interface IModuleDefMDCreator
     {
-        Task<ModuleDefMDCreationResult> CreateAsync();
+        ModuleDefMDCreationResult Create();
     }
 }
