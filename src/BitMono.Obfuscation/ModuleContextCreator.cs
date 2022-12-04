da8abcb7eccf862c88ae167797f909f4c3d54999
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-04 19:07:12 +0200

    Create ModuleContextCreator.cs

---
+++ src/BitMono.Obfuscation/ModuleContextCreator.cs
@@ -0,0 +1,12 @@
+﻿using dnlib.DotNet;
+
+namespace BitMono.Obfuscation
+{
+    public class ModuleContextCreator
+    {
+        public ModuleContext Create()
+        {
+            return ModuleDefMD.CreateModuleContext();
+        }
+    }
+}
