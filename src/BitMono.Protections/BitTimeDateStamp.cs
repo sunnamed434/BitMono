83e6000a4af7b2679778267a4f6e22aaca4c40bf
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-04 19:07:22 +0200

    Create BitTimeDateStamp.cs

---
+++ src/BitMono.Protections/BitTimeDateStamp.cs
@@ -0,0 +1,30 @@
+﻿using BitMono.API.Protecting;
+using BitMono.API.Protecting.Contexts;
+using BitMono.API.Protecting.Pipeline;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BitMono.Protections
+{
+    public class BitTimeDateStamp : IStageProtection
+    {
+        public PipelineStages Stage => PipelineStages.ModuleWritten;
+
+        public Task ExecuteAsync(ProtectionContext context, CancellationToken cancellationToken = default)
+        {
+            using (var stream = File.Open(context.BitMonoContext.OutputModuleFile, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite))
+            using (var reader = new BinaryReader(stream))
+            using (var writer = new BinaryWriter(stream))
+            {
+                stream.Position = 0x3C;
+                var peHeader = reader.ReadUInt32();
+                stream.Position = peHeader;
+                var timeDateStamp = stream.Position + 0x8;
+                stream.Position = timeDateStamp;
+                writer.Write(0);
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
