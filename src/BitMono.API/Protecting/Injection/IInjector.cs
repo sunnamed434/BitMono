dccd7482180405f25c64fece175031f486f18ebd
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-07 23:24:26 +0200

    Update IInjector.cs

--- src/BitMono.API/Protecting/Injection/IInjector.cs
+++ src/BitMono.API/Protecting/Injection/IInjector.cs
@@ -4,10 +4,10 @@ namespace BitMono.API.Protecting.Injection
 {
     public interface IInjector
     {
-        FieldDef InjectArrayInGlobalNestedTypes(ModuleDefMD moduleDefMD, byte[] injectedData, string injectedName);
-        FieldDef InjectArray(ModuleDefMD moduleDefMD, TypeDef typeDef, byte[] injectedData, string injectedName);
+        FieldDef InjectInvisibleArray(ModuleDefMD moduleDefMD, TypeDef typeDef, byte[] data, string name);
         TypeDef CreateInvisibleType(ModuleDefMD moduleDefMD, string name = null);
         TypeDef CreateInvisibleValueType(ModuleDefMD moduleDefMD, string name = null);
+        TypeDef InjectInvisibleValueType(ModuleDefMD moduleDefMD, TypeDef typeDef, string name = null);
         CustomAttribute InjectCompilerGeneratedAttribute(ModuleDefMD moduleDefMD, TypeDef typeDef = null);
         void InjectAttributeWithContent(ModuleDefMD moduleDefMD, string @namespace, string @name, string text);
         void InjectAttribute(ModuleDefMD moduleDefMD, string @namespace, string @name);
