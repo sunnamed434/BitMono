a7203ac69844990433034454daae307ee4cc95b1
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-07 23:23:08 +0200

    Update ProtectionContext.cs

--- src/BitMono.API/Protecting/Contexts/ProtectionContext.cs
+++ src/BitMono.API/Protecting/Contexts/ProtectionContext.cs
@@ -9,9 +9,9 @@ namespace BitMono.API.Protecting.Contexts
         [AllowNull] public ModuleDefMD ModuleDefMD { get; set; }
         [AllowNull] public ModuleCreationOptions ModuleCreationOptions { get; set; }
         [AllowNull] public ModuleWriterOptions ModuleWriterOptions { get; set; }
-        [AllowNull] public ModuleDefMD ExternalComponentsModuleDefMD { get; set; }
+        [AllowNull] public ModuleDefMD RuntimeModuleDefMD { get; set; }
         [AllowNull] public Importer Importer { get; set; }
-        [AllowNull] public Importer ExternalComponentsImporter { get; set; }
+        [AllowNull] public Importer RuntimeImporter { get; set; }
         [AllowNull] public BitMonoContext BitMonoContext { get; set; }
 
         [AllowNull] public ModuleContext ModuleContext => ModuleCreationOptions.Context;
