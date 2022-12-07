c4d7b3c50ccbdb1cedc5fa31770d2e23bcd18e87
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-07 23:23:41 +0200

    Create DnlibDefExtensions.cs

---
+++ src/BitMono.Utilities/Extensions/dnlib/DnlibDefExtensions.cs
@@ -0,0 +1,24 @@
+﻿using dnlib.DotNet;
+
+namespace BitMono.Utilities.Extensions.dnlib
+{
+    public static class DnlibDefExtensions
+    {
+        public static MethodDef ResolveMethodDefOrThrow(this IDnlibDef source) 
+        {
+            if (source is MethodDef methodDef) 
+            {
+                return methodDef.ResolveMethodDefThrow();
+            }
+            throw new MemberRefResolveException($"Could not resolve method: {source}");
+        }
+        public static FieldDef ResolveFieldDefOrThrow(this IDnlibDef source)
+        {
+            if (source is FieldDef fieldDef) 
+            {
+                return fieldDef.ResolveFieldDefThrow();
+            }
+            throw new MemberRefResolveException($"Could not resolve field: {source}");
+        }
+    }
+}
