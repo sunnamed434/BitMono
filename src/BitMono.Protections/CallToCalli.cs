b0c964e2fd81f464cbf3aabee10bfda4ec9f7128
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-07 23:24:31 +0200

    Update CallToCalli.cs

--- src/BitMono.Protections/CallToCalli.cs
+++ src/BitMono.Protections/CallToCalli.cs
@@ -63,7 +63,6 @@ namespace BitMono.Protections
                     m_Logger.Information("Found {0}, skipping.", nameof(ObfuscationAttribute));
                     continue;
                 }
-
                 if (m_DnlibDefSpecificNamespaceHavingCriticalAnalyzer.NotCriticalToMakeChanges(typeDef) == false)
                 {
                     m_Logger.Information("Not able to make changes because of specific namespace was found, skipping.");
@@ -83,7 +82,6 @@ namespace BitMono.Protections
                             m_Logger.Information("Found {0}, skipping.", nameof(ObfuscationAttribute));
                             continue;
                         }
-
                         if (m_DnlibDefSpecificNamespaceHavingCriticalAnalyzer.NotCriticalToMakeChanges(methodDef) == false)
                         {
                             m_Logger.Information("Not able to make changes because of specific namespace was found, skipping.");
