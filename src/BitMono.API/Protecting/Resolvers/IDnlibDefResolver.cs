6cfe5ad7d739cdbc2fd9253882d4fc6cda1e2a85
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-13 12:35:02 +0200

    Create IDnlibDefResolver.cs

---
+++ src/BitMono.API/Protecting/Resolvers/IDnlibDefResolver.cs
@@ -0,0 +1,9 @@
+﻿using dnlib.DotNet;
+
+namespace BitMono.API.Protecting.Resolvers
+{
+    public interface IDnlibDefResolver
+    {
+        bool Resolve(string feature, IDnlibDef dnlibDef);
+    }
+}
