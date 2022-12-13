7731cc941e61d7ed30fe69150a6cf1b628ec2c9d
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-13 12:26:41 +0200

    Create ProtectionParameters.cs

---
+++ src/BitMono.API/Protecting/Contexts/ProtectionParameters.cs
@@ -0,0 +1,15 @@
+﻿using dnlib.DotNet;
+using System.Collections.Generic;
+
+namespace BitMono.Core.Protecting
+{
+    public class ProtectionParameters
+    {
+        public ProtectionParameters(List<IDnlibDef> targets)
+        {
+            Targets = targets;
+        }
+
+        public List<IDnlibDef> Targets { get; private set; }
+    }
+}
