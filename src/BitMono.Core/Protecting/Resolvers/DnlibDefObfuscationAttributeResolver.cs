f99b3b5ba728c7a1732fbbe105b094fae0e9c163
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-13 12:35:31 +0200

    Create DnlibDefObfuscationAttributeResolver.cs

---
+++ src/BitMono.Core/Protecting/Resolvers/DnlibDefObfuscationAttributeResolver.cs
@@ -0,0 +1,32 @@
+﻿using BitMono.API.Protecting;
+using BitMono.API.Protecting.Resolvers;
+using BitMono.Utilities.Extensions;
+using dnlib.DotNet;
+using System.Reflection;
+
+namespace BitMono.Core.Protecting.Resolvers
+{
+    public class DnlibDefObfuscationAttributeResolver : IDnlibDefObfuscationAttributeResolver
+    {
+        private readonly IObfuscationAttributeExcludingResolver m_ObfuscationAttributeExcludingResolver;
+
+        public DnlibDefObfuscationAttributeResolver(IObfuscationAttributeExcludingResolver obfuscationAttributeExcludingResolver)
+        {
+            m_ObfuscationAttributeExcludingResolver = obfuscationAttributeExcludingResolver;
+        }
+
+        public bool Resolve(string feature, IDnlibDef dnlibDef)
+        {
+            if (m_ObfuscationAttributeExcludingResolver.TryResolve(feature, dnlibDef,
+                out ObfuscationAttribute typeDefObfuscationAttribute) && typeDefObfuscationAttribute.Exclude)
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool Resolve<TFeature>(IDnlibDef dnlibDef) where TFeature : IProtection
+        {
+            return Resolve(typeof(TFeature).GetName(), dnlibDef);
+        }
+    }
+}
