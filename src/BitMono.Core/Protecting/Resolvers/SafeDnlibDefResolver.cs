a579924d3f3be0d7c6886abcb9b3921608d0afcd
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-13 12:35:35 +0200

    Create SafeDnlibDefResolver.cs

---
+++ src/BitMono.Core/Protecting/Resolvers/SafeDnlibDefResolver.cs
@@ -0,0 +1,33 @@
+﻿using BitMono.API.Protecting.Resolvers;
+using BitMono.Core.Protecting.Analyzing.DnlibDefs;
+using dnlib.DotNet;
+
+namespace BitMono.Core.Protecting.Resolvers
+{
+    public class SafeDnlibDefResolver : IDnlibDefResolver
+    {
+        private readonly IDnlibDefObfuscationAttributeResolver m_DnlibDefObfuscationAttributeResolver;
+        private readonly DnlibDefSpecificNamespaceCriticalAnalyzer m_DnlibDefSpecificNamespaceCriticalAnalyzer;
+
+        public SafeDnlibDefResolver(
+            IDnlibDefObfuscationAttributeResolver dnlibDefObfuscationAttributeResolver,
+            DnlibDefSpecificNamespaceCriticalAnalyzer dnlibDefSpecificNamespaceCriticalAnalyzer)
+        {
+            m_DnlibDefObfuscationAttributeResolver = dnlibDefObfuscationAttributeResolver;
+            m_DnlibDefSpecificNamespaceCriticalAnalyzer = dnlibDefSpecificNamespaceCriticalAnalyzer;
+        }
+
+        public bool Resolve(string feature, IDnlibDef dnlibDef)
+        {
+            if (m_DnlibDefObfuscationAttributeResolver.Resolve(feature, dnlibDef))
+            {
+                return false;
+            }
+            if (m_DnlibDefSpecificNamespaceCriticalAnalyzer.NotCriticalToMakeChanges(dnlibDef) == false)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
