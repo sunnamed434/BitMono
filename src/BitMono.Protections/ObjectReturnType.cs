4a1b5f60f9346a4c4be4b1ade793fb46ca162935
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-13 12:51:11 +0200

    Update ObjectReturnType.cs

--- src/BitMono.Protections/ObjectReturnType.cs
+++ src/BitMono.Protections/ObjectReturnType.cs
@@ -1,81 +1,49 @@
 ﻿using BitMono.API.Protecting;
 using BitMono.API.Protecting.Contexts;
-using BitMono.API.Protecting.Resolvers;
 using BitMono.Core.Protecting;
 using BitMono.Core.Protecting.Analyzing.DnlibDefs;
 using BitMono.Core.Protecting.Attributes;
 using BitMono.Utilities.Extensions.dnlib;
-using Serilog;
+using dnlib.DotNet;
 using System.Linq;
-using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
+using ILogger = Serilog.ILogger;
 
 namespace BitMono.Protections
 {
     [ProtectionName(nameof(ObjectReturnType))]
     public class ObjectReturnType : IProtection
     {
-        private readonly IDnlibDefObfuscationAttributeResolver m_DnlibDefFeatureObfuscationAttributeHavingResolver;
-        private readonly DnlibDefSpecificNamespaceCriticalAnalyzer m_DnlibDefSpecificNamespaceHavingCriticalAnalyzer;
         private readonly DnlibDefCriticalAnalyzer m_DnlibDefCriticalAnalyzer;
         private readonly ILogger m_Logger;
 
         public ObjectReturnType(
-            IDnlibDefObfuscationAttributeResolver dnlibDefFeatureObfuscationAttributeHavingResolver,
-            DnlibDefSpecificNamespaceCriticalAnalyzer dnlibDefSpecificNamespaceHavingCriticalAnalyzer,
             DnlibDefCriticalAnalyzer methodDefCriticalAnalyzer,
             ILogger logger)
         {
-            m_DnlibDefFeatureObfuscationAttributeHavingResolver = dnlibDefFeatureObfuscationAttributeHavingResolver;
-            m_DnlibDefSpecificNamespaceHavingCriticalAnalyzer = dnlibDefSpecificNamespaceHavingCriticalAnalyzer;
             m_DnlibDefCriticalAnalyzer = methodDefCriticalAnalyzer;
             m_Logger = logger.ForContext<ObjectReturnType>();
         }
 
         public Task ExecuteAsync(ProtectionContext context, ProtectionParameters parameters, CancellationToken cancellationToken = default)
         {
-            foreach (var typeDef in context.ModuleDefMD.GetTypes().ToArray())
+            foreach (var typeDef in parameters.Targets.OfType<TypeDef>())
             {
-                if (m_DnlibDefFeatureObfuscationAttributeHavingResolver.Resolve<ObjectReturnType>(typeDef))
+                foreach (var methodDef in typeDef.Methods.ToArray())
                 {
-                    m_Logger.Information("Found {0}, skipping.", nameof(ObfuscationAttribute));
-                    continue;
-                }
-                if (m_DnlibDefSpecificNamespaceHavingCriticalAnalyzer.NotCriticalToMakeChanges(typeDef) == false)
-                {
-                    m_Logger.Information("Not able to make changes because of specific namespace was found, skipping.");
-                    continue;
-                }
-
-                if (typeDef.HasMethods)
-                {
-                    foreach (var methodDef in typeDef.Methods.ToArray())
+                    if (methodDef.HasReturnType
+                        && methodDef.ReturnType != context.ModuleDefMD.CorLibTypes.Boolean)
                     {
-                        if (m_DnlibDefFeatureObfuscationAttributeHavingResolver.Resolve<ObjectReturnType>(methodDef))
+                        if (methodDef.IsConstructor == false && methodDef.IsVirtual == false
+                            && m_DnlibDefCriticalAnalyzer.NotCriticalToMakeChanges(methodDef)
+                            && methodDef.NotAsync())
                         {
-                            m_Logger.Information("Found {0}, skipping.", nameof(ObfuscationAttribute));
-                            continue;
-                        }
-                        if (m_DnlibDefSpecificNamespaceHavingCriticalAnalyzer.NotCriticalToMakeChanges(methodDef) == false)
-                        {
-                            m_Logger.Information("Not able to make changes because of specific namespace was found, skipping.");
-                            continue;
-                        }
-
-                        if (methodDef.HasReturnType
-                            && methodDef.ReturnType != context.ModuleDefMD.CorLibTypes.Boolean)
-                        {
-                            if (methodDef.IsConstructor == false && methodDef.IsVirtual == false
-                                && m_DnlibDefCriticalAnalyzer.NotCriticalToMakeChanges(methodDef)
-                                && methodDef.NotAsync())
+                            if (methodDef.IsSetter == false && methodDef.IsGetter == false)
                             {
-                                if (methodDef.IsSetter == false && methodDef.IsGetter == false)
+                                if (methodDef.Parameters.Any(p => p.HasParamDef && (p.ParamDef.IsOut || p.ParamDef.IsIn)) == false)
                                 {
-                                    if (methodDef.Parameters.Any(p => p.HasParamDef && (p.ParamDef.IsOut || p.ParamDef.IsIn)) == false)
-                                    {
-                                        methodDef.ReturnType = context.ModuleDefMD.CorLibTypes.Object;
-                                    }
+                                    methodDef.ReturnType = context.ModuleDefMD.CorLibTypes.Object;
                                 }
                             }
                         }
