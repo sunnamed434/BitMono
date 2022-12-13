fc1c9aa48052a6a6bbe5d1188cdf1094f86e036c
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-13 12:36:27 +0200

    Update CallToCalli.cs

--- src/BitMono.Protections/CallToCalli.cs
+++ src/BitMono.Protections/CallToCalli.cs
@@ -4,12 +4,12 @@ using BitMono.API.Protecting.Injection;
 using BitMono.API.Protecting.Pipeline;
 using BitMono.API.Protecting.Renaming;
 using BitMono.API.Protecting.Resolvers;
+using BitMono.Core.Protecting;
 using BitMono.Core.Protecting.Analyzing.DnlibDefs;
-using BitMono.Utilities.Extensions.dnlib;
+using BitMono.Core.Protecting.Attributes;
 using dnlib.DotNet;
 using dnlib.DotNet.Emit;
 using System;
-using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Threading;
@@ -18,20 +18,21 @@ using ILogger = Serilog.ILogger;
 
 namespace BitMono.Protections
 {
+    [ProtectionName(nameof(CallToCalli))]
     public class CallToCalli : IStageProtection
     {
         private readonly IInjector m_Injector;
         private readonly IRenamer m_Renamer;
-        private readonly IDnlibDefFeatureObfuscationAttributeHavingResolver m_DnlibDefFeatureObfuscationAttributeHavingResolver;
-        private readonly DnlibDefSpecificNamespaceHavingCriticalAnalyzer m_DnlibDefSpecificNamespaceHavingCriticalAnalyzer;
+        private readonly IDnlibDefObfuscationAttributeResolver m_DnlibDefFeatureObfuscationAttributeHavingResolver;
+        private readonly DnlibDefSpecificNamespaceCriticalAnalyzer m_DnlibDefSpecificNamespaceHavingCriticalAnalyzer;
         private readonly DnlibDefCriticalAnalyzer m_DnlibDefCriticalAnalyzer;
         private readonly ILogger m_Logger;
 
         public CallToCalli(
             IInjector injector,
             IRenamer renamer,
-            IDnlibDefFeatureObfuscationAttributeHavingResolver dnlibDefFeatureObfuscationAttributeHavingResolver,
-            DnlibDefSpecificNamespaceHavingCriticalAnalyzer dnlibDefSpecificNamespaceHavingCriticalAnalyzer,
+            IDnlibDefObfuscationAttributeResolver dnlibDefFeatureObfuscationAttributeHavingResolver,
+            DnlibDefSpecificNamespaceCriticalAnalyzer dnlibDefSpecificNamespaceHavingCriticalAnalyzer,
             DnlibDefCriticalAnalyzer dnlibDefCriticalAnalyzer,
             ILogger logger)
         {
@@ -45,12 +46,8 @@ namespace BitMono.Protections
 
         public PipelineStages Stage => PipelineStages.ModuleWritten;
 
-        public Task ExecuteAsync(ProtectionContext context, CancellationToken cancellationToken = default)
+        public Task ExecuteAsync(ProtectionContext context, ProtectionParameters parameters, CancellationToken cancellationToken = default)
         {
-            var moduleDefMD = ModuleDefMD.Load(context.BitMonoContext.OutputModuleFile, context.ModuleCreationOptions);
-            context.ModuleDefMD = moduleDefMD;
-            context.Importer = new Importer(moduleDefMD);
-
             var runtimeMethodHandle = context.Importer.Import(typeof(RuntimeMethodHandle));
             var getTypeFromHandleMethod = context.Importer.Import(typeof(Type).GetMethod(nameof(Type.GetTypeFromHandle), new Type[]
             {
@@ -64,45 +61,21 @@ namespace BitMono.Protections
             var getMethodHandleMethod = context.Importer.Import(typeof(MethodBase).GetProperty(nameof(MethodBase.MethodHandle)).GetMethod);
             var getFunctionPointerMethod = context.Importer.Import(typeof(RuntimeMethodHandle).GetMethod(nameof(RuntimeMethodHandle.GetFunctionPointer)));
 
-            foreach (var typeDef in context.ModuleDefMD.GetTypes().ToArray())
+            foreach (var typeDef in parameters.Targets.OfType<TypeDef>())
             {
-                if (m_DnlibDefFeatureObfuscationAttributeHavingResolver.Resolve<CallToCalli>(typeDef))
-                {
-                    m_Logger.Information("Found {0}, skipping.", nameof(ObfuscationAttribute));
-                    continue;
-                }
-                if (m_DnlibDefSpecificNamespaceHavingCriticalAnalyzer.NotCriticalToMakeChanges(typeDef) == false)
-                {
-                    m_Logger.Information("Not able to make changes because of specific namespace was found, skipping.");
-                    continue;
-                }
-
-                foreach (var methodDef in typeDef.Methods.ToArray())
+                foreach (var methodDef in typeDef.Methods)
                 {
                     if (methodDef.HasBody && methodDef.Body.HasInstructions
-                        && methodDef.DeclaringType.IsGlobalModuleType == false
                         && methodDef.IsConstructor == false
-                        && methodDef.NotGetterAndSetter()
                         && m_DnlibDefCriticalAnalyzer.NotCriticalToMakeChanges(methodDef))
                     {
-                        if (m_DnlibDefFeatureObfuscationAttributeHavingResolver.Resolve<CallToCalli>(methodDef))
-                        {
-                            m_Logger.Information("Found {0}, skipping.", nameof(ObfuscationAttribute));
-                            continue;
-                        }
-                        if (m_DnlibDefSpecificNamespaceHavingCriticalAnalyzer.NotCriticalToMakeChanges(methodDef) == false)
-                        {
-                            m_Logger.Information("Not able to make changes because of specific namespace was found, skipping.");
-                            continue;
-                        }
-
-                        for (int i = 0; i < methodDef.Body.Instructions.Count; i++)
+                        for (var i = 0; i < methodDef.Body.Instructions.Count; i++)
                         {
                             if (methodDef.Body.Instructions[i].OpCode == OpCodes.Call)
                             {
                                 if (methodDef.Body.Instructions[i].Operand is MethodDef callingMethodDef && callingMethodDef.HasBody)
                                 {
-                                    var local = methodDef.Body.Variables.Add(new Local(new ValueTypeSig(runtimeMethodHandle)));
+                                    var runtimeMethodHandleLocal = methodDef.Body.Variables.Add(new Local(new ValueTypeSig(runtimeMethodHandle)));
                                     if (methodDef.Body.HasExceptionHandlers == false)
                                     {
                                         methodDef.Body.Instructions[i].OpCode = OpCodes.Nop;
@@ -112,8 +85,8 @@ namespace BitMono.Protections
                                         methodDef.Body.Instructions.Insert(i + 4, new Instruction(OpCodes.Ldc_I4, callingMethodDef.MDToken.ToInt32()));
                                         methodDef.Body.Instructions.Insert(i + 5, new Instruction(OpCodes.Call, resolveMethodMethod));
                                         methodDef.Body.Instructions.Insert(i + 6, new Instruction(OpCodes.Callvirt, getMethodHandleMethod));
-                                        methodDef.Body.Instructions.Insert(i + 7, new Instruction(OpCodes.Stloc, local));
-                                        methodDef.Body.Instructions.Insert(i + 8, new Instruction(OpCodes.Ldloca, local));
+                                        methodDef.Body.Instructions.Insert(i + 7, new Instruction(OpCodes.Stloc, runtimeMethodHandleLocal));
+                                        methodDef.Body.Instructions.Insert(i + 8, new Instruction(OpCodes.Ldloca, runtimeMethodHandleLocal));
                                         methodDef.Body.Instructions.Insert(i + 9, new Instruction(OpCodes.Call, getFunctionPointerMethod));
                                         methodDef.Body.Instructions.Insert(i + 10, new Instruction(OpCodes.Calli, callingMethodDef.MethodSig));
                                         i += 10;
@@ -123,12 +96,6 @@ namespace BitMono.Protections
                         }
                     }
                 }
-            }
-
-            using (context.ModuleDefMD)
-            using (var fileStream = File.Open(context.BitMonoContext.OutputModuleFile, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite))
-            {
-                context.ModuleDefMD.Write(fileStream, context.ModuleWriterOptions);
             }
             return Task.CompletedTask;
         }
