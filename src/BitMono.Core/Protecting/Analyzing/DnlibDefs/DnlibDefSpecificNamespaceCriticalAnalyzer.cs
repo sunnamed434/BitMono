84f55733369e7ce1af8f97d401c9889726b32d94
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-13 12:35:20 +0200

    Create DnlibDefSpecificNamespaceCriticalAnalyzer.cs

---
+++ src/BitMono.Core/Protecting/Analyzing/DnlibDefs/DnlibDefSpecificNamespaceCriticalAnalyzer.cs
@@ -0,0 +1,53 @@
+﻿using BitMono.API.Configuration;
+using BitMono.API.Protecting.Analyzing;
+using BitMono.Core.Extensions.Configuration;
+using BitMono.Shared.Models;
+using BitMono.Utilities.Extensions.dnlib;
+using dnlib.DotNet;
+using Microsoft.Extensions.Configuration;
+using System.Linq;
+
+namespace BitMono.Core.Protecting.Analyzing.DnlibDefs
+{
+    public class DnlibDefSpecificNamespaceCriticalAnalyzer : ICriticalAnalyzer<IDnlibDef>
+    {
+        private readonly IConfiguration m_Configuration;
+
+        public DnlibDefSpecificNamespaceCriticalAnalyzer(IBitMonoObfuscationConfiguration configuration)
+        {
+            m_Configuration = configuration.Configuration;
+        }
+
+        public bool NotCriticalToMakeChanges(IDnlibDef dnlibDef)
+        {
+            if (m_Configuration.GetValue<bool>(nameof(Obfuscation.SpecificNamespacesObfuscationOnly)) == false)
+            {
+                return true;
+            }
+
+            var specificNamespaces = m_Configuration.GetSpecificNamespaces();
+            if (dnlibDef is TypeDef typeDef && typeDef.HasNamespace()) 
+            {
+                if (specificNamespaces.Any(s => s.Equals(typeDef.Namespace.String)) == false)
+                {
+                    return false;
+                }
+            }
+            if (dnlibDef is MethodDef methodDef && methodDef.DeclaringType.HasNamespace())
+            {
+                if (specificNamespaces.Any(s => s.Equals(methodDef.DeclaringType.Namespace.String)) == false)
+                {
+                    return false;
+                }
+            }
+            if (dnlibDef is FieldDef fieldDef && fieldDef.DeclaringType.HasNamespace())
+            {
+                if (specificNamespaces.Any(s => s.Equals(fieldDef.DeclaringType.Namespace.String)) == false)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
