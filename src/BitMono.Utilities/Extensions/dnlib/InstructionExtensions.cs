7c4d3161624d4fe9c8911f633ab8bb3cad0afa70
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-15 17:46:08 +0200

    Create InstructionExtensions.cs

---
+++ src/BitMono.Utilities/Extensions/dnlib/InstructionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using dnlib.DotNet.Emit;
+
+namespace BitMono.Utilities.Extensions.dnlib;
+
+public static class InstructionExtensions
+{
+    public static Instruction ReplaceWith(this Instruction source, OpCode opCode, object operand)
+    {
+        source.OpCode = opCode;
+        source.Operand = operand;
+        return source;
+    }
+}
