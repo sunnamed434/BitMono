de79d1eff253d2356ce40e981cbd87ba5f6b50c9
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-15 17:46:47 +0200

    Update AssemblyInfo.cs

--- src/BitMono.Runtime/AssemblyInfo.cs
+++ src/BitMono.Runtime/AssemblyInfo.cs
@@ -1,5 +1,3 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("BitMono.CLI", AllInternalsVisible = true)]
+﻿[assembly: InternalsVisibleTo("BitMono.CLI", AllInternalsVisible = true)]
 [assembly: InternalsVisibleTo("BitMono.GUI", AllInternalsVisible = true)]
 [assembly: InternalsVisibleTo("BitMono.Protections", AllInternalsVisible = true)]
