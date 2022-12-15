734a029cc48c3032d99d921e00797b7c9bb270f0
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-15 21:29:02 +0200

    Update InstructionExtensions.cs

--- src/BitMono.Utilities/Extensions/dnlib/InstructionExtensions.cs
+++ src/BitMono.Utilities/Extensions/dnlib/InstructionExtensions.cs
@@ -10,4 +10,13 @@ public static class InstructionExtensions
         source.Operand = operand;
         return source;
     }
+    public static int CountWithoutRet(this IList<Instruction> source)
+    {
+        const int InstructionCount = 1;
+        if (source[source.Count - InstructionCount].OpCode == OpCodes.Ret) 
+        {
+            return source.Count - InstructionCount;
+        }
+        throw new IndexOutOfRangeException();
+    }
 }
