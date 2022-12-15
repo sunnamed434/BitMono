0bfa286455ef8c12918a1f24384191d59c072f7a
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-15 19:12:28 +0200

    Update MethodImplAttributeExcludingResolver.cs

--- src/BitMono.Core/Protecting/Resolvers/MethodImplAttributeExcludingResolver.cs
+++ src/BitMono.Core/Protecting/Resolvers/MethodImplAttributeExcludingResolver.cs
@@ -1,44 +1,35 @@
-﻿using BitMono.API.Configuration;
-using BitMono.API.Protecting.Resolvers;
-using BitMono.Shared.Models;
-using dnlib.DotNet;
-using Microsoft.Extensions.Configuration;
-using NullGuard;
-using System.Runtime.CompilerServices;
+﻿namespace BitMono.Core.Protecting.Resolvers;
 
-namespace BitMono.Core.Protecting.Resolvers
+public class MethodImplAttributeExcludingResolver : IMethodImplAttributeExcludingResolver
 {
-    public class MethodImplAttributeExcludingResolver : IMethodImplAttributeExcludingResolver
+    private readonly IAttemptAttributeResolver m_AttemptAttributeResolver;
+    private readonly IConfiguration m_Configuration;
+
+    public MethodImplAttributeExcludingResolver(IAttemptAttributeResolver attemptAttributeResolver, IBitMonoObfuscationConfiguration configuration)
     {
-        private readonly IAttemptAttributeResolver m_AttemptAttributeResolver;
-        private readonly IConfiguration m_Configuration;
+        m_AttemptAttributeResolver = attemptAttributeResolver;
+        m_Configuration = configuration.Configuration;
+    }
 
-        public MethodImplAttributeExcludingResolver(IAttemptAttributeResolver attemptAttributeResolver, IBitMonoObfuscationConfiguration configuration)
+    public bool TryResolve(IHasCustomAttribute from, [AllowNull] out MethodImplAttribute methodImplAttribute)
+    {
+        methodImplAttribute = null;
+        if (m_Configuration.GetValue<bool>(nameof(Obfuscation.NoInliningMethodObfuscationExcluding)) == false)
         {
-            m_AttemptAttributeResolver = attemptAttributeResolver;
-            m_Configuration = configuration.Configuration;
+            return false;
         }
-
-        public bool TryResolve(IHasCustomAttribute from, [AllowNull] out MethodImplAttribute methodImplAttribute)
+        if (m_AttemptAttributeResolver.TryResolve(from, null, null, out methodImplAttribute) == false)
         {
-            methodImplAttribute = null;
-            if (m_Configuration.GetValue<bool>(nameof(Obfuscation.NoInliningMethodObfuscationExcluding)) == false)
-            {
-                return false;
-            }
-            if (m_AttemptAttributeResolver.TryResolve(from, null, null, out methodImplAttribute) == false)
-            {
-                return false;
-            }
-            if (methodImplAttribute == null)
-            {
-                return false;
-            }
-            if (methodImplAttribute.Value == MethodImplOptions.NoInlining)
-            {
-                return false;
-            }
-            return true;
+            return false;
         }
+        if (methodImplAttribute == null)
+        {
+            return false;
+        }
+        if (methodImplAttribute.Value == MethodImplOptions.NoInlining)
+        {
+            return false;
+        }
+        return true;
     }
 }
