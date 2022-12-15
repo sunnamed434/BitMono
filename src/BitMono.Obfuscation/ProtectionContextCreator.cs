2106fd91cdac374604f0e684a6e860cdb63587fd
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-15 17:47:43 +0200

    Update ProtectionContextCreator.cs

--- src/BitMono.Obfuscation/ProtectionContextCreator.cs
+++ src/BitMono.Obfuscation/ProtectionContextCreator.cs
@@ -1,36 +1,32 @@
-﻿using BitMono.API.Protecting.Contexts;
-using dnlib.DotNet;
+﻿namespace BitMono.Obfuscation;
 
-namespace BitMono.Obfuscation
+public class ProtectionContextCreator
 {
-    public class ProtectionContextCreator
+    private readonly ModuleCreationResult m_ModuleDefMDCreationResult;
+    private readonly ModuleDefMD m_RuntimeModuleDefMD;
+    private readonly BitMonoContext m_Context;
+
+    public ProtectionContextCreator(
+        ModuleCreationResult moduleDefMDCreationResult, 
+        ModuleDefMD externalComponentsModuleDefMD, 
+        BitMonoContext context)
     {
-        private readonly ModuleDefMDCreationResult m_ModuleDefMDCreationResult;
-        private readonly ModuleDefMD m_RuntimeModuleDefMD;
-        private readonly BitMonoContext m_Context;
+        m_ModuleDefMDCreationResult = moduleDefMDCreationResult;
+        m_RuntimeModuleDefMD = externalComponentsModuleDefMD;
+        m_Context = context;
+    }
 
-        public ProtectionContextCreator(
-            ModuleDefMDCreationResult moduleDefMDCreationResult, 
-            ModuleDefMD externalComponentsModuleDefMD, 
-            BitMonoContext context)
+    public ProtectionContext Create()
+    {
+        return new ProtectionContext
         {
-            m_ModuleDefMDCreationResult = moduleDefMDCreationResult;
-            m_RuntimeModuleDefMD = externalComponentsModuleDefMD;
-            m_Context = context;
-        }
-
-        public ProtectionContext Create()
-        {
-            return new ProtectionContext
-            {
-                ModuleDefMD = m_ModuleDefMDCreationResult.ModuleDefMD,
-                ModuleCreationOptions = m_ModuleDefMDCreationResult.ModuleCreationOptions,
-                ModuleWriterOptions = m_ModuleDefMDCreationResult.ModuleWriterOptions,
-                RuntimeModuleDefMD = m_RuntimeModuleDefMD,
-                Importer = new Importer(m_ModuleDefMDCreationResult.ModuleDefMD),
-                RuntimeImporter = new Importer(m_RuntimeModuleDefMD, ImporterOptions.TryToUseDefs),
-                BitMonoContext = m_Context,
-            };
-        }
+            ModuleDefMD = m_ModuleDefMDCreationResult.ModuleDefMD,
+            ModuleCreationOptions = m_ModuleDefMDCreationResult.ModuleCreationOptions,
+            ModuleWriterOptions = m_ModuleDefMDCreationResult.ModuleWriterOptions,
+            RuntimeModuleDefMD = m_RuntimeModuleDefMD,
+            Importer = new Importer(m_ModuleDefMDCreationResult.ModuleDefMD),
+            RuntimeImporter = new Importer(m_RuntimeModuleDefMD, ImporterOptions.TryToUseDefs),
+            BitMonoContext = m_Context,
+        };
     }
 }
