143fc27d22fa8bfa05a4152aa4d363f3992b391b
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-15 19:12:34 +0200

    Update CustomAttributesResolver.cs

--- src/BitMono.Core/Protecting/Resolvers/CustomAttributesResolver.cs
+++ src/BitMono.Core/Protecting/Resolvers/CustomAttributesResolver.cs
@@ -1,43 +1,36 @@
-﻿using BitMono.API.Protecting.Resolvers;
-using dnlib.DotNet;
-using NullGuard;
-using System;
-using System.Collections.Generic;
+﻿namespace BitMono.Core.Protecting.Resolvers;
 
-namespace BitMono.Core.Protecting.Resolvers
+public class CustomAttributesResolver : ICustomAttributesResolver
 {
-    public class CustomAttributesResolver : ICustomAttributesResolver
+    [return: AllowNull]
+    public IEnumerable<TAttribute> Resolve<TAttribute>(IHasCustomAttribute from, [AllowNull] Func<TAttribute, bool> strip)
+        where TAttribute : Attribute
     {
-        [return: AllowNull]
-        public IEnumerable<TAttribute> Resolve<TAttribute>(IHasCustomAttribute from, [AllowNull] Func<TAttribute, bool> strip)
-            where TAttribute : Attribute
+        for (int i = 0; i < from.CustomAttributes.Count; i++)
         {
-            for (int i = 0; i < from.CustomAttributes.Count; i++)
+            var attribute = Activator.CreateInstance<TAttribute>();
+            var customAttribute = from.CustomAttributes[i];
+            foreach (var customAttributeProperty in customAttribute.Properties)
             {
-                var attribute = Activator.CreateInstance<TAttribute>();
-                var customAttribute = from.CustomAttributes[i];
-                foreach (var customAttributeProperty in customAttribute.Properties)
+                if (customAttribute.TypeFullName.Equals(typeof(TAttribute).FullName))
                 {
-                    if (customAttribute.TypeFullName.Equals(typeof(TAttribute).FullName))
+                    var propertyInfo = typeof(TAttribute).GetProperty(customAttributeProperty.Name);
+                    if (customAttributeProperty.Value is UTF8String utf8String)
                     {
-                        var propertyInfo = typeof(TAttribute).GetProperty(customAttributeProperty.Name);
-                        if (customAttributeProperty.Value is UTF8String utf8String)
-                        {
-                            propertyInfo.SetValue(attribute, utf8String.String);
-                        }
-                        else
-                        {
-                            propertyInfo.SetValue(attribute, customAttributeProperty.Value);
-                        }
+                        propertyInfo.SetValue(attribute, utf8String.String);
+                    }
+                    else
+                    {
+                        propertyInfo.SetValue(attribute, customAttributeProperty.Value);
                     }
                 }
-
-                if (strip?.Invoke(attribute) == true)
-                {
-                    from.CustomAttributes.RemoveAt(i);
-                }
-                yield return attribute;
             }
+
+            if (strip?.Invoke(attribute) == true)
+            {
+                from.CustomAttributes.RemoveAt(i);
+            }
+            yield return attribute;
         }
     }
 }
