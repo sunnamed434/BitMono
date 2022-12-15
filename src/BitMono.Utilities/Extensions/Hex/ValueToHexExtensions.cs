b22c02662ca06e09e292269a6b2a86244d8ebd67
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-15 17:45:56 +0200

    Update ValueToHexExtensions.cs

--- src/BitMono.Utilities/Extensions/Hex/ValueToHexExtensions.cs
+++ src/BitMono.Utilities/Extensions/Hex/ValueToHexExtensions.cs
@@ -1,14 +1,13 @@
-﻿namespace BitMono.Utilities.Extensions.Hex
+﻿namespace BitMono.Utilities.Extensions.Hex;
+
+public static class ValueToHexExtensions
 {
-    public static class ValueToHexExtensions
+    public static string ToHexString(this int source)
     {
-        public static string ToHexString(this int source)
-        {
-            return string.Format("0x{0:X}", source);
-        }
-        public static string ToHexString(this long source)
-        {
-            return string.Format("0x{0:X}", source);
-        }
+        return string.Format("0x{0:X}", source);
+    }
+    public static string ToHexString(this long source)
+    {
+        return string.Format("0x{0:X}", source);
     }
 }
