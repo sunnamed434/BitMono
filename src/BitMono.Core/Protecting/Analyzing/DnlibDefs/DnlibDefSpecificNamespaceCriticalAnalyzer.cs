69dd3dbcb22ca89de256842d33db8cd0bf48cd93
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-15 19:09:25 +0200

    Update DnlibDefSpecificNamespaceCriticalAnalyzer.cs

--- src/BitMono.Core/Protecting/Analyzing/DnlibDefs/DnlibDefSpecificNamespaceCriticalAnalyzer.cs
+++ src/BitMono.Core/Protecting/Analyzing/DnlibDefs/DnlibDefSpecificNamespaceCriticalAnalyzer.cs
@@ -1,53 +1,43 @@
-﻿using BitMono.API.Configuration;
-using BitMono.API.Protecting.Analyzing;
-using BitMono.Core.Extensions.Configuration;
-using BitMono.Shared.Models;
-using BitMono.Utilities.Extensions.dnlib;
-using dnlib.DotNet;
-using Microsoft.Extensions.Configuration;
-using System.Linq;
+﻿namespace BitMono.Core.Protecting.Analyzing.DnlibDefs;
 
-namespace BitMono.Core.Protecting.Analyzing.DnlibDefs
+public class DnlibDefSpecificNamespaceCriticalAnalyzer : ICriticalAnalyzer<IDnlibDef>
 {
-    public class DnlibDefSpecificNamespaceCriticalAnalyzer : ICriticalAnalyzer<IDnlibDef>
+    private readonly IConfiguration m_Configuration;
+
+    public DnlibDefSpecificNamespaceCriticalAnalyzer(IBitMonoObfuscationConfiguration configuration)
     {
-        private readonly IConfiguration m_Configuration;
+        m_Configuration = configuration.Configuration;
+    }
 
-        public DnlibDefSpecificNamespaceCriticalAnalyzer(IBitMonoObfuscationConfiguration configuration)
+    public bool NotCriticalToMakeChanges(IDnlibDef dnlibDef)
+    {
+        if (m_Configuration.GetValue<bool>(nameof(Obfuscation.SpecificNamespacesObfuscationOnly)) == false)
         {
-            m_Configuration = configuration.Configuration;
-        }
-
-        public bool NotCriticalToMakeChanges(IDnlibDef dnlibDef)
-        {
-            if (m_Configuration.GetValue<bool>(nameof(Obfuscation.SpecificNamespacesObfuscationOnly)) == false)
-            {
-                return true;
-            }
-
-            var specificNamespaces = m_Configuration.GetSpecificNamespaces();
-            if (dnlibDef is TypeDef typeDef && typeDef.HasNamespace()) 
-            {
-                if (specificNamespaces.Any(s => s.Equals(typeDef.Namespace.String)) == false)
-                {
-                    return false;
-                }
-            }
-            if (dnlibDef is MethodDef methodDef && methodDef.DeclaringType.HasNamespace())
-            {
-                if (specificNamespaces.Any(s => s.Equals(methodDef.DeclaringType.Namespace.String)) == false)
-                {
-                    return false;
-                }
-            }
-            if (dnlibDef is FieldDef fieldDef && fieldDef.DeclaringType.HasNamespace())
-            {
-                if (specificNamespaces.Any(s => s.Equals(fieldDef.DeclaringType.Namespace.String)) == false)
-                {
-                    return false;
-                }
-            }
             return true;
         }
+
+        var specificNamespaces = m_Configuration.GetSpecificNamespaces();
+        if (dnlibDef is TypeDef typeDef && typeDef.HasNamespace()) 
+        {
+            if (specificNamespaces.Any(s => s.Equals(typeDef.Namespace.String)) == false)
+            {
+                return false;
+            }
+        }
+        if (dnlibDef is MethodDef methodDef && methodDef.DeclaringType.HasNamespace())
+        {
+            if (specificNamespaces.Any(s => s.Equals(methodDef.DeclaringType.Namespace.String)) == false)
+            {
+                return false;
+            }
+        }
+        if (dnlibDef is FieldDef fieldDef && fieldDef.DeclaringType.HasNamespace())
+        {
+            if (specificNamespaces.Any(s => s.Equals(fieldDef.DeclaringType.Namespace.String)) == false)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
