82dffd42986549fb870efce5eb2247eb425f5556
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-15 19:12:19 +0200

    Update MethodDefsSearcher.cs

--- src/BitMono.Core/Protecting/Injection/MethodDefs/MethodDefsSearcher.cs
+++ src/BitMono.Core/Protecting/Injection/MethodDefs/MethodDefsSearcher.cs
@@ -1,25 +1,20 @@
-﻿using BitMono.API.Protecting.Injection.MethodDefs;
-using dnlib.DotNet;
-using NullGuard;
+﻿namespace BitMono.Core.Protecting.Injection.MethodDefs;
 
-namespace BitMono.Core.Protecting.Injection.MethodDefs
+public class MethodDefsSearcher : IMethodDefSearcher
 {
-    public class MethodDefsSearcher : IMethodDefSearcher
+    [return: AllowNull]
+    public MethodDef Find(string name, ModuleDefMD moduleDefMD)
     {
-        [return: AllowNull]
-        public MethodDef Find(string name, ModuleDefMD moduleDefMD)
+        foreach (var typeDef in moduleDefMD.GetTypes())
         {
-            foreach (var typeDef in moduleDefMD.GetTypes())
+            foreach (var methodDef in typeDef.Methods)
             {
-                foreach (var methodDef in typeDef.Methods)
+                if (methodDef.Name.Equals(name))
                 {
-                    if (methodDef.Name.Equals(name))
-                    {
-                        return methodDef;
-                    }
+                    return methodDef;
                 }
             }
-            return null;
         }
+        return null;
     }
 }
