a875b0a17e1be11f6bc3880dc25072d54880c225
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-15 17:47:02 +0200

    Update FullRenamer.cs

--- src/BitMono.Protections/FullRenamer.cs
+++ src/BitMono.Protections/FullRenamer.cs
@@ -1,76 +1,61 @@
-﻿using BitMono.API.Protecting;
-using BitMono.API.Protecting.Contexts;
-using BitMono.API.Protecting.Renaming;
-using BitMono.Core.Protecting;
-using BitMono.Core.Protecting.Analyzing.DnlibDefs;
-using BitMono.Core.Protecting.Analyzing.TypeDefs;
-using BitMono.Core.Protecting.Attributes;
-using BitMono.Utilities.Extensions.dnlib;
-using dnlib.DotNet;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using ILogger = Serilog.ILogger;
+﻿namespace BitMono.Protections;
 
-namespace BitMono.Protections
+[ProtectionName(nameof(FullRenamer))]
+public class FullRenamer : IProtection
 {
-    [ProtectionName(nameof(FullRenamer))]
-    public class FullRenamer : IProtection
+    private readonly DnlibDefCriticalAnalyzer m_DnlibDefCriticalAnalyzer;
+    private readonly TypeDefModelCriticalAnalyzer m_TypeDefModelCriticalAnalyzer;
+    private readonly IRenamer m_Renamer;
+    private readonly ILogger m_Logger;
+
+    public FullRenamer(
+        DnlibDefCriticalAnalyzer dnlibDefCriticalAnalyzer,
+        TypeDefModelCriticalAnalyzer typeDefModelCriticalAnalyzer,
+        IRenamer renamer,
+        ILogger logger)
     {
-        private readonly DnlibDefCriticalAnalyzer m_DnlibDefCriticalAnalyzer;
-        private readonly TypeDefModelCriticalAnalyzer m_TypeDefModelCriticalAnalyzer;
-        private readonly IRenamer m_Renamer;
-        private readonly ILogger m_Logger;
+        m_DnlibDefCriticalAnalyzer = dnlibDefCriticalAnalyzer;
+        m_TypeDefModelCriticalAnalyzer = typeDefModelCriticalAnalyzer;
+        m_Renamer = renamer;
+        m_Logger = logger.ForContext<FullRenamer>();
+    }
 
-        public FullRenamer(
-            DnlibDefCriticalAnalyzer dnlibDefCriticalAnalyzer,
-            TypeDefModelCriticalAnalyzer typeDefModelCriticalAnalyzer,
-            IRenamer renamer,
-            ILogger logger)
+    public Task ExecuteAsync(ProtectionContext context, ProtectionParameters parameters, CancellationToken cancellationToken = default)
+    {
+        foreach (var typeDef in parameters.Targets.OfType<TypeDef>())
         {
-            m_DnlibDefCriticalAnalyzer = dnlibDefCriticalAnalyzer;
-            m_TypeDefModelCriticalAnalyzer = typeDefModelCriticalAnalyzer;
-            m_Renamer = renamer;
-            m_Logger = logger.ForContext<FullRenamer>();
-        }
-
-        public Task ExecuteAsync(ProtectionContext context, ProtectionParameters parameters, CancellationToken cancellationToken = default)
-        {
-            foreach (var typeDef in parameters.Targets.OfType<TypeDef>())
+            if (typeDef.IsGlobalModuleType == false
+                && m_DnlibDefCriticalAnalyzer.NotCriticalToMakeChanges(typeDef)
+                && m_TypeDefModelCriticalAnalyzer.NotCriticalToMakeChanges(typeDef))
             {
-                if (typeDef.IsGlobalModuleType == false
-                    && m_DnlibDefCriticalAnalyzer.NotCriticalToMakeChanges(typeDef)
-                    && m_TypeDefModelCriticalAnalyzer.NotCriticalToMakeChanges(typeDef))
+                m_Renamer.Rename(typeDef);
+
+                foreach (var fieldDef in typeDef.Fields.ToArray())
                 {
-                    m_Renamer.Rename(typeDef);
-
-                    foreach (var fieldDef in typeDef.Fields.ToArray())
+                    if (m_DnlibDefCriticalAnalyzer.NotCriticalToMakeChanges(fieldDef))
                     {
-                        if (m_DnlibDefCriticalAnalyzer.NotCriticalToMakeChanges(fieldDef))
-                        {
-                            m_Renamer.Rename(fieldDef);
-                        }
+                        m_Renamer.Rename(fieldDef);
                     }
+                }
 
-                    foreach (var methodDef in typeDef.Methods.ToArray())
+                foreach (var methodDef in typeDef.Methods.ToArray())
+                {
+                    if (methodDef.IsConstructor == false
+                        && methodDef.IsVirtual == false
+                        && m_DnlibDefCriticalAnalyzer.NotCriticalToMakeChanges(methodDef))
                     {
-                        if (methodDef.IsConstructor == false
-                            && methodDef.IsVirtual == false
-                            && m_DnlibDefCriticalAnalyzer.NotCriticalToMakeChanges(methodDef))
+                        m_Renamer.Rename(methodDef);
+                        if (methodDef.HasParameters())
                         {
-                            m_Renamer.Rename(methodDef);
-                            if (methodDef.HasParameters())
+                            foreach (var parameter in methodDef.Parameters.ToArray())
                             {
-                                foreach (var parameter in methodDef.Parameters.ToArray())
-                                {
-                                    m_Renamer.Rename(parameter);
-                                }
+                                m_Renamer.Rename(parameter);
                             }
                         }
                     }
                 }
             }
-            return Task.CompletedTask;
         }
+        return Task.CompletedTask;
     }
 }
