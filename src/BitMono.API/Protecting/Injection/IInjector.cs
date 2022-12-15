cb775b0e7bb45b026257c087be4e8f16b2caaed6
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-15 17:45:24 +0200

    Update IInjector.cs

--- src/BitMono.API/Protecting/Injection/IInjector.cs
+++ src/BitMono.API/Protecting/Injection/IInjector.cs
@@ -1,15 +1,12 @@
-﻿using dnlib.DotNet;
+﻿namespace BitMono.API.Protecting.Injection;
 
-namespace BitMono.API.Protecting.Injection
+public interface IInjector
 {
-    public interface IInjector
-    {
-        FieldDef InjectInvisibleArray(ModuleDefMD moduleDefMD, TypeDef typeDef, byte[] data, string name);
-        TypeDef CreateInvisibleType(ModuleDefMD moduleDefMD, string name = null);
-        TypeDef CreateInvisibleValueType(ModuleDefMD moduleDefMD, string name = null);
-        TypeDef InjectInvisibleValueType(ModuleDefMD moduleDefMD, TypeDef typeDef, string name = null);
-        CustomAttribute InjectCompilerGeneratedAttribute(ModuleDefMD moduleDefMD, TypeDef typeDef = null);
-        CustomAttribute InjectAttributeWithContent(ModuleDefMD moduleDefMD, string @namespace, string @name, string text);
-        CustomAttribute InjectAttribute(ModuleDefMD moduleDefMD, string @namespace, string @name);
-    }
+    FieldDef InjectInvisibleArray(ModuleDefMD moduleDefMD, TypeDef typeDef, byte[] data, string name);
+    TypeDef CreateInvisibleType(ModuleDefMD moduleDefMD, string name = null);
+    TypeDef CreateInvisibleValueType(ModuleDefMD moduleDefMD, string name = null);
+    TypeDef InjectInvisibleValueType(ModuleDefMD moduleDefMD, TypeDef typeDef, string name = null);
+    CustomAttribute InjectCompilerGeneratedAttribute(ModuleDefMD moduleDefMD, TypeDef typeDef = null);
+    CustomAttribute InjectAttributeWithContent(ModuleDefMD moduleDefMD, string @namespace, string @name, string text);
+    CustomAttribute InjectAttribute(ModuleDefMD moduleDefMD, string @namespace, string @name);
 }
