4ecaf291a6cdc2cda08c50e902a2f72f3db2f391
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-15 17:47:40 +0200

    Update ProtectionParametersCreator.cs

--- src/BitMono.Obfuscation/ProtectionParametersCreator.cs
+++ src/BitMono.Obfuscation/ProtectionParametersCreator.cs
@@ -1,28 +1,20 @@
-﻿using BitMono.API.Protecting.Resolvers;
-using BitMono.Core.Protecting;
-using BitMono.Utilities.Extensions.dnlib;
-using dnlib.DotNet;
-using System.Collections.Generic;
-using System.Linq;
+﻿namespace BitMono.Obfuscation;
 
-namespace BitMono.Obfuscation
+public class ProtectionParametersCreator
 {
-    public class ProtectionParametersCreator
+    private readonly DnlibDefsResolver m_DnlibDefsResolver;
+    private readonly IEnumerable<IDnlibDefResolver> m_Resolvers;
+
+    public ProtectionParametersCreator(DnlibDefsResolver dnlibDefsResolver, IEnumerable<IDnlibDefResolver> resolvers)
     {
-        private readonly DnlibDefsResolver m_DnlibDefsResolver;
-        private readonly IEnumerable<IDnlibDefResolver> m_Resolvers;
+        m_DnlibDefsResolver = dnlibDefsResolver;
+        m_Resolvers = resolvers;
+    }
 
-        public ProtectionParametersCreator(DnlibDefsResolver dnlibDefsResolver, IEnumerable<IDnlibDefResolver> resolvers)
-        {
-            m_DnlibDefsResolver = dnlibDefsResolver;
-            m_Resolvers = resolvers;
-        }
-
-        public ProtectionParameters Create(string feature, ModuleDefMD moduleDefMD)
-        {
-            var definitions = moduleDefMD.FindDefinitions();
-            var targets = m_DnlibDefsResolver.Resolve(feature, definitions, m_Resolvers).ToList();
-            return new ProtectionParameters(targets);
-        }
+    public ProtectionParameters Create(string feature, ModuleDefMD moduleDefMD)
+    {
+        var definitions = moduleDefMD.FindDefinitions();
+        var targets = m_DnlibDefsResolver.Resolve(feature, definitions, m_Resolvers).ToList();
+        return new ProtectionParameters(targets);
     }
 }
