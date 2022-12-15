084c0660ca6e5e76c221544f0c2bac532b1bd3a0
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-15 17:45:44 +0200

    Update IRenamer.cs

--- src/BitMono.API/Protecting/Renaming/IRenamer.cs
+++ src/BitMono.API/Protecting/Renaming/IRenamer.cs
@@ -1,14 +1,11 @@
-﻿using dnlib.DotNet;
+﻿namespace BitMono.API.Protecting.Renaming;
 
-namespace BitMono.API.Protecting.Renaming
+public interface IRenamer
 {
-    public interface IRenamer
-    {
-        string RenameUnsafely();
-        void Rename(IDnlibDef dnlibDef);
-        void Rename(params IDnlibDef[] dnlibDefs);
-        void Rename(IFullName fullName);
-        void Rename(params IFullName[] fullNames);
-        void Rename(IVariable variable);
-    }
+    string RenameUnsafely();
+    void Rename(IDnlibDef dnlibDef);
+    void Rename(params IDnlibDef[] dnlibDefs);
+    void Rename(IFullName fullName);
+    void Rename(params IFullName[] fullNames);
+    void Rename(IVariable variable);
 }
