7bc1f6192d6c9345f398cfd28b34e2af71850893
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-15 17:45:34 +0200

    Update IProtection.cs

--- src/BitMono.API/Protecting/IProtection.cs
+++ src/BitMono.API/Protecting/IProtection.cs
@@ -1,12 +1,6 @@
-﻿using BitMono.API.Protecting.Contexts;
-using BitMono.Core.Protecting;
-using System.Threading;
-using System.Threading.Tasks;
+﻿namespace BitMono.API.Protecting;
 
-namespace BitMono.API.Protecting
+public interface IProtection
 {
-    public interface IProtection
-    {
-        Task ExecuteAsync(ProtectionContext context, ProtectionParameters parameters, CancellationToken cancellationToken = default);
-    }
+    Task ExecuteAsync(ProtectionContext context, ProtectionParameters parameters, CancellationToken cancellationToken = default);
 }
