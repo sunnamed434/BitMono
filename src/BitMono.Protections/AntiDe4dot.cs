ec20272cd87f88a249d948376f342d3838940e77
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-15 17:47:26 +0200

    Update AntiDe4dot.cs

--- src/BitMono.Protections/AntiDe4dot.cs
+++ src/BitMono.Protections/AntiDe4dot.cs
@@ -1,42 +1,33 @@
-﻿using BitMono.API.Protecting;
-using BitMono.API.Protecting.Contexts;
-using BitMono.API.Protecting.Injection;
-using BitMono.Core.Protecting;
-using BitMono.Core.Protecting.Attributes;
-using System.Threading;
-using System.Threading.Tasks;
+﻿namespace BitMono.Protections;
 
-namespace BitMono.Protections
+[ProtectionName(nameof(AntiDe4dot))]
+public class AntiDe4dot : IProtection
 {
-    [ProtectionName(nameof(AntiDe4dot))]
-    public class AntiDe4dot : IProtection
+    private readonly IInjector m_Injector;
+
+    public AntiDe4dot(IInjector injector)
     {
-        private readonly IInjector m_Injector;
+        m_Injector = injector;
+    }
 
-        public AntiDe4dot(IInjector injector)
-        {
-            m_Injector = injector;
-        }
-
-        public Task ExecuteAsync(ProtectionContext context, ProtectionParameters parameters, CancellationToken cancellationToken = default)
-        {
-            m_Injector.InjectAttributeWithContent(context.ModuleDefMD, "SmartAssembly.Attributes", "PoweredBy", string.Empty);
-            m_Injector.InjectAttributeWithContent(context.ModuleDefMD, "Xenocode.Client.Attributes.AssemblyAttributes", "PoweredBy", string.Empty);
-            m_Injector.InjectAttributeWithContent(context.ModuleDefMD, string.Empty, "ObfuscatedByGoliath", string.Empty);
-            m_Injector.InjectAttributeWithContent(context.ModuleDefMD, "SecureTeam.Attributes", "ObfuscatedByAgileDotNet", string.Empty);
-            m_Injector.InjectAttributeWithContent(context.ModuleDefMD, string.Empty, "TrinityObfuscator", string.Empty);
-            m_Injector.InjectAttributeWithContent(context.ModuleDefMD, "SecureTeam.Attributes", "ObfuscatedByCliSecure", string.Empty);
-            m_Injector.InjectAttributeWithContent(context.ModuleDefMD, string.Empty, "ZYXDNGuarder", string.Empty);
-            m_Injector.InjectAttributeWithContent(context.ModuleDefMD, string.Empty, "BabelObfuscator", string.Empty);
-            m_Injector.InjectAttributeWithContent(context.ModuleDefMD, string.Empty, "BabelObfuscator", string.Empty);
-            m_Injector.InjectAttributeWithContent(context.ModuleDefMD, string.Empty, "Dotfuscator", string.Empty);
-            m_Injector.InjectAttributeWithContent(context.ModuleDefMD, string.Empty, "Centos", string.Empty);
-            m_Injector.InjectAttributeWithContent(context.ModuleDefMD, string.Empty, "ConfusedBy", string.Empty);
-            m_Injector.InjectAttributeWithContent(context.ModuleDefMD, "NineRays.Obfuscator", "Evaluation", string.Empty);
-            m_Injector.InjectAttributeWithContent(context.ModuleDefMD, "CryptoObfuscator", "ProtectedWithCryptoObfuscator", string.Empty);
-            m_Injector.InjectAttributeWithContent(context.ModuleDefMD, string.Empty, "();\u0009", string.Empty);
-            m_Injector.InjectAttributeWithContent(context.ModuleDefMD, string.Empty, "EMyPID_8234_", string.Empty);
-            return Task.CompletedTask;
-        }
+    public Task ExecuteAsync(ProtectionContext context, ProtectionParameters parameters, CancellationToken cancellationToken = default)
+    {
+        m_Injector.InjectAttributeWithContent(context.ModuleDefMD, "SmartAssembly.Attributes", "PoweredBy", string.Empty);
+        m_Injector.InjectAttributeWithContent(context.ModuleDefMD, "Xenocode.Client.Attributes.AssemblyAttributes", "PoweredBy", string.Empty);
+        m_Injector.InjectAttributeWithContent(context.ModuleDefMD, string.Empty, "ObfuscatedByGoliath", string.Empty);
+        m_Injector.InjectAttributeWithContent(context.ModuleDefMD, "SecureTeam.Attributes", "ObfuscatedByAgileDotNet", string.Empty);
+        m_Injector.InjectAttributeWithContent(context.ModuleDefMD, string.Empty, "TrinityObfuscator", string.Empty);
+        m_Injector.InjectAttributeWithContent(context.ModuleDefMD, "SecureTeam.Attributes", "ObfuscatedByCliSecure", string.Empty);
+        m_Injector.InjectAttributeWithContent(context.ModuleDefMD, string.Empty, "ZYXDNGuarder", string.Empty);
+        m_Injector.InjectAttributeWithContent(context.ModuleDefMD, string.Empty, "BabelObfuscator", string.Empty);
+        m_Injector.InjectAttributeWithContent(context.ModuleDefMD, string.Empty, "BabelObfuscator", string.Empty);
+        m_Injector.InjectAttributeWithContent(context.ModuleDefMD, string.Empty, "Dotfuscator", string.Empty);
+        m_Injector.InjectAttributeWithContent(context.ModuleDefMD, string.Empty, "Centos", string.Empty);
+        m_Injector.InjectAttributeWithContent(context.ModuleDefMD, string.Empty, "ConfusedBy", string.Empty);
+        m_Injector.InjectAttributeWithContent(context.ModuleDefMD, "NineRays.Obfuscator", "Evaluation", string.Empty);
+        m_Injector.InjectAttributeWithContent(context.ModuleDefMD, "CryptoObfuscator", "ProtectedWithCryptoObfuscator", string.Empty);
+        m_Injector.InjectAttributeWithContent(context.ModuleDefMD, string.Empty, "();\u0009", string.Empty);
+        m_Injector.InjectAttributeWithContent(context.ModuleDefMD, string.Empty, "EMyPID_8234_", string.Empty);
+        return Task.CompletedTask;
     }
 }
