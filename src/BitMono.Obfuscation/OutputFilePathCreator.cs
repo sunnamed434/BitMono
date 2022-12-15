e0dd5e6629bf68e70a2d1dd9559a9aae59957f95
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-15 17:47:46 +0200

    Create OutputFilePathCreator.cs

---
+++ src/BitMono.Obfuscation/OutputFilePathCreator.cs
@@ -0,0 +1,19 @@
+﻿namespace BitMono.Obfuscation;
+
+public class OutputFilePathCreator
+{
+    public BitMonoContext Create(BitMonoContext context)
+    {
+        var stringBuilder = new StringBuilder()
+            .Append(Path.GetFileNameWithoutExtension(context.FileName));
+        if (context.Watermark)
+        {
+            stringBuilder.
+                Append("_bitmono");
+        }
+        stringBuilder.Append(Path.GetExtension(context.FileName));
+        var outputFile = Path.Combine(context.OutputDirectoryName, stringBuilder.ToString());
+        context.OutputFile = outputFile;
+        return context;
+    }
+}
