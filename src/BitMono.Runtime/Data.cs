5c303fb84dd13ffc555991a43563e1d4c44d64ab
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-15 17:46:43 +0200

    Create Data.cs

---
+++ src/BitMono.Runtime/Data.cs
@@ -0,0 +1,13 @@
+﻿namespace BitMono.Runtime;
+
+public struct Data
+{
+    internal readonly static byte[] CryptKeyBytes = new byte[8];
+    internal readonly static byte[] SaltBytes = new byte[8];
+
+    static Data()
+    {
+        CryptKeyBytes = new byte[8];
+        SaltBytes = new byte[8];
+    }
+}
