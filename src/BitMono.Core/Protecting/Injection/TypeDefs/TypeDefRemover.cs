08672919b78af37d477a6663c91b6db9c07d7c32
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-15 19:12:18 +0200

    Update TypeDefRemover.cs

--- src/BitMono.Core/Protecting/Injection/TypeDefs/TypeDefRemover.cs
+++ src/BitMono.Core/Protecting/Injection/TypeDefs/TypeDefRemover.cs
@@ -1,29 +1,24 @@
-﻿using BitMono.API.Protecting.Injection.TypeDefs;
-using dnlib.DotNet;
-using System.Linq;
+﻿namespace BitMono.Core.Protecting.Injection.TypeDefs;
 
-namespace BitMono.Core.Protecting.Injection.TypeDefs
+public class TypeDefRemover : ITypeDefRemover
 {
-    public class TypeDefRemover : ITypeDefRemover
+    public bool Remove(string name, ModuleDefMD module)
     {
-        public bool Remove(string name, ModuleDefMD module)
+        foreach (var typeDef in module.GetTypes().ToArray())
         {
-            foreach (var typeDef in module.GetTypes().ToArray())
+            if (typeDef.Name.Equals(name))
             {
-                if (typeDef.Name.Equals(name))
+                if (typeDef.IsNested)
                 {
-                    if (typeDef.IsNested)
-                    {
-                        typeDef.NestedTypes.Remove(typeDef);
-                    }
-                    else
-                    {
-                        module.Types.Remove(typeDef);
-                    }
-                    return true;
+                    typeDef.NestedTypes.Remove(typeDef);
                 }
+                else
+                {
+                    module.Types.Remove(typeDef);
+                }
+                return true;
             }
-            return false;
         }
+        return false;
     }
 }
