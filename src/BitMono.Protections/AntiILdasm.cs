bdf7f5a8403ffbbeb05106793658c462b7331be2
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-15 17:47:21 +0200

    Update AntiILdasm.cs

--- src/BitMono.Protections/AntiILdasm.cs
+++ src/BitMono.Protections/AntiILdasm.cs
@@ -1,28 +1,18 @@
-﻿using BitMono.API.Protecting;
-using BitMono.API.Protecting.Contexts;
-using BitMono.API.Protecting.Injection;
-using BitMono.Core.Protecting;
-using BitMono.Core.Protecting.Attributes;
-using System.Runtime.CompilerServices;
-using System.Threading;
-using System.Threading.Tasks;
+﻿namespace BitMono.Protections;
 
-namespace BitMono.Protections
+[ProtectionName(nameof(AntiILdasm))]
+public class AntiILdasm : IProtection
 {
-    [ProtectionName(nameof(AntiILdasm))]
-    public class AntiILdasm : IProtection
+    private readonly IInjector m_Injector;
+
+    public AntiILdasm(IInjector injector)
     {
-        private readonly IInjector m_Injector;
+        m_Injector = injector;
+    }
 
-        public AntiILdasm(IInjector injector)
-        {
-            m_Injector = injector;
-        }
-
-        public Task ExecuteAsync(ProtectionContext context, ProtectionParameters parameters, CancellationToken cancellationToken = default)
-        {
-            m_Injector.InjectAttribute(context.ModuleDefMD, typeof(SuppressIldasmAttribute).Namespace, nameof(SuppressIldasmAttribute));
-            return Task.CompletedTask;
-        }
+    public Task ExecuteAsync(ProtectionContext context, ProtectionParameters parameters, CancellationToken cancellationToken = default)
+    {
+        m_Injector.InjectAttribute(context.ModuleDefMD, typeof(SuppressIldasmAttribute).Namespace, nameof(SuppressIldasmAttribute));
+        return Task.CompletedTask;
     }
 }
