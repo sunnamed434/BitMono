88561dac385e49ce1f75144c7368b3b842d56644
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-15 17:49:38 +0200

    Create ProtectionsResolver.cs

---
+++ src/BitMono.Core/Protecting/Resolvers/ProtectionsResolver.cs
@@ -0,0 +1,44 @@
+﻿namespace BitMono.Core.Protecting.Resolvers;
+
+public class ProtectionsResolver
+{
+    private readonly List<IProtection> m_Protections;
+    private readonly IEnumerable<ProtectionSettings> m_ProtectionSettings;
+    private readonly ILogger m_Logger;
+
+    public ProtectionsResolver(List<IProtection> protections, IEnumerable<ProtectionSettings> protectionSettings, ILogger logger)
+    {
+        m_Protections = protections;
+        m_ProtectionSettings = protectionSettings;
+        m_Logger = logger.ForContext<ProtectionsResolver>();
+    }
+
+    public ProtectionsResolveResult Sort()
+    {
+        var foundProtections = new List<IProtection>();
+        var cachedProtections = m_Protections.ToArray().ToList();
+        var disabledProtections = new List<string>();
+        foreach (var protectionSettings in m_ProtectionSettings.Where(p => p.Enabled))
+        {
+            var protection = cachedProtections.FirstOrDefault(p => p.GetName().Equals(protectionSettings.Name, StringComparison.OrdinalIgnoreCase));
+            if (protection != null)
+            {
+                foundProtections.Add(protection);
+                cachedProtections.Remove(protection);
+            }
+            else
+            {
+                m_Logger.Warning("Protection: {0}, does not exsist in context!", protectionSettings.Name);
+            }
+        }
+        foreach (var protection in cachedProtections)
+        {
+            disabledProtections.Add(protection.GetName());
+        }
+        return new ProtectionsResolveResult
+        {
+            FoundProtections = foundProtections,
+            DisabledProtections = disabledProtections,
+        };
+    }
+}
