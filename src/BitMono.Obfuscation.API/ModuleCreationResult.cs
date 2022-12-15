b1f0ea61a395acd20a39920450f41bb2c57c3182
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-15 17:48:29 +0200

    Create ModuleCreationResult.cs

---
+++ src/BitMono.Obfuscation.API/ModuleCreationResult.cs
@@ -0,0 +1,8 @@
+﻿namespace BitMono.Obfuscation.API;
+
+public class ModuleCreationResult
+{
+    public ModuleDefMD ModuleDefMD { get; set; }
+    public ModuleCreationOptions ModuleCreationOptions { get; set; }
+    public ModuleWriterOptions ModuleWriterOptions { get; set; }
+}
