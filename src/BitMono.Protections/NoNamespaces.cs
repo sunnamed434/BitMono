8eaf8e0458f75415de9b7e2ae15e7929cdd3ba53
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-15 17:46:55 +0200

    Update NoNamespaces.cs

--- src/BitMono.Protections/NoNamespaces.cs
+++ src/BitMono.Protections/NoNamespaces.cs
@@ -1,43 +1,30 @@
-﻿using BitMono.API.Protecting;
-using BitMono.API.Protecting.Contexts;
-using BitMono.Core.Protecting;
-using BitMono.Core.Protecting.Analyzing.DnlibDefs;
-using BitMono.Core.Protecting.Attributes;
-using BitMono.Utilities.Extensions.dnlib;
-using dnlib.DotNet;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using ILogger = Serilog.ILogger;
+﻿namespace BitMono.Protections;
 
-namespace BitMono.Protections
+[ProtectionName(nameof(NoNamespaces))]
+public class NoNamespaces : IProtection
 {
-    [ProtectionName(nameof(NoNamespaces))]
-    public class NoNamespaces : IProtection
+    private readonly DnlibDefCriticalAnalyzer m_DnlibDefCriticalAnalyzer;
+    private readonly ILogger m_Logger;
+
+    public NoNamespaces(
+        DnlibDefCriticalAnalyzer typeDefCriticalAnalyzer, 
+        ILogger logger)
     {
-        private readonly DnlibDefCriticalAnalyzer m_DnlibDefCriticalAnalyzer;
-        private readonly ILogger m_Logger;
+        m_DnlibDefCriticalAnalyzer = typeDefCriticalAnalyzer;
+        m_Logger = logger.ForContext<NoNamespaces>();
+    }
 
-        public NoNamespaces(
-            DnlibDefCriticalAnalyzer typeDefCriticalAnalyzer, 
-            ILogger logger)
+    public Task ExecuteAsync(ProtectionContext context, ProtectionParameters parameters, CancellationToken cancellationToken = default)
+    {
+        foreach (var typeDef in parameters.Targets.OfType<TypeDef>())
         {
-            m_DnlibDefCriticalAnalyzer = typeDefCriticalAnalyzer;
-            m_Logger = logger.ForContext<NoNamespaces>();
-        }
-
-        public Task ExecuteAsync(ProtectionContext context, ProtectionParameters parameters, CancellationToken cancellationToken = default)
-        {
-            foreach (var typeDef in parameters.Targets.OfType<TypeDef>())
+            if (typeDef.IsGlobalModuleType == false
+                && m_DnlibDefCriticalAnalyzer.NotCriticalToMakeChanges(typeDef)
+                && typeDef.HasNamespace())
             {
-                if (typeDef.IsGlobalModuleType == false
-                    && m_DnlibDefCriticalAnalyzer.NotCriticalToMakeChanges(typeDef)
-                    && typeDef.HasNamespace())
-                {
-                    typeDef.Namespace = string.Empty;
-                }
+                typeDef.Namespace = string.Empty;
             }
-            return Task.CompletedTask;
         }
+        return Task.CompletedTask;
     }
 }
