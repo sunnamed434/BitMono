85047b5eb961c218e66282a64ad95e4a27c80857
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-15 17:47:29 +0200

    Update TipsNotifier.cs

--- src/BitMono.Obfuscation/TipsNotifier.cs
+++ src/BitMono.Obfuscation/TipsNotifier.cs
@@ -1,31 +1,22 @@
-﻿using BitMono.API.Configuration;
-using Microsoft.Extensions.Configuration;
-using Serilog;
-using System;
-using System.Linq;
-using System.Threading.Tasks;
+﻿namespace BitMono.Obfuscation;
 
-namespace BitMono.Obfuscation
+public class TipsNotifier
 {
-    public class TipsNotifier
+    private readonly IConfiguration m_Configuration;
+    private readonly ILogger m_Logger;
+
+    public TipsNotifier(IBitMonoAppSettingsConfiguration configuration, ILogger logger)
     {
-        private readonly IConfiguration m_Configuration;
-        private readonly ILogger m_Logger;
+        m_Configuration = configuration.Configuration;
+        m_Logger = logger.ForContext<TipsNotifier>();
+    }
 
-        public TipsNotifier(IBitMonoAppSettingsConfiguration configuration, ILogger logger)
-        {
-            m_Configuration = configuration.Configuration;
-            m_Logger = logger.ForContext<TipsNotifier>();
-        }
-
-        public Task NotifyAsync()
-        {
-            var tips = m_Configuration.GetSection("Tips").Get<string[]>();
-            Random random = new Random();
-            var tip = tips.Reverse().ToArray()[random.Next(0, tips.Length)];
-            m_Logger.Information("Today is your day! Generating helpful tip for you - see it a bit down!");
-            m_Logger.Information(tip);
-            return Task.CompletedTask;
-        }
+    public void Notify()
+    {
+        var tips = m_Configuration.GetSection("Tips").Get<string[]>();
+        var random = new Random();
+        var tip = tips.Reverse().ToArray()[random.Next(0, tips.Length)];
+        m_Logger.Information("Today is your day! Generating helpful tip for you - see it a bit down!");
+        m_Logger.Information(tip);
     }
 }
