ef5c7536491b279708e2689fce81b39d7716001e
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-15 17:46:57 +0200

    Update InstructionTokensUpdate.cs

--- src/BitMono.Protections/InstructionTokensUpdate.cs
+++ src/BitMono.Protections/InstructionTokensUpdate.cs
@@ -1,12 +1,9 @@
-﻿using dnlib.DotNet;
+﻿namespace BitMono.Protections;
 
-namespace BitMono.Protections
+public class InstructionTokensUpdate
 {
-    public class InstructionTokensUpdate
-    {
-        public MethodDef InitializatorMethodDef { get; set; }
-        public MethodDef FromMethodDef { get; set; }
-        public MethodDef ToMethodDef { get; set; }
-        public int Index { get; set; }
-    }
+    public MethodDef InitializatorMethodDef { get; set; }
+    public MethodDef FromMethodDef { get; set; }
+    public MethodDef ToMethodDef { get; set; }
+    public int Index { get; set; }
 }
