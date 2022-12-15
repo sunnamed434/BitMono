fad7b36a8dc0b0368e5cfdb0285327e0e2bcb973
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-15 19:12:15 +0200

    Update MethodDefsRemover.cs

--- src/BitMono.Core/Protecting/Injection/MethodDefs/MethodDefsRemover.cs
+++ src/BitMono.Core/Protecting/Injection/MethodDefs/MethodDefsRemover.cs
@@ -1,37 +1,32 @@
-﻿using BitMono.API.Protecting.Injection.MethodDefs;
-using dnlib.DotNet;
-using System.Linq;
+﻿namespace BitMono.Core.Protecting.Injection.MethodDefs;
 
-namespace BitMono.Core.Protecting.Injection.MethodDefs
+public class MethodDefsRemover : IMethodDefRemover
 {
-    public class MethodDefsRemover : IMethodDefRemover
+    public bool Remove(ModuleDefMD moduleDefMD, string name)
     {
-        public bool Remove(ModuleDefMD moduleDefMD, string name)
+        foreach (var typeDef in moduleDefMD.GetTypes().ToArray())
         {
-            foreach (var typeDef in moduleDefMD.GetTypes().ToArray())
+            foreach (var methodDef in typeDef.Methods)
             {
-                foreach (var methodDef in typeDef.Methods)
-                {
-                    if (typeDef.Name.Equals(name))
-                    {
-                        typeDef.Methods.Remove(methodDef);
-                        return true;
-                    }
-                }
-            }
-            return false;
-        }
-        public bool Remove(TypeDef typeDef, string name)
-        {
-            foreach (var methodDef in typeDef.Methods.ToArray())
-            {
-                if (methodDef.Name.Equals(name))
+                if (typeDef.Name.Equals(name))
                 {
                     typeDef.Methods.Remove(methodDef);
                     return true;
                 }
             }
-            return false;
         }
+        return false;
+    }
+    public bool Remove(TypeDef typeDef, string name)
+    {
+        foreach (var methodDef in typeDef.Methods.ToArray())
+        {
+            if (methodDef.Name.Equals(name))
+            {
+                typeDef.Methods.Remove(methodDef);
+                return true;
+            }
+        }
+        return false;
     }
 }
