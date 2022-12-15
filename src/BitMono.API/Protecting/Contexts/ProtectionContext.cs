9ed71b3d2fbf74d6f226b044f02e265e483e1995
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-15 17:45:08 +0200

    Update ProtectionContext.cs

--- src/BitMono.API/Protecting/Contexts/ProtectionContext.cs
+++ src/BitMono.API/Protecting/Contexts/ProtectionContext.cs
@@ -1,29 +1,22 @@
-﻿using dnlib.DotNet;
-using dnlib.DotNet.Writer;
-using NullGuard;
-using System.IO;
+﻿namespace BitMono.API.Protecting.Contexts;
 
-namespace BitMono.API.Protecting.Contexts
+public class ProtectionContext
 {
-    public class ProtectionContext
+    [AllowNull] public ModuleDefMD ModuleDefMD { get; set; }
+    [AllowNull] public ModuleCreationOptions ModuleCreationOptions { get; set; }
+    [AllowNull] public ModuleWriterOptions ModuleWriterOptions { get; set; }
+    [AllowNull] public ModuleDefMD RuntimeModuleDefMD { get; set; }
+    [AllowNull] public Importer Importer { get; set; }
+    [AllowNull] public Importer RuntimeImporter { get; set; }
+    [AllowNull] public BitMonoContext BitMonoContext { get; set; }
+    [AllowNull] public byte[] ModuleDefMDOutput { get; set; }
+
+    [AllowNull] public ModuleContext ModuleContext => ModuleCreationOptions.Context;
+    [AllowNull] public AssemblyResolver AssemblyResolver => (AssemblyResolver)ModuleContext.AssemblyResolver;
+
+    public void ReloadModule()
     {
-        [AllowNull] public ModuleDefMD ModuleDefMD { get; set; }
-        [AllowNull] public ModuleCreationOptions ModuleCreationOptions { get; set; }
-        [AllowNull] public ModuleWriterOptions ModuleWriterOptions { get; set; }
-        [AllowNull] public ModuleDefMD RuntimeModuleDefMD { get; set; }
-        [AllowNull] public Importer Importer { get; set; }
-        [AllowNull] public Importer RuntimeImporter { get; set; }
-        [AllowNull] public BitMonoContext BitMonoContext { get; set; }
-        [AllowNull] public byte[] ModuleDefMDOutput { get; set; }
-        [AllowNull] public MemoryStream ModuleDefMDMemoryStream { get; set; }
-
-        [AllowNull] public ModuleContext ModuleContext => ModuleCreationOptions.Context;
-        [AllowNull] public AssemblyResolver AssemblyResolver => (AssemblyResolver)ModuleContext.AssemblyResolver;
-
-        public void ReloadModule()
-        {
-            ModuleDefMD = ModuleDefMD.Load(ModuleDefMDMemoryStream, ModuleCreationOptions);
-            Importer = new Importer(ModuleDefMD);
-        }
+        ModuleDefMD = ModuleDefMD.Load(ModuleDefMDOutput);
+        Importer = new Importer(ModuleDefMD);
     }
 }
