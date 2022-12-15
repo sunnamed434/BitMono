6f97422d4057102f24ab6b3b1e93a82d91a40cad
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-15 19:12:12 +0200

    Update FieldDefSearcher.cs

--- src/BitMono.Core/Protecting/Injection/FieldDefs/FieldDefSearcher.cs
+++ src/BitMono.Core/Protecting/Injection/FieldDefs/FieldDefSearcher.cs
@@ -1,43 +1,38 @@
-﻿using BitMono.API.Protecting.Injection.FieldDefs;
-using dnlib.DotNet;
-using NullGuard;
+﻿namespace BitMono.Core.Protecting.Injection.FieldDefs;
 
-namespace BitMono.Core.Protecting.Injection.FieldDefs
+public class FieldDefSearcher : IFieldSearcher
 {
-    public class FieldDefSearcher : IFieldSearcher
+    [return: AllowNull]
+    public FieldDef Find(string name, ModuleDefMD moduleDefMD)
     {
-        [return: AllowNull]
-        public FieldDef Find(string name, ModuleDefMD moduleDefMD)
+        foreach (var typeDef in moduleDefMD.Types)
         {
-            foreach (var typeDef in moduleDefMD.Types)
+            foreach (var fieldDef in typeDef.Fields)
             {
-                foreach (var fieldDef in typeDef.Fields)
+                if (fieldDef.Name.Equals(name))
                 {
-                    if (fieldDef.Name.Equals(name))
+                    return fieldDef;
+                }
+            }
+        }
+        return null;
+    }
+    [return: AllowNull]
+    public FieldDef FindInGlobalNestedTypes(string name, ModuleDefMD moduleDefMD)
+    {
+        foreach (var type in moduleDefMD.GlobalType.NestedTypes)
+        {
+            if (type.HasFields)
+            {
+                foreach (var childField in type.Fields)
+                {
+                    if (childField.Name.Equals(name))
                     {
-                        return fieldDef;
+                        return childField;
                     }
                 }
             }
-            return null;
         }
-        [return: AllowNull]
-        public FieldDef FindInGlobalNestedTypes(string name, ModuleDefMD moduleDefMD)
-        {
-            foreach (var type in moduleDefMD.GlobalType.NestedTypes)
-            {
-                if (type.HasFields)
-                {
-                    foreach (var childField in type.Fields)
-                    {
-                        if (childField.Name.Equals(name))
-                        {
-                            return childField;
-                        }
-                    }
-                }
-            }
-            return null;
-        }
+        return null;
     }
 }
