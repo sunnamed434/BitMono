a2961be33a8a3a435afb9db6d34e1ba410aafb38
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-15 17:45:07 +0200

    Update BitMonoContext.cs

--- src/BitMono.API/Protecting/Contexts/BitMonoContext.cs
+++ src/BitMono.API/Protecting/Contexts/BitMonoContext.cs
@@ -1,13 +1,10 @@
-﻿using System.Collections.Generic;
+﻿namespace BitMono.API.Protecting.Contexts;
 
-namespace BitMono.API.Protecting.Contexts
+public class BitMonoContext
 {
-    public class BitMonoContext
-    {
-        public string ModuleFileName { get; set; }
-        public string OutputPath { get; set; }
-        public string OutputModuleFile { get; set; }
-        public IEnumerable<byte[]> DependenciesData { get; set; }
-        public bool Watermark { get; set; }
-    }
+    public string FileName { get; set; }
+    public string OutputDirectoryName { get; set; }
+    public string OutputFile { get; set; }
+    public IEnumerable<byte[]> DependenciesData { get; set; }
+    public bool Watermark { get; set; }
 }
