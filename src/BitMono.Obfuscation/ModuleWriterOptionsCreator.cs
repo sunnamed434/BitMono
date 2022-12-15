d6c2aa2dd4568615e8b7d71c8f7c5dc6055f1972
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-15 17:47:45 +0200

    Create ModuleWriterOptionsCreator.cs

---
+++ src/BitMono.Obfuscation/ModuleWriterOptionsCreator.cs
@@ -0,0 +1,18 @@
+﻿namespace BitMono.Obfuscation;
+
+public class ModuleWriterOptionsCreator
+{
+    public ModuleWriterOptions Create(ModuleDefMD moduleDefMD)
+    {
+        var moduleWriterOptions = new ModuleWriterOptions(moduleDefMD);
+        moduleWriterOptions.MetadataLogger = DummyLogger.NoThrowInstance;
+        moduleWriterOptions.MetadataOptions.Flags |= 
+              MetadataFlags.PreserveStringsOffsets 
+            | MetadataFlags.PreserveUSOffsets 
+            | MetadataFlags.PreserveBlobOffsets 
+            | MetadataFlags.PreserveExtraSignatureData;
+        moduleWriterOptions.Cor20HeaderOptions.Flags = ComImageFlags.ILOnly;
+        moduleWriterOptions.PEHeadersOptions.CopyPEHeaders(moduleDefMD);
+        return moduleWriterOptions;
+    }
+}
