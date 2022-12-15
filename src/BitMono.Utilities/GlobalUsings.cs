f14b5dbe276d7c38f195213c01fb14ce1ed2121d
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-15 21:27:27 +0200

    Update GlobalUsings.cs

--- src/BitMono.Utilities/GlobalUsings.cs
+++ src/BitMono.Utilities/GlobalUsings.cs
@@ -1,7 +1,8 @@
 ﻿global using dnlib.DotNet;
+global using dnlib.DotNet.Writer;
+global using MoreLinq;
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
 global using System.Runtime.CompilerServices;
 global using TypeAttributes = dnlib.DotNet.TypeAttributes;
-global using dnlib.DotNet.Writer;
-global using System.Collections.Generic;
-global using System;
-global using System.Linq;
