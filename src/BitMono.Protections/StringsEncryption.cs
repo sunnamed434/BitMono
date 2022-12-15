aad3996c37aa5e6f62f156467d0e21f2b42d2216
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-15 17:46:53 +0200

    Update StringsEncryption.cs

--- src/BitMono.Protections/StringsEncryption.cs
+++ src/BitMono.Protections/StringsEncryption.cs
@@ -1,87 +1,67 @@
-﻿using BitMono.API.Protecting;
-using BitMono.API.Protecting.Contexts;
-using BitMono.API.Protecting.Injection;
-using BitMono.API.Protecting.Injection.FieldDefs;
-using BitMono.API.Protecting.Injection.MethodDefs;
-using BitMono.API.Protecting.Renaming;
-using BitMono.Core.Protecting;
-using BitMono.Core.Protecting.Analyzing.DnlibDefs;
-using BitMono.Core.Protecting.Attributes;
-using BitMono.Core.Protecting.Helpers;
-using BitMono.Runtime;
-using BitMono.Utilities.Extensions.dnlib;
-using dnlib.DotNet;
-using dnlib.DotNet.Emit;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using ILogger = Serilog.ILogger;
+﻿namespace BitMono.Protections;
 
-namespace BitMono.Protections
+[ProtectionName(nameof(StringsEncryption))]
+public class StringsEncryption : IProtection
 {
-    [ProtectionName(nameof(StringsEncryption))]
-    public class StringsEncryption : IProtection
+    private readonly IInjector m_Injector;
+    private readonly IFieldSearcher m_FieldSearcher;
+    private readonly IMethodDefSearcher m_MethodSearcher;
+    private readonly DnlibDefCriticalAnalyzer m_DnlibDefCriticalAnalyzer;
+    private readonly IRenamer m_Renamer;
+    private readonly ILogger m_Logger;
+
+    public StringsEncryption(
+        IInjector injector,
+        IFieldSearcher fieldSearcher,
+        IMethodDefSearcher methodSearcher,
+        DnlibDefCriticalAnalyzer dnlibDefCriticalAnalyzer,
+        IRenamer renamer,
+        ILogger logger)
     {
-        private readonly IInjector m_Injector;
-        private readonly IFieldSearcher m_FieldSearcher;
-        private readonly IMethodDefSearcher m_MethodSearcher;
-        private readonly DnlibDefCriticalAnalyzer m_DnlibDefCriticalAnalyzer;
-        private readonly IRenamer m_Renamer;
-        private readonly ILogger m_Logger;
+        m_Injector = injector;
+        m_FieldSearcher = fieldSearcher;
+        m_MethodSearcher = methodSearcher;
+        m_DnlibDefCriticalAnalyzer = dnlibDefCriticalAnalyzer;
+        m_Renamer = renamer;
+        m_Logger = logger.ForContext<StringsEncryption>();
+    }
 
-        public StringsEncryption(
-            IInjector injector,
-            IFieldSearcher fieldSearcher,
-            IMethodDefSearcher methodSearcher,
-            DnlibDefCriticalAnalyzer dnlibDefCriticalAnalyzer,
-            IRenamer renamer,
-            ILogger logger)
+    public Task ExecuteAsync(ProtectionContext context, ProtectionParameters parameters, CancellationToken cancellationToken = default)
+    {
+        var runtimeDecryptorTypeDef = context.RuntimeModuleDefMD.ResolveTypeDefOrThrow<Decryptor>();
+
+        var decryptorTypeDef = m_Injector.InjectInvisibleValueType(context.ModuleDefMD, context.ModuleDefMD.GlobalType, m_Renamer.RenameUnsafely()).ResolveTypeDefThrow();
+        var cryptKeyFieldDef = m_Injector.InjectInvisibleArray(context.ModuleDefMD, context.ModuleDefMD.GlobalType, Data.CryptKeyBytes, m_Renamer.RenameUnsafely()).ResolveFieldDefThrow();
+        var saltBytesFieldDef = m_Injector.InjectInvisibleArray(context.ModuleDefMD, context.ModuleDefMD.GlobalType, Data.SaltBytes, m_Renamer.RenameUnsafely()).ResolveFieldDefThrow();
+
+        var injectedDecryptorDnlibDefs = InjectHelper.Inject(runtimeDecryptorTypeDef, decryptorTypeDef, context.ModuleDefMD);
+        var decryptMethodDef = injectedDecryptorDnlibDefs.FirstOrDefault(i => i.Name.String.Equals(nameof(Decryptor.Decrypt))).ResolveMethodDefOrThrow();
+        
+        foreach (var typeDef in parameters.Targets.OfType<TypeDef>())
         {
-            m_Injector = injector;
-            m_FieldSearcher = fieldSearcher;
-            m_MethodSearcher = methodSearcher;
-            m_DnlibDefCriticalAnalyzer = dnlibDefCriticalAnalyzer;
-            m_Renamer = renamer;
-            m_Logger = logger.ForContext<StringsEncryption>();
-        }
-
-        public Task ExecuteAsync(ProtectionContext context, ProtectionParameters parameters, CancellationToken cancellationToken = default)
-        {
-            var runtimeDecryptorTypeDef = context.RuntimeModuleDefMD.ResolveTypeDefOrThrow<Decryptor>();
-
-            var decryptorTypeDef = m_Injector.InjectInvisibleValueType(context.ModuleDefMD, context.ModuleDefMD.GlobalType, m_Renamer.RenameUnsafely()).ResolveTypeDefThrow();
-            var cryptKeyFieldDef = m_Injector.InjectInvisibleArray(context.ModuleDefMD, context.ModuleDefMD.GlobalType, Data.CryptKeyBytes, m_Renamer.RenameUnsafely()).ResolveFieldDefThrow();
-            var saltBytesFieldDef = m_Injector.InjectInvisibleArray(context.ModuleDefMD, context.ModuleDefMD.GlobalType, Data.SaltBytes, m_Renamer.RenameUnsafely()).ResolveFieldDefThrow();
-
-            var injectedDecryptorDnlibDefs = InjectHelper.Inject(runtimeDecryptorTypeDef, decryptorTypeDef, context.ModuleDefMD);
-            var decryptMethodDef = injectedDecryptorDnlibDefs.FirstOrDefault(i => i.Name.String.Equals(nameof(Decryptor.Decrypt))).ResolveMethodDefOrThrow();
-            
-            foreach (var typeDef in parameters.Targets.OfType<TypeDef>())
+            foreach (var methodDef in typeDef.Methods.ToArray())
             {
-                foreach (var methodDef in typeDef.Methods.ToArray())
+                if (methodDef.HasBody && m_DnlibDefCriticalAnalyzer.NotCriticalToMakeChanges(methodDef))
                 {
-                    if (methodDef.HasBody && m_DnlibDefCriticalAnalyzer.NotCriticalToMakeChanges(methodDef))
+                    methodDef.Body.KeepOldMaxStack = true;
+                    for (var i = 0; i < methodDef.Body.Instructions.Count(); i++)
                     {
-                        methodDef.Body.KeepOldMaxStack = true;
-                        for (var i = 0; i < methodDef.Body.Instructions.Count(); i++)
+                        if (methodDef.Body.Instructions[i].OpCode == OpCodes.Ldstr
+                            && methodDef.Body.Instructions[i].Operand is string content)
                         {
-                            if (methodDef.Body.Instructions[i].OpCode == OpCodes.Ldstr
-                                && methodDef.Body.Instructions[i].Operand is string content)
-                            {
-                                var encryptedContentBytes = Encryptor.EncryptContent(content, Data.SaltBytes, Data.CryptKeyBytes);
-                                var encryptedDataFieldDef = m_Injector.InjectInvisibleArray(context.ModuleDefMD, context.ModuleDefMD.GlobalType, encryptedContentBytes, m_Renamer.RenameUnsafely());
+                            var encryptedContentBytes = Encryptor.EncryptContent(content, Data.SaltBytes, Data.CryptKeyBytes);
+                            var encryptedDataFieldDef = m_Injector.InjectInvisibleArray(context.ModuleDefMD, context.ModuleDefMD.GlobalType, encryptedContentBytes, m_Renamer.RenameUnsafely());
 
-                                methodDef.Body.Instructions[i].ReplaceWith(OpCodes.Ldsfld, encryptedDataFieldDef);
-                                methodDef.Body.Instructions.Insert(i + 1, new Instruction(OpCodes.Ldsfld, saltBytesFieldDef));
-                                methodDef.Body.Instructions.Insert(i + 2, new Instruction(OpCodes.Ldsfld, cryptKeyFieldDef));
-                                methodDef.Body.Instructions.Insert(i + 3, new Instruction(OpCodes.Call, decryptMethodDef));
-                                i += 3;
-                            }
+                            methodDef.Body.Instructions[i].ReplaceWith(OpCodes.Ldsfld, encryptedDataFieldDef);
+                            methodDef.Body.Instructions.Insert(i + 1, new Instruction(OpCodes.Ldsfld, saltBytesFieldDef));
+                            methodDef.Body.Instructions.Insert(i + 2, new Instruction(OpCodes.Ldsfld, cryptKeyFieldDef));
+                            methodDef.Body.Instructions.Insert(i + 3, new Instruction(OpCodes.Call, decryptMethodDef));
+                            i += 3;
                         }
                     }
                 }
             }
-            return Task.CompletedTask;
         }
+        return Task.CompletedTask;
     }
 }
