aed15899d6ac3bf9eca2a7c078c843ec1c55a489
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-16 13:20:56 +0200

    Update ProtectionContextCreator.cs

--- src/BitMono.Obfuscation/ProtectionContextCreator.cs
+++ src/BitMono.Obfuscation/ProtectionContextCreator.cs
@@ -2,17 +2,17 @@
 
 public class ProtectionContextCreator
 {
-    private readonly ModuleCreationResult m_ModuleDefMDCreationResult;
+    private readonly ModuleCreationResult m_ModuleCreationResult;
     private readonly ModuleDefMD m_RuntimeModuleDefMD;
     private readonly BitMonoContext m_Context;
 
     public ProtectionContextCreator(
-        ModuleCreationResult moduleDefMDCreationResult, 
-        ModuleDefMD externalComponentsModuleDefMD, 
+        ModuleCreationResult moduleCreationResult, 
+        ModuleDefMD runtimeModuleDefMD, 
         BitMonoContext context)
     {
-        m_ModuleDefMDCreationResult = moduleDefMDCreationResult;
-        m_RuntimeModuleDefMD = externalComponentsModuleDefMD;
+        m_ModuleCreationResult = moduleCreationResult;
+        m_RuntimeModuleDefMD = runtimeModuleDefMD;
         m_Context = context;
     }
 
@@ -20,11 +20,11 @@ public class ProtectionContextCreator
     {
         return new ProtectionContext
         {
-            ModuleDefMD = m_ModuleDefMDCreationResult.ModuleDefMD,
-            ModuleCreationOptions = m_ModuleDefMDCreationResult.ModuleCreationOptions,
-            ModuleWriterOptions = m_ModuleDefMDCreationResult.ModuleWriterOptions,
+            ModuleDefMD = m_ModuleCreationResult.ModuleDefMD,
+            ModuleCreationOptions = m_ModuleCreationResult.ModuleCreationOptions,
+            ModuleWriterOptions = m_ModuleCreationResult.ModuleWriterOptions,
             RuntimeModuleDefMD = m_RuntimeModuleDefMD,
-            Importer = new Importer(m_ModuleDefMDCreationResult.ModuleDefMD),
+            Importer = new Importer(m_ModuleCreationResult.ModuleDefMD),
             RuntimeImporter = new Importer(m_RuntimeModuleDefMD, ImporterOptions.TryToUseDefs),
             BitMonoContext = m_Context,
         };
