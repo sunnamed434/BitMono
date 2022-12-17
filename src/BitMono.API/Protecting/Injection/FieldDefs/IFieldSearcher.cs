dd061e078db191593873abc9107e0c01ab0a58ad
sunnamed <65300126+sunnamed434@users.noreply.github.com>
2022-12-17 12:39:32 +0200

    Delete IFieldSearcher.cs

--- src/BitMono.API/Protecting/Injection/FieldDefs/IFieldSearcher.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BitMono.API.Protecting.Injection.FieldDefs;
-
-public interface IFieldSearcher
-{
-    FieldDef Find(string name, ModuleDefMD moduleDefMD);
-    FieldDef FindInGlobalNestedTypes(string name, ModuleDefMD moduleDefMD);
-}
