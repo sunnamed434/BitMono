59ffe0ec62e6599215e4e864d5500eab38418e02
sunnamed <65300126+sunnamed434@users.noreply.github.com>
2022-12-17 12:39:30 +0200

    Delete IMethodDefRemover.cs

--- src/BitMono.API/Protecting/Injection/MethodDefs/IMethodDefRemover.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BitMono.API.Protecting.Injection.MethodDefs;
-
-public interface IMethodDefRemover
-{
-    bool Remove(ModuleDefMD moduleDefMD, string name);
-    bool Remove(TypeDef typeDef, string name);
-}
