203aa0477ca019e053930579b44e3de2c86e8d4b
sunnamed <65300126+sunnamed434@users.noreply.github.com>
2022-12-17 12:39:47 +0200

    Create TypeDefAttributeCriticalAnalyzer.cs

---
+++ src/BitMono.Core/Protecting/Analyzing/TypeDefs/TypeDefAttributeCriticalAnalyzer.cs
@@ -0,0 +1,32 @@
+﻿namespace BitMono.Core.Protecting.Analyzing.TypeDefs;
+
+public class TypeDefAttributeCriticalAnalyzer : ICriticalAnalyzer<IHasCustomAttribute>
+{
+    private readonly IAttemptAttributeResolver m_AttemptAttributeResolver;
+
+    public TypeDefAttributeCriticalAnalyzer(IAttemptAttributeResolver attemptAttributeResolver)
+    {
+        m_AttemptAttributeResolver = attemptAttributeResolver;
+    }
+
+    public bool NotCriticalToMakeChanges(IHasCustomAttribute customAttribute)
+    {
+        if (m_AttemptAttributeResolver.TryResolve<SerializableAttribute>(customAttribute, null, null, out _))
+        {
+            return false;
+        }
+        if (m_AttemptAttributeResolver.TryResolve<XmlAttributeAttribute>(customAttribute, null, null, out _))
+        {
+            return false;
+        }
+        if (m_AttemptAttributeResolver.TryResolve<XmlArrayItemAttribute>(customAttribute, null, null, out _))
+        {
+            return false;
+        }
+        if (m_AttemptAttributeResolver.TryResolve<JsonPropertyAttribute>(customAttribute, null, null, out _))
+        {
+            return false;
+        }
+        return false;
+    }
+}
