02c826ebb57ef68725056fdd7d16e5b8ccd8b698
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-18 10:51:30 +0200

    Update ModuleCreator.cs

--- src/BitMono.Obfuscation/ModuleCreator.cs
+++ src/BitMono.Obfuscation/ModuleCreator.cs
@@ -1,19 +1,23 @@
-﻿namespace BitMono.Obfuscation;
+﻿using AsmResolver.DotNet;
+using AsmResolver.DotNet.Serialized;
+
+namespace BitMono.Obfuscation;
 
 public class ModuleCreator : IModuleCreator
 {
-    private readonly byte[] m_ModuleBytes;
+    private readonly byte[] m_Bytes;
 
-    public ModuleCreator(byte[] moduleBytes)
+    public ModuleCreator(byte[] bytes)
     {
-        m_ModuleBytes = moduleBytes;
+        m_Bytes = bytes;
     }
 
     public ModuleCreationResult Create()
     {
         var moduleContext = ModuleDefMD.CreateModuleContext();
         var moduleCreationOptions = new ModuleCreationOptions(moduleContext, CLRRuntimeReaderKind.Mono);
-        var moduleDefMD = ModuleDefMD.Load(m_ModuleBytes, moduleCreationOptions);
+        var moduleDefention = ModuleDefinition.FromBytes(m_Bytes, new ModuleReaderParameters());
+        var moduleDefMD = ModuleDefMD.Load(m_Bytes, moduleCreationOptions);
         var ModuleWriterOptions = new ModuleWriterOptionsCreator().Create(moduleDefMD);
         return new ModuleCreationResult
         {
