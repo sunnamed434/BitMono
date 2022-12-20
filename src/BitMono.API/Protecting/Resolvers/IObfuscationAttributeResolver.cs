67f1143900561e4235af9c25d6e53cbf37e0dcce
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-20 15:56:14 +0200

    Create IObfuscationAttributeResolver.cs

---
+++ src/BitMono.API/Protecting/Resolvers/IObfuscationAttributeResolver.cs
@@ -0,0 +1,7 @@
+﻿namespace BitMono.API.Protecting.Resolvers;
+
+public interface IObfuscationAttributeResolver
+{
+    bool Resolve(string feature, IHasCustomAttribute from);
+    bool Resolve<TFeature>(IHasCustomAttribute from) where TFeature : IProtection;
+}
