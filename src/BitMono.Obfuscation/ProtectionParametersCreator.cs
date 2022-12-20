bd35dd858f84527ae8c5f27efa4181a80c11573c
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-20 16:01:01 +0200

    Update ProtectionParametersCreator.cs

--- src/BitMono.Obfuscation/ProtectionParametersCreator.cs
+++ src/BitMono.Obfuscation/ProtectionParametersCreator.cs
@@ -3,17 +3,17 @@
 public class ProtectionParametersCreator
 {
     private readonly DnlibDefsResolver m_DnlibDefsResolver;
-    private readonly IEnumerable<IDnlibDefResolver> m_Resolvers;
+    private readonly IEnumerable<IMemberDefinitionfResolver> m_Resolvers;
 
-    public ProtectionParametersCreator(DnlibDefsResolver dnlibDefsResolver, IEnumerable<IDnlibDefResolver> resolvers)
+    public ProtectionParametersCreator(DnlibDefsResolver dnlibDefsResolver, IEnumerable<IMemberDefinitionfResolver> resolvers)
     {
         m_DnlibDefsResolver = dnlibDefsResolver;
         m_Resolvers = resolvers;
     }
 
-    public ProtectionParameters Create(string feature, ModuleDefMD moduleDefMD)
+    public ProtectionParameters Create(string feature, ModuleDefinition moduleDefinition)
     {
-        var definitions = moduleDefMD.FindDefinitions();
+        var definitions = moduleDefinition.FindDefinitions();
         var targets = m_DnlibDefsResolver.Resolve(feature, definitions, m_Resolvers).ToList();
         return new ProtectionParameters(targets);
     }
