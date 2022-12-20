b4bd55219b2fa07719c044cf14d391b6dc76626a
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-20 15:55:09 +0200

    Fix StringsEncryption protection

--- src/BitMono.Protections/StringsEncryption.cs
+++ src/BitMono.Protections/StringsEncryption.cs
@@ -1,6 +1,5 @@
 ﻿namespace BitMono.Protections;
 
-[ProtectionName(nameof(StringsEncryption))]
 public class StringsEncryption : IProtection
 {
     private readonly IInjector m_Injector;
@@ -19,35 +18,35 @@ public class StringsEncryption : IProtection
 
     public Task ExecuteAsync(ProtectionContext context, ProtectionParameters parameters, CancellationToken cancellationToken = default)
     {
-        var runtimeDecryptorTypeDef = context.RuntimeModuleDefMD.ResolveTypeDefOrThrow<Decryptor>();
+        var globalModuleType = context.Module.GetOrCreateModuleType();
+        var decryptorType = m_Injector.InjectInvisibleValueType(context.Module, globalModuleType, m_Renamer.RenameUnsafely());
+        var cryptKeyField = m_Injector.InjectInvisibleArray(context.Module, globalModuleType, Data.CryptKeyBytes, m_Renamer.RenameUnsafely());
+        var saltBytesField = m_Injector.InjectInvisibleArray(context.Module, globalModuleType, Data.SaltBytes, m_Renamer.RenameUnsafely());
 
-        var decryptorTypeDef = m_Injector.InjectInvisibleValueType(context.ModuleDefMD, context.ModuleDefMD.GlobalType, m_Renamer.RenameUnsafely()).ResolveTypeDefThrow();
-        var cryptKeyFieldDef = m_Injector.InjectInvisibleArray(context.ModuleDefMD, context.ModuleDefMD.GlobalType, Data.CryptKeyBytes, m_Renamer.RenameUnsafely()).ResolveFieldDefThrow();
-        var saltBytesFieldDef = m_Injector.InjectInvisibleArray(context.ModuleDefMD, context.ModuleDefMD.GlobalType, Data.SaltBytes, m_Renamer.RenameUnsafely()).ResolveFieldDefThrow();
+        var runtimeDecryptorType = context.RuntimeModule.ResolveOrThrow<TypeDefinition>(typeof(Decryptor));
+        var memberCloneResult = new MemberCloner(context.Module, new InjectTypeClonerListener(context.Module))
+            .Include(runtimeDecryptorType)
+            .Clone();
 
-        var injectedDecryptorDnlibDefs = InjectHelper.Inject(runtimeDecryptorTypeDef, decryptorTypeDef, context.ModuleDefMD);
-        var decryptMethodDef = injectedDecryptorDnlibDefs.FirstOrDefault(i => i.Name.String.Equals(nameof(Decryptor.Decrypt))).ResolveMethodDefOrThrow();
+        var decryptMethod = memberCloneResult.ClonedMembers.Single(c => c.Name.Equals(nameof(Decryptor.Decrypt))) as MethodDefinition;
         
-        foreach (var typeDef in parameters.Targets.OfType<TypeDef>())
+        foreach (var method in parameters.Targets.OfType<MethodDefinition>())
         {
-            foreach (var methodDef in typeDef.Methods.ToArray())
+            if (method.HasMethodBody && m_DnlibDefCriticalAnalyzer.NotCriticalToMakeChanges(method))
             {
-                if (methodDef.HasBody && m_DnlibDefCriticalAnalyzer.NotCriticalToMakeChanges(methodDef))
+                for (var i = 0; i < method.CilMethodBody.Instructions.Count(); i++)
                 {
-                    for (var i = 0; i < methodDef.Body.Instructions.Count(); i++)
+                    if (method.CilMethodBody.Instructions[i].OpCode == CilOpCodes.Ldstr
+                        && method.CilMethodBody.Instructions[i].Operand is string content)
                     {
-                        if (methodDef.Body.Instructions[i].OpCode == OpCodes.Ldstr
-                            && methodDef.Body.Instructions[i].Operand is string content)
-                        {
-                            var encryptedContentBytes = Encryptor.EncryptContent(content, Data.SaltBytes, Data.CryptKeyBytes);
-                            var encryptedDataFieldDef = m_Injector.InjectInvisibleArray(context.ModuleDefMD, context.ModuleDefMD.GlobalType, encryptedContentBytes, m_Renamer.RenameUnsafely());
+                        var data = Encryptor.EncryptContent(content, Data.SaltBytes, Data.CryptKeyBytes);
+                        var encryptedDataFieldDef = m_Injector.InjectInvisibleArray(context.Module, globalModuleType, data, m_Renamer.RenameUnsafely());
 
-                            methodDef.Body.Instructions[i].ReplaceWith(OpCodes.Ldsfld, encryptedDataFieldDef);
-                            methodDef.Body.Instructions.Insert(i + 1, new Instruction(OpCodes.Ldsfld, saltBytesFieldDef));
-                            methodDef.Body.Instructions.Insert(i + 2, new Instruction(OpCodes.Ldsfld, cryptKeyFieldDef));
-                            methodDef.Body.Instructions.Insert(i + 3, new Instruction(OpCodes.Call, decryptMethodDef));
-                            i += 3;
-                        }
+                        method.CilMethodBody.Instructions[i].ReplaceWith(CilOpCodes.Ldsfld, encryptedDataFieldDef);
+                        method.CilMethodBody.Instructions.Insert(i + 1, new CilInstruction(CilOpCodes.Ldsfld, saltBytesField));
+                        method.CilMethodBody.Instructions.Insert(i + 2, new CilInstruction(CilOpCodes.Ldsfld, cryptKeyField));
+                        method.CilMethodBody.Instructions.Insert(i + 3, new CilInstruction(CilOpCodes.Call, decryptMethod));
+                        i += 3;
                     }
                 }
             }
