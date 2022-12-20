01bd32870956cb5828210514f27b90f5e1e3f58d
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-20 22:38:38 +0200

    Update NoNamespaces.cs

--- src/BitMono.Protections/NoNamespaces.cs
+++ src/BitMono.Protections/NoNamespaces.cs
@@ -1,22 +1,17 @@
 ﻿namespace BitMono.Protections;
 
-[ProtectionName(nameof(NoNamespaces))]
 public class NoNamespaces : IProtection
 {
     private readonly DnlibDefCriticalAnalyzer m_DnlibDefCriticalAnalyzer;
-    private readonly ILogger m_Logger;
 
-    public NoNamespaces(
-        DnlibDefCriticalAnalyzer typeDefCriticalAnalyzer, 
-        ILogger logger)
+    public NoNamespaces(DnlibDefCriticalAnalyzer typeDefCriticalAnalyzer)
     {
         m_DnlibDefCriticalAnalyzer = typeDefCriticalAnalyzer;
-        m_Logger = logger.ForContext<NoNamespaces>();
     }
 
     public Task ExecuteAsync(ProtectionContext context, ProtectionParameters parameters, CancellationToken cancellationToken = default)
     {
-        foreach (var typeDef in parameters.Targets.OfType<TypeDef>())
+        foreach (var typeDef in parameters.Targets.OfType<TypeDefinition>())
         {
             if (typeDef.IsGlobalModuleType == false
                 && m_DnlibDefCriticalAnalyzer.NotCriticalToMakeChanges(typeDef)
