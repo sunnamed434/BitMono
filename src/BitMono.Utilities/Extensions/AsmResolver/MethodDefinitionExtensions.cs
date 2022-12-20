cc6ee1857796386cb3406a58e951882a8a7206d2
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-20 15:54:36 +0200

    Create MethodDefinitionExtensions.cs

---
+++ src/BitMono.Utilities/Extensions/AsmResolver/MethodDefinitionExtensions.cs
@@ -0,0 +1,39 @@
+﻿namespace BitMono.Utilities.Extensions.AsmResolver;
+
+public static class MethodDefinitionExtensions
+{
+    public static bool IsAsync(this MethodDefinition source)
+    {
+        foreach (var typeDef in source.Module.GetAllTypes())
+        {
+            foreach (var nestedTypeDef in typeDef.NestedTypes.Where(n => n.Name.StartsWith("<")))
+            {
+                if (nestedTypeDef.IsValueType
+                    && nestedTypeDef.Interfaces.Any()
+                    && nestedTypeDef.Attributes == TypeAttributes.AutoLayout
+                    && nestedTypeDef.Interfaces.Any(i => i.Interface.Name.Equals(nameof(IAsyncStateMachine)))
+                    && nestedTypeDef.Name.Contains(source.Name))
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static bool NotAsync(this MethodDefinition source)
+    {
+        return source.IsAsync() == false;
+    }
+    public static bool IsGetterAndSetter(this MethodDefinition source)
+    {
+        return source.IsGetMethod == true && source.IsSetMethod == true;
+    }
+    public static bool NotGetterAndSetter(this MethodDefinition source)
+    {
+        return source.IsGetMethod == false && source.IsSetMethod == false;
+    }
+    public static bool HasParameters(this MethodDefinition source)
+    {
+        return source.Parameters.Any();
+    }
+}
