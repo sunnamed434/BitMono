dca2fffdafbce4347587324639922dedf54c6c3f
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-20 15:56:07 +0200

    Delete IMethodImplAttributeExcludingResolver.cs

--- src/BitMono.API/Protecting/Resolvers/IMethodImplAttributeExcludingResolver.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace BitMono.API.Protecting.Resolvers;
-
-public interface IMethodImplAttributeExcludingResolver
-{
-    bool TryResolve(IHasCustomAttribute from, out MethodImplAttribute obfuscationAttribute);
-}
