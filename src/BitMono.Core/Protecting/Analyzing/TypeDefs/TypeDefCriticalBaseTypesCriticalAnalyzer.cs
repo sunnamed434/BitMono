3d307551705cd42dcd3c73fc0c033e6db4414ea3
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-20 15:56:33 +0200

    Update TypeDefCriticalBaseTypesCriticalAnalyzer.cs

--- src/BitMono.Core/Protecting/Analyzing/TypeDefs/TypeDefCriticalBaseTypesCriticalAnalyzer.cs
+++ src/BitMono.Core/Protecting/Analyzing/TypeDefs/TypeDefCriticalBaseTypesCriticalAnalyzer.cs
@@ -1,6 +1,6 @@
 ﻿namespace BitMono.Core.Protecting.Analyzing.TypeDefs;
 
-public class TypeDefCriticalBaseTypesCriticalAnalyzer : ICriticalAnalyzer<TypeDef>
+public class TypeDefCriticalBaseTypesCriticalAnalyzer : ICriticalAnalyzer<TypeDefinition>
 {
     private readonly IConfiguration m_Configuration;
 
@@ -9,12 +9,13 @@ public class TypeDefCriticalBaseTypesCriticalAnalyzer : ICriticalAnalyzer<TypeDe
         m_Configuration = configuration.Configuration;
     }
 
-    public bool NotCriticalToMakeChanges(TypeDef typeDef)
+    public bool NotCriticalToMakeChanges(TypeDefinition typeDefinition)
     {
-        if (typeDef.HasBaseType())
+        if (typeDefinition.HasBaseType())
         {
             var criticalBaseTypes = m_Configuration.GetCriticalBaseTypes();
-            if (criticalBaseTypes.FirstOrDefault(c => c.StartsWith(typeDef.BaseType.TypeName.Split('`')[0])) != null)
+            Console.WriteLine("Base types: " + typeDefinition.BaseType.Name.Value);
+            if (criticalBaseTypes.FirstOrDefault(c => c.StartsWith(typeDefinition.BaseType.Name.Value.Split('`')[0])) != null)
             {
                 return false;
             }
