634d01ad37398a20be3060cbd647b216bcf1779f
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-20 15:57:10 +0200

    Create MethodImplAttributeExcludeResolver.cs

---
+++ src/BitMono.Core/Protecting/Resolvers/MethodImplAttributeExcludeResolver.cs
@@ -0,0 +1,35 @@
+﻿namespace BitMono.Core.Protecting.Resolvers;
+
+public class MethodImplAttributeExcludeResolver : IMethodImplAttributeExcludeResolver
+{
+    private readonly IAttemptAttributeResolver m_AttemptAttributeResolver;
+    private readonly IConfiguration m_Configuration;
+
+    public MethodImplAttributeExcludeResolver(IAttemptAttributeResolver attemptAttributeResolver, IBitMonoObfuscationConfiguration configuration)
+    {
+        m_AttemptAttributeResolver = attemptAttributeResolver;
+        m_Configuration = configuration.Configuration;
+    }
+
+    public bool TryResolve(IHasCustomAttribute from, [AllowNull] out MethodImplAttribute methodImplAttribute)
+    {
+        methodImplAttribute = null;
+        if (m_Configuration.GetValue<bool>(nameof(Obfuscation.NoInliningMethodObfuscationExcluding)) == false)
+        {
+            return false;
+        }
+        if (m_AttemptAttributeResolver.TryResolve(from, null, null, out methodImplAttribute) == false)
+        {
+            return false;
+        }
+        if (methodImplAttribute == null)
+        {
+            return false;
+        }
+        if (methodImplAttribute.Value == MethodImplOptions.NoInlining)
+        {
+            return false;
+        }
+        return true;
+    }
+}
