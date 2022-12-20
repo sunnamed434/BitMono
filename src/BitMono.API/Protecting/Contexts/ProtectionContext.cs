88774f71aef93f164b60e3df42b009b411024530
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-20 15:55:48 +0200

    Update ProtectionContext.cs

--- src/BitMono.API/Protecting/Contexts/ProtectionContext.cs
+++ src/BitMono.API/Protecting/Contexts/ProtectionContext.cs
@@ -1,16 +1,18 @@
-﻿namespace BitMono.API.Protecting.Contexts;
+﻿using AsmResolver.DotNet.Builder;
+using IAssemblyResolver = AsmResolver.DotNet.IAssemblyResolver;
+
+namespace BitMono.API.Protecting.Contexts;
 
 public class ProtectionContext
 {
-    [AllowNull] public ModuleDefMD ModuleDefMD { get; set; }
-    [AllowNull] public ModuleCreationOptions ModuleCreationOptions { get; set; }
-    [AllowNull] public ModuleWriterOptions ModuleWriterOptions { get; set; }
-    [AllowNull] public ModuleDefMD RuntimeModuleDefMD { get; set; }
-    [AllowNull] public Importer Importer { get; set; }
-    [AllowNull] public Importer RuntimeImporter { get; set; }
+    [AllowNull] public ModuleDefinition Module { get; set; }
+    [AllowNull] public ModuleReaderParameters ModuleReaderParameters { get; set; }
+    [AllowNull] public IPEImageBuilder PEImageBuilder { get; set; }
+    [AllowNull] public ModuleDefinition RuntimeModule { get; set; }
+    [AllowNull] public ReferenceImporter RuntimeImporter { get; set; }
     [AllowNull] public BitMonoContext BitMonoContext { get; set; }
-    [AllowNull] public byte[] ModuleDefMDOutput { get; set; }
+    [AllowNull] public byte[] ModuleOutput { get; set; }
 
-    [AllowNull] public ModuleContext ModuleContext => ModuleCreationOptions.Context;
-    [AllowNull] public AssemblyResolver AssemblyResolver => (AssemblyResolver)ModuleContext.AssemblyResolver;
+    [AllowNull] public IAssemblyResolver AssemblyResolver => Module.MetadataResolver.AssemblyResolver;
+    [AllowNull] public ReferenceImporter Importer => Module.DefaultImporter;
 }
