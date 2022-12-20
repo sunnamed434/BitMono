92d675a24d18a5f4471f8e5cd4108fb2e0f73f3a
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-20 15:57:40 +0200

    Create ObfuscationAttributeResolver.cs

---
+++ src/BitMono.Core/Protecting/Resolvers/ObfuscationAttributeResolver.cs
@@ -0,0 +1,25 @@
+﻿namespace BitMono.Core.Protecting.Resolvers;
+
+public class ObfuscationAttributeResolver : IObfuscationAttributeResolver
+{
+    private readonly IObfuscationAttributeExcludeResolver m_ObfuscationAttributeExcludingResolver;
+
+    public ObfuscationAttributeResolver(IObfuscationAttributeExcludeResolver obfuscationAttributeExcludingResolver)
+    {
+        m_ObfuscationAttributeExcludingResolver = obfuscationAttributeExcludingResolver;
+    }
+
+    public bool Resolve(string feature, IHasCustomAttribute from)
+    {
+        if (m_ObfuscationAttributeExcludingResolver.TryResolve(feature, from,
+            out ObfuscationAttribute obfuscationAttribute) && obfuscationAttribute.Exclude)
+        {
+            return true;
+        }
+        return false;
+    }
+    public bool Resolve<TFeature>(IHasCustomAttribute from) where TFeature : IProtection
+    {
+        return Resolve(typeof(TFeature).GetName(), from);
+    }
+}
