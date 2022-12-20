cd7fc2d5d843abeb1a9deeefd9a1654cab41dd9c
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-20 15:54:35 +0200

    Create ModuleDefinitionExtensions.cs

---
+++ src/BitMono.Utilities/Extensions/AsmResolver/ModuleDefinitionExtensions.cs
@@ -0,0 +1,39 @@
+﻿namespace BitMono.Utilities.Extensions.AsmResolver;
+
+public static class ModuleDefinitionExtensions
+{
+    public static TMember ResolveOrThrow<TMember>(this ModuleDefinition source, Type type)
+    {
+        if (source.TryLookupMember(new MetadataToken((uint)type.MetadataToken), out IMetadataMember metadataMember))
+        {
+            if (metadataMember is TMember member)
+            {
+                return member;
+            }
+        }
+        throw new ArgumentException($"Unable to resolve member {type.FullName}");
+    }
+    public static IEnumerable<IMemberDefinition> FindDefinitions(this ModuleDefinition source)
+    {
+        foreach (var typeDef in source.GetAllTypes())     
+        {
+            yield return typeDef;
+            foreach (var methodDef in typeDef.Methods)
+            {
+                yield return methodDef;
+            }
+            foreach (var fieldDef in typeDef.Fields)
+            {
+                yield return fieldDef;
+            }
+            foreach (var propertyDef in typeDef.Properties)
+            {
+                yield return propertyDef;
+            }
+            foreach (var eventDef in typeDef.Events)
+            {
+                yield return eventDef;
+            }
+        }
+    }
+}
