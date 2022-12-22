8c54b00b5d3248772c312bfbbeb258f236cdfd72
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-22 13:55:20 +0200

    Create AttributeCriticalAnalyzer.cs

---
+++ src/BitMono.Core/Protecting/Analyzing/TypeDefs/AttributeCriticalAnalyzer.cs
@@ -0,0 +1,32 @@
+﻿namespace BitMono.Core.Protecting.Analyzing.TypeDefs;
+
+public class AttributeCriticalAnalyzer : ICriticalAnalyzer<IHasCustomAttribute>
+{
+    private readonly IAttemptAttributeResolver m_AttemptAttributeResolver;
+
+    public AttributeCriticalAnalyzer(IAttemptAttributeResolver attemptAttributeResolver)
+    {
+        m_AttemptAttributeResolver = attemptAttributeResolver;
+    }
+
+    public bool NotCriticalToMakeChanges(IHasCustomAttribute customAttribute)
+    {
+        if (m_AttemptAttributeResolver.TryResolve<SerializableAttribute>(customAttribute, null, null, out _))
+        {
+            return false;
+        }
+        if (m_AttemptAttributeResolver.TryResolve<XmlAttributeAttribute>(customAttribute, null, null, out _))
+        {
+            return false;
+        }
+        if (m_AttemptAttributeResolver.TryResolve<XmlArrayItemAttribute>(customAttribute, null, null, out _))
+        {
+            return false;
+        }
+        if (m_AttemptAttributeResolver.TryResolve<JsonPropertyAttribute>(customAttribute, null, null, out _))
+        {
+            return false;
+        }
+        return false;
+    }
+}
