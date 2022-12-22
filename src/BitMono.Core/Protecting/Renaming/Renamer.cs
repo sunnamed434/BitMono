c2e27b62282f300bfe0ad52b51e738c414b6fb06
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-22 13:55:13 +0200

    Add new methods to Renamer and update it

--- src/BitMono.Core/Protecting/Renaming/Renamer.cs
+++ src/BitMono.Core/Protecting/Renaming/Renamer.cs
@@ -3,12 +3,17 @@
 public class Renamer : IRenamer
 {
     private readonly NameCriticalAnalyzer m_NameCriticalAnalyzer;
+    private readonly SpecificNamespaceCriticalAnalyzer m_SpecificNamespaceCriticalAnalyzer;
     private readonly IConfiguration m_Configuration;
     private readonly Random random = new Random();
 
-    public Renamer(NameCriticalAnalyzer nameCriticalAnalyzer, IBitMonoObfuscationConfiguration configuration)
+    public Renamer(
+        NameCriticalAnalyzer nameCriticalAnalyzer,
+        SpecificNamespaceCriticalAnalyzer specificNamespaceCriticalAnalyzer,
+        IBitMonoObfuscationConfiguration configuration)
     {
         m_NameCriticalAnalyzer = nameCriticalAnalyzer;
+        m_SpecificNamespaceCriticalAnalyzer = specificNamespaceCriticalAnalyzer;
         m_Configuration = configuration.Configuration;
     }
 
@@ -20,36 +25,65 @@ public class Renamer : IRenamer
         var randomStringThree = strings[random.Next(0, strings.Length - 1)];
         return $"{randomStringTwo} {randomStringOne}.{randomStringThree}";
     }
-    public void Rename(IMetadataMember metadataMember)
+    public void Rename(object @object)
     {
-        if (metadataMember is TypeDefinition type)
+        if (@object is TypeDefinition type)
         {
             if (m_NameCriticalAnalyzer.NotCriticalToMakeChanges(type))
             {
                 type.Name = RenameUnsafely();
             }
         }
-        if (metadataMember is MethodDefinition method)
+        if (@object is MethodDefinition method)
         {
             if (m_NameCriticalAnalyzer.NotCriticalToMakeChanges(method))
             {
                 method.Name = RenameUnsafely();
             }
         }
-        if (metadataMember is FieldDefinition field)
+        if (@object is FieldDefinition field)
         {
             field.Name = RenameUnsafely();
         }
-        if (metadataMember is ParameterDefinition parameter)
+        if (@object is ParameterDefinition parameter)
         {
             parameter.Name = RenameUnsafely();
         }
     }
-    public void Rename(params IMetadataMember[] metadataMembers)
+    public void Rename(params object[] objects)
     {
-        for (int i = 0; i < metadataMembers.Length; i++)
+        for (int i = 0; i < objects.Length; i++)
         {
-            Rename(metadataMembers[i]);
+            Rename(objects[i]);
         }
+    }
+    public void RemoveNamespace(object @object)
+    {
+        if (@object is TypeDefinition type)
+        {
+            if (m_SpecificNamespaceCriticalAnalyzer.NotCriticalToMakeChanges(type))
+            {
+                type.Namespace = string.Empty;
+            }
+        }
+        if (@object is MethodDefinition method)
+        {
+            if (m_NameCriticalAnalyzer.NotCriticalToMakeChanges(method))
+            {
+                method.Name = RenameUnsafely();
+            }
+        }
+        if (@object is FieldDefinition field)
+        {
+            field.Name = RenameUnsafely();
+        }
+        if (@object is ParameterDefinition parameter)
+        {
+            parameter.Name = RenameUnsafely();
+        }
+    }
+    public void RemoveNamespace(params object[] objects)
+    {
+        objects.ForEach(@object => RemoveNamespace(@object));
     }
 }
