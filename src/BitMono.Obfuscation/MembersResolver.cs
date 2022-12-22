fa7402d11167166eb6e6801f23b27334caf86016
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-22 23:37:58 +0200

    Create MembersResolver.cs

---
+++ src/BitMono.Obfuscation/MembersResolver.cs
@@ -0,0 +1,18 @@
+﻿namespace BitMono.Obfuscation;
+
+public class MembersResolver
+{
+    public IEnumerable<IMemberDefinition> Resolve(string feature, IEnumerable<IMemberDefinition> definitions, IEnumerable<IMemberDefinitionfResolver> resolvers)
+    {
+        foreach (var definition in definitions) 
+        {
+            foreach (var resolver in resolvers)
+            {
+                if (resolver.Resolve(feature, definition))
+                {
+                    yield return definition;
+                }
+            }
+        }
+    }
+}
