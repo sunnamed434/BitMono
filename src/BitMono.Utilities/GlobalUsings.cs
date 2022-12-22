00c9ed1d97f8a2381b5e6d3b1b091756f49b9c76
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-22 13:53:20 +0200

    Update GlobalUsings.cs

--- src/BitMono.Utilities/GlobalUsings.cs
+++ src/BitMono.Utilities/GlobalUsings.cs
@@ -1,10 +1,13 @@
 ﻿global using AsmResolver;
 global using AsmResolver.DotNet;
+global using AsmResolver.DotNet.Cloning;
 global using AsmResolver.DotNet.Code.Cil;
 global using AsmResolver.PE.DotNet.Cil;
 global using AsmResolver.PE.DotNet.Metadata.Tables;
 global using AsmResolver.PE.DotNet.Metadata.Tables.Rows;
+global using BitMono.API.Protecting.Renaming;
 global using MoreLinq;
+global using NullGuard;
 global using System;
 global using System.Collections.Generic;
 global using System.Linq;
