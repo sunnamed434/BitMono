5fc6d30d60260bcc607c9f64c9d14ffb461cb629
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-22 13:53:45 +0200

    Add CloningExtensions for Cloning to easy manipulate with it

---
+++ src/BitMono.Utilities/Extensions/AsmResolver/CloningExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace BitMono.Utilities.Extensions.AsmResolver;
+
+public static class CloningExtensions
+{
+    public static MemberCloneResult RenameClonedMembers(this MemberCloneResult source, IRenamer renamer)
+    {
+        source.ClonedMembers.ForEach(member => renamer.Rename(member));
+        return source;
+    }
+    public static MemberCloneResult RemoveNamespaceOfClonedMembers(this MemberCloneResult source, IRenamer renamer)
+    {
+        source.ClonedMembers.ForEach(member => renamer.RemoveNamespace(member));
+        return source;
+    }
+}
