83cae76b5ca93f1e887c28694b3a67c78401060b
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-23 17:18:18 +0200

    Update BitMonoAssemblyResolver.cs

--- src/BitMono.Core/Protecting/Resolvers/BitMonoAssemblyResolver.cs
+++ src/BitMono.Core/Protecting/Resolvers/BitMonoAssemblyResolver.cs
@@ -14,12 +14,20 @@ public class BitMonoAssemblyResolver
         cancellationToken.ThrowIfCancellationRequested();
 
         var resolvingSucceed = true;
-        /*foreach (var dependencyData in dependenciesData)
+        foreach (var dependencyData in dependenciesData)
         {
             cancellationToken.ThrowIfCancellationRequested();
 
-            context.AssemblyResolver.AddToCache(AssemblyDefinition.FromBytes(dependencyData), context.Module.Assembly);
-        }*/
+            try
+            {
+                context.AssemblyResolver.AddToCache(context.Module.Assembly, AssemblyDefinition.FromBytes(dependencyData));
+                Console.WriteLine("resolved!");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
         foreach (var assemblyReference in context.Module.AssemblyReferences)
         {
             cancellationToken.ThrowIfCancellationRequested();
@@ -27,12 +35,16 @@ public class BitMonoAssemblyResolver
             try
             {
                 m_Logger.Information("Resolving assembly: " + assemblyReference.Name);
-                context.AssemblyResolver.Resolve(assemblyReference);
+                var assembly = context.AssemblyResolver.Resolve(assemblyReference);
+                if (assembly == null)
+                {
+                    m_Logger.Error("Failed to resolve dependency {0}", assemblyReference.FullName);
+                }
             }
             catch (Exception ex)
             {
                 resolvingSucceed = false;
-                m_Logger.Error("Failed to resolve dependency {0}, message: ", assemblyReference.FullName, ex.Message);
+                m_Logger.Error("Failed to resolve dependency {0}, error: {1}", assemblyReference.FullName, ex.Message);
             }
         }
         return resolvingSucceed;
