e602accd32e451bbac755ed42a63550955816947
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-26 19:48:05 +0200

    Delete ICustomAttributesResolver.cs

--- src/BitMono.API/Protecting/Resolvers/ICustomAttributesResolver.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace BitMono.API.Protecting.Resolvers;
-
-public interface ICustomAttributesResolver
-{
-    IEnumerable<TAttribute> Resolve<TAttribute>(IHasCustomAttribute from) where TAttribute : Attribute;
-}
