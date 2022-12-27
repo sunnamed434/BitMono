6ef384d7252a8833f1d158b16bde5ba8e64fa987
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-27 14:55:42 +0200

    Create SafeToMakeChangesMemberResolver.cs

---
+++ src/BitMono.Core/Protecting/Resolvers/SafeToMakeChangesMemberResolver.cs
@@ -0,0 +1,33 @@
+﻿using BitMono.Core.Protecting.Analyzing;
+
+namespace BitMono.Core.Protecting.Resolvers;
+
+public class SafeToMakeChangesMemberResolver : IMemberResolver
+{
+    private readonly IObfuscationAttributeResolver m_ObfuscationAttributeResolver;
+    private readonly SpecificNamespaceCriticalAnalyzer m_SpecificNamespaceCriticalAnalyzer;
+
+    public SafeToMakeChangesMemberResolver(
+        IObfuscationAttributeResolver obfuscationAttributeResolver,
+        SpecificNamespaceCriticalAnalyzer specificNamespaceCriticalAnalyzer)
+    {
+        m_ObfuscationAttributeResolver = obfuscationAttributeResolver;
+        m_SpecificNamespaceCriticalAnalyzer = specificNamespaceCriticalAnalyzer;
+    }
+
+    public bool Resolve(string feature, IMetadataMember member)
+    {
+        if (member is IHasCustomAttribute from)
+        {
+            if (m_ObfuscationAttributeResolver.Resolve(feature, from))
+            {
+                return false;
+            }
+        }
+        if (m_SpecificNamespaceCriticalAnalyzer.NotCriticalToMakeChanges(member) == false)
+        {
+            return false;
+        }
+        return true;
+    }
+}
