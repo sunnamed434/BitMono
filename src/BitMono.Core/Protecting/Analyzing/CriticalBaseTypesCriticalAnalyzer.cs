56c4562b42473be5a25d7a8249a6413b5037c134
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-27 14:57:10 +0200

    Create CriticalBaseTypesCriticalAnalyzer.cs

---
+++ src/BitMono.Core/Protecting/Analyzing/CriticalBaseTypesCriticalAnalyzer.cs
@@ -0,0 +1,24 @@
+﻿namespace BitMono.Core.Protecting.Analyzing;
+
+public class CriticalBaseTypesCriticalAnalyzer : ICriticalAnalyzer<TypeDefinition>
+{
+    private readonly IConfiguration m_Configuration;
+
+    public CriticalBaseTypesCriticalAnalyzer(IBitMonoCriticalsConfiguration configuration)
+    {
+        m_Configuration = configuration.Configuration;
+    }
+
+    public bool NotCriticalToMakeChanges(TypeDefinition typeDefinition)
+    {
+        if (typeDefinition.HasBaseType())
+        {
+            var criticalBaseTypes = m_Configuration.GetCriticalBaseTypes();
+            if (criticalBaseTypes.FirstOrDefault(c => c.StartsWith(typeDefinition.BaseType.Name.Value.Split('`')[0])) != null)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
