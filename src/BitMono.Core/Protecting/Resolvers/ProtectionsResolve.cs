e0f7528f0971c736d2749768dc643f8a1e9e5557
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-27 14:55:49 +0200

    Create ProtectionsResolve.cs

---
+++ src/BitMono.Core/Protecting/Resolvers/ProtectionsResolve.cs
@@ -0,0 +1,7 @@
+﻿namespace BitMono.Core.Protecting.Resolvers;
+
+public class ProtectionsResolve
+{
+    public List<IProtection> FoundProtections { get; set; }
+    public List<string> DisabledProtections { get; set; }
+}
