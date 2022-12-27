b8094800d7f970a2077dde2654becabcefd851ea
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-27 14:55:59 +0200

    Update GlobalUsings.cs

--- src/BitMono.API/GlobalUsings.cs
+++ src/BitMono.API/GlobalUsings.cs
@@ -1,15 +1,16 @@
 ﻿global using AsmResolver.DotNet;
+global using AsmResolver.DotNet.Builder;
 global using AsmResolver.DotNet.Serialized;
 global using Autofac.Extensions.DependencyInjection;
 global using BitMono.API.Protecting.Contexts;
 global using BitMono.API.Protecting.Pipeline;
+global using BitMono.Shared.Models;
 global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
 global using NullGuard;
 global using System;
 global using System.Collections.Generic;
-global using System.Reflection;
-global using System.Runtime.CompilerServices;
 global using System.Threading;
 global using System.Threading.Tasks;
+global using IAssemblyResolver = AsmResolver.DotNet.IAssemblyResolver;
 global using IModule = Autofac.Core.IModule;
