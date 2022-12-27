28490070923ec3359f602f04270925adae5ac15a
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-27 14:57:16 +0200

    Update IObfuscationAttributeResolver.cs

--- src/BitMono.API/Protecting/Resolvers/IObfuscationAttributeResolver.cs
+++ src/BitMono.API/Protecting/Resolvers/IObfuscationAttributeResolver.cs
@@ -3,5 +3,7 @@
 public interface IObfuscationAttributeResolver
 {
     bool Resolve(string feature, IHasCustomAttribute from);
+    bool Resolve(IHasCustomAttribute from);
+    bool Resolve(Type featureType, IHasCustomAttribute from);
     bool Resolve<TFeature>(IHasCustomAttribute from) where TFeature : IProtection;
 }
