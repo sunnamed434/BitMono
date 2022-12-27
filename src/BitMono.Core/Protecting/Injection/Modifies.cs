194cdeb8abf86908f5c7986fc92d45a83077a9cf
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-27 14:56:33 +0200

    Create Modifies.cs

---
+++ src/BitMono.Core/Protecting/Injection/Modifies.cs
@@ -0,0 +1,10 @@
+﻿namespace BitMono.Core.Protecting.Injection;
+
+[Flags]
+public enum Modifies
+{
+    None = 0,
+    Rename = 0x1,
+    RemoveNamespace = 0x2,
+    RenameAndRemoveNamespace = Rename | RemoveNamespace,
+}
