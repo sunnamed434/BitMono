ac65a8f2ee51e3461168121447c4850217c489a6
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-28 22:49:37 +0200

    Create ProtectionsSort.cs

---
+++ src/BitMono.Obfuscation/ProtectionsSort.cs
@@ -0,0 +1,14 @@
+﻿namespace BitMono.Obfuscation;
+
+public class ProtectionsSort
+{
+    public ICollection<IProtection> FoundProtections { get; set; }
+    public ICollection<IProtection> SortedProtections { get; set; }
+    public ICollection<IPacker> Packers { get; set; }
+    public IEnumerable<IProtection> DeprecatedProtections { get; set; }
+    public ICollection<string> DisabledProtections { get; set; }
+    public IEnumerable<IStageProtection> StageProtections { get; set; }
+    public IEnumerable<IPipelineProtection> PipelineProtections { get; set; }
+    public IEnumerable<IProtection> ObfuscationAttributeExcludingProtections { get; set; }
+    public bool HasProtections { get; set; }
+}
