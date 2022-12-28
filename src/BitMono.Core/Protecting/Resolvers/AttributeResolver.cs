26ecb4562622b992167a97813bb741c3f5283b13
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-28 22:50:00 +0200

    Create AttributeResolver.cs

---
+++ src/BitMono.Core/Protecting/Resolvers/AttributeResolver.cs
@@ -0,0 +1,22 @@
+﻿namespace BitMono.Core.Protecting.Resolvers;
+
+public abstract class AttributeResolver : IAttributeResolver
+{
+    public abstract bool Resolve(string feature, IHasCustomAttribute from, out CustomAttributeResolve attributeResolve);
+    public virtual bool Resolve(string feature, IHasCustomAttribute from)
+    {
+        return Resolve(feature, from, out _);
+    }
+    public virtual bool Resolve(IHasCustomAttribute from)
+    {
+        return Resolve(feature: null, from);
+    }
+    public virtual bool Resolve(Type featureType, IHasCustomAttribute from)
+    {
+        return Resolve(featureType.GetName(), from);
+    }
+    public virtual bool Resolve<TFeature>(IHasCustomAttribute from) where TFeature : IProtection
+    {
+        return Resolve(typeof(TFeature), from);
+    }
+}
