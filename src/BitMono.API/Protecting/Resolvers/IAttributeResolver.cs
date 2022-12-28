04e95d1a9affc75403377513afff31a211e0b87a
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-28 22:50:37 +0200

    Create IAttributeResolver.cs

---
+++ src/BitMono.API/Protecting/Resolvers/IAttributeResolver.cs
@@ -0,0 +1,10 @@
+﻿namespace BitMono.API.Protecting.Resolvers;
+
+public interface IAttributeResolver
+{
+    bool Resolve([AllowNull] string feature, IHasCustomAttribute from, [AllowNull] out CustomAttributeResolve attributeResolve);
+    bool Resolve([AllowNull] string feature, IHasCustomAttribute from);
+    bool Resolve(IHasCustomAttribute from);
+    bool Resolve(Type featureType, IHasCustomAttribute from);
+    bool Resolve<TFeature>(IHasCustomAttribute from) where TFeature : IProtection;
+}
