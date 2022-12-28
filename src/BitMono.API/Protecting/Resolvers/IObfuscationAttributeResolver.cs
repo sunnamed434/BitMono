0956ada9e7a33e6e4c35527afb13d225f25129df
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-28 22:50:28 +0200

    Delete IObfuscationAttributeResolver.cs

--- src/BitMono.API/Protecting/Resolvers/IObfuscationAttributeResolver.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BitMono.API.Protecting.Resolvers;
-
-public interface IObfuscationAttributeResolver
-{
-    bool Resolve(string feature, IHasCustomAttribute from);
-    bool Resolve(IHasCustomAttribute from);
-    bool Resolve(Type featureType, IHasCustomAttribute from);
-    bool Resolve<TFeature>(IHasCustomAttribute from) where TFeature : IProtection;
-}
