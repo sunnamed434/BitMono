349cbea8ceaf7edc1a517710cf08228c8e4caa41
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-28 22:50:12 +0200

    Create CustomAttributeResolveValueExtensions.cs

---
+++ src/BitMono.Core/Extensions/Protections/CustomAttributeResolveValueExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace BitMono.Core.Extensions.Protections;
+
+public static class CustomAttributeResolveValueExtensions
+{
+    public static bool TryGetValueOrDefault(this Dictionary<string, CustomAttributeResolve> source, string key, out CustomAttributeResolve attributeResolve, bool defaultValue = false)
+    {
+        var value = defaultValue;
+        if (source.TryGetValue(key, out attributeResolve))
+        {
+            if (attributeResolve.Value is bool resolveValue)
+            {
+                value = resolveValue;
+            }
+        }
+        return value;
+    }
+    public static bool TryGetValueOrDefault(this Dictionary<string, CustomAttributeResolve> source, string key, bool defaultValue = false)
+    {
+        return TryGetValueOrDefault(source, key, out _, defaultValue);
+    }
+}
