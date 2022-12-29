d860a43cd27d5b1a77f8a60ed4dab5b5f9d5091f
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-29 19:34:30 +0200

    Create CriticalMethodsCriticalAnalyzer.cs

---
+++ src/BitMono.Core/Protecting/Analyzing/CriticalMethodsCriticalAnalyzer.cs
@@ -0,0 +1,25 @@
+﻿namespace BitMono.Core.Protecting.Analyzing;
+
+public class CriticalMethodsCriticalAnalyzer : ICriticalAnalyzer<MethodDefinition>
+{
+    private readonly IConfiguration m_Configuration;
+
+    public CriticalMethodsCriticalAnalyzer(IBitMonoCriticalsConfiguration configuration)
+    {
+        m_Configuration = configuration.Configuration;
+    }
+
+    public bool NotCriticalToMakeChanges(MethodDefinition method)
+    {
+        if (m_Configuration.GetValue<bool>("UseCriticalMethods") == false)
+        {
+            return true;
+        }
+        var criticalMethodNames = m_Configuration.GetCriticalMethods();
+        if (criticalMethodNames.Any(c => c.Equals(method.Name)) == false)
+        {
+            return true;
+        }
+        return false;
+    }
+}
