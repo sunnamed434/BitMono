2079fb8eac0dab316d342e5dc0b3a098bf32b26a
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-29 19:35:16 +0200

    Update RuntimeCriticalAnalyzer.cs

--- src/BitMono.Core/Protecting/Analyzing/RuntimeCriticalAnalyzer.cs
+++ src/BitMono.Core/Protecting/Analyzing/RuntimeCriticalAnalyzer.cs
@@ -1,26 +1,26 @@
 ﻿namespace BitMono.Core.Protecting.Analyzing;
 
-public class RuntimeCriticalAnalyzer : ICriticalAnalyzer<IMemberDefinition>
+public class RuntimeCriticalAnalyzer : ICriticalAnalyzer<IMetadataMember>
 {
-    public bool NotCriticalToMakeChanges(IMemberDefinition memberDefinition)
+    public bool NotCriticalToMakeChanges(IMetadataMember member)
     {
-        if (memberDefinition is TypeDefinition type)
+        if (member is TypeDefinition type)
         {
             return type.IsRuntimeSpecialName == false;
         }
-        if (memberDefinition is FieldDefinition field)
+        if (member is FieldDefinition field)
         {
             return field.IsRuntimeSpecialName == false
                 && field.IsLiteral == false
                 && field.DeclaringType.IsEnum == false;
         }
-        if (memberDefinition is MethodDefinition method)
+        if (member is MethodDefinition method)
         {
             return method.IsRuntimeSpecialName && method.DeclaringType.IsForwarder
                 ? false
                 : true;
         }
-        if (memberDefinition is EventDefinition @event)
+        if (member is EventDefinition @event)
         {
             return @event.IsRuntimeSpecialName == false;
         }
