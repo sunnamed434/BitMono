4fbf9c48a000784402003a7cbd0442e2c4b77be1
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-29 19:35:10 +0200

    Update NoNamespaces.cs

--- src/BitMono.Protections/NoNamespaces.cs
+++ src/BitMono.Protections/NoNamespaces.cs
@@ -1,21 +1,13 @@
-﻿using BitMono.Core.Protecting.Analyzing;
+﻿namespace BitMono.Protections;
 
-namespace BitMono.Protections;
-
+[DoNotResolve(Members.SpecialRuntime)]
 public class NoNamespaces : IProtection
 {
-    private readonly RuntimeCriticalAnalyzer m_RuntimeCriticalAnalyzer;
-
-    public NoNamespaces(RuntimeCriticalAnalyzer runtimeCriticalAnalyzer)
-    {
-        m_RuntimeCriticalAnalyzer = runtimeCriticalAnalyzer;
-    }
-
     public Task ExecuteAsync(ProtectionContext context, ProtectionParameters parameters, CancellationToken cancellationToken = default)
     {
         foreach (var type in parameters.Targets.OfType<TypeDefinition>())
         {
-            if (type.HasNamespace() && m_RuntimeCriticalAnalyzer.NotCriticalToMakeChanges(type))
+            if (type.HasNamespace())
             {
                 type.Namespace = string.Empty;
             }
