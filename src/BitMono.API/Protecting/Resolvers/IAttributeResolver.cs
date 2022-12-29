cf8bb843d2d5805e736313435170067cfc8a8d4d
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-29 19:33:51 +0200

    Update IAttributeResolver.cs

--- src/BitMono.API/Protecting/Resolvers/IAttributeResolver.cs
+++ src/BitMono.API/Protecting/Resolvers/IAttributeResolver.cs
@@ -2,8 +2,8 @@
 
 public interface IAttributeResolver
 {
-    bool Resolve([AllowNull] string feature, IHasCustomAttribute from, [AllowNull] out CustomAttributeResolve attributeResolve);
-    bool Resolve([AllowNull] string feature, IHasCustomAttribute from);
+    bool Resolve(string feature, IHasCustomAttribute from, out CustomAttributeResolve attributeResolve);
+    bool Resolve(string feature, IHasCustomAttribute from);
     bool Resolve(IHasCustomAttribute from);
     bool Resolve(Type featureType, IHasCustomAttribute from);
     bool Resolve<TFeature>(IHasCustomAttribute from) where TFeature : IProtection;
