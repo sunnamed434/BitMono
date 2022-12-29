122cb019ad29f7a745728d4afd6c28ca896c46bf
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-29 19:34:35 +0200

    Update SafeToMakeChangesMemberResolver.cs

--- src/BitMono.Core/Protecting/Resolvers/SafeToMakeChangesMemberResolver.cs
+++ src/BitMono.Core/Protecting/Resolvers/SafeToMakeChangesMemberResolver.cs
@@ -1,25 +1,31 @@
-﻿using BitMono.Core.Protecting.Analyzing;
-
-namespace BitMono.Core.Protecting.Resolvers;
+﻿namespace BitMono.Core.Protecting.Resolvers;
 
 public class SafeToMakeChangesMemberResolver : IMemberResolver
 {
     private readonly ObfuscationAttributeResolver m_ObfuscationAttributeResolver;
+    private readonly MethodImplAttributeResolver m_MethodImplAttributeResolver;
     private readonly SpecificNamespaceCriticalAnalyzer m_SpecificNamespaceCriticalAnalyzer;
 
     public SafeToMakeChangesMemberResolver(
         ObfuscationAttributeResolver obfuscationAttributeResolver,
+        MethodImplAttributeResolver methodImplAttributeResolver,
         SpecificNamespaceCriticalAnalyzer specificNamespaceCriticalAnalyzer)
     {
         m_ObfuscationAttributeResolver = obfuscationAttributeResolver;
+        m_MethodImplAttributeResolver = methodImplAttributeResolver;
         m_SpecificNamespaceCriticalAnalyzer = specificNamespaceCriticalAnalyzer;
     }
 
-    public bool Resolve(string feature, IMetadataMember member)
+    public bool Resolve(IProtection protection, IMetadataMember member)
     {
-        if (member is IHasCustomAttribute from)
+        var feature = protection.GetName();
+        if (member is IHasCustomAttribute customAttribute)
         {
-            if (m_ObfuscationAttributeResolver.Resolve(feature, from))
+            if (m_ObfuscationAttributeResolver.Resolve(feature, customAttribute))
+            {
+                return false;
+            }
+            if (m_MethodImplAttributeResolver.Resolve(feature, customAttribute))
             {
                 return false;
             }
