4995b4ffc1c4b18fc98554f8fd339cb14c1dd2f6
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-29 19:35:18 +0200

    Create DoNotResolveAttribute.cs

---
+++ src/BitMono.Core/Protecting/Attributes/DoNotResolveAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace BitMono.Core.Protecting.Attributes;
+
+[AttributeUsage(AttributeTargets.Class, Inherited = false)]
+public class DoNotResolveAttribute : Attribute
+{
+    public DoNotResolveAttribute(Members resolves)
+    {
+        Resolves = resolves;
+    }
+
+    public Members Resolves { get; }
+}
