a405f236876ed124fabd4a31b266272ce8ffad00
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-12-29 19:35:26 +0200

    Update FullRenamer.cs

--- src/BitMono.Protections/FullRenamer.cs
+++ src/BitMono.Protections/FullRenamer.cs
@@ -1,5 +1,6 @@
 ﻿namespace BitMono.Protections;
 
+[DoNotResolve(Members.SpecialRuntime | Members.Model)]
 public class FullRenamer : IProtection
 {
     private readonly RuntimeCriticalAnalyzer m_RuntimeCriticalAnalyzer;
@@ -18,15 +19,18 @@ public class FullRenamer : IProtection
         var moduleType = context.Module.GetOrCreateModuleType();
         foreach (var type in parameters.Targets.OfType<TypeDefinition>())
         {
+            Console.WriteLine("-------------------> PREPARE TO RENAME THE TYPE: " + type.FullName);
             if (type != moduleType
                 && m_RuntimeCriticalAnalyzer.NotCriticalToMakeChanges(type)
                 && m_ModelAttributelCriticalAnalyzer.NotCriticalToMakeChanges(type))
             {
+                Console.WriteLine("-------------------> RENAME TYPE: " + type.FullName);
                 m_Renamer.Rename(type);
                 foreach (var field in type.Fields.ToArray())
                 {
                     if (m_RuntimeCriticalAnalyzer.NotCriticalToMakeChanges(field))
                     {
+                        Console.WriteLine("-------------------> RENAME field: " + field.FullName);
                         m_Renamer.Rename(field);
                     }
                 }
@@ -36,11 +40,13 @@ public class FullRenamer : IProtection
                         && method.IsVirtual == false
                         && m_RuntimeCriticalAnalyzer.NotCriticalToMakeChanges(method))
                     {
+                        Console.WriteLine("-------------------> RENAME method: " + method.FullName);
                         m_Renamer.Rename(method);
                         if (method.HasParameters())
                         {
                             foreach (var parameter in method.Parameters.ToArray())
                             {
+                                Console.WriteLine("-------------------> RENAME parameter: " + parameter.Name);
                                 m_Renamer.Rename(parameter.Definition);
                             }
                         }
