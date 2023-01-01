f6c8dc5df016601e18ec8b44653278cbc854a15b
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2023-01-01 21:20:51 +0200

    Update NameCriticalAnalyzer.cs

--- src/BitMono.Core/Protecting/Analyzing/NameCriticalAnalyzer.cs
+++ src/BitMono.Core/Protecting/Analyzing/NameCriticalAnalyzer.cs
@@ -4,39 +4,36 @@ public class NameCriticalAnalyzer :
     ICriticalAnalyzer<TypeDefinition>,
     ICriticalAnalyzer<MethodDefinition>
 {
-    private readonly CriticalInterfacesCriticalAnalyzer m_TypeDefCriticalInterfacesCriticalAnalyzer;
-    private readonly CriticalBaseTypesCriticalAnalyzer m_TypeDefCriticalBaseTypesCriticalAnalyzer;
-    private readonly IConfiguration m_Configuration;
+    private readonly CriticalInterfacesCriticalAnalyzer m_CriticalInterfacesCriticalAnalyzer;
+    private readonly CriticalBaseTypesCriticalAnalyzer m_CriticalBaseTypesCriticalAnalyzer;
+    private readonly CriticalMethodsCriticalAnalyzer m_CriticalMethodsCriticalAnalyzer;
 
     public NameCriticalAnalyzer(
-        CriticalInterfacesCriticalAnalyzer typeDefCriticalInterfacesCriticalAnalyzer,
-        CriticalBaseTypesCriticalAnalyzer typeDefCriticalBaseTypesCriticalAnalyzer,
-        IBitMonoCriticalsConfiguration configuration)
+        CriticalInterfacesCriticalAnalyzer criticalInterfacesCriticalAnalyzer,
+        CriticalBaseTypesCriticalAnalyzer criticalBaseTypesCriticalAnalyzer,
+        CriticalMethodsCriticalAnalyzer criticalMethodsCriticalAnalyzer)
     {
-        m_TypeDefCriticalInterfacesCriticalAnalyzer = typeDefCriticalInterfacesCriticalAnalyzer;
-        m_TypeDefCriticalBaseTypesCriticalAnalyzer = typeDefCriticalBaseTypesCriticalAnalyzer;
-        m_Configuration = configuration.Configuration;
+        m_CriticalInterfacesCriticalAnalyzer = criticalInterfacesCriticalAnalyzer;
+        m_CriticalBaseTypesCriticalAnalyzer = criticalBaseTypesCriticalAnalyzer;
+        m_CriticalMethodsCriticalAnalyzer = criticalMethodsCriticalAnalyzer;
     }
 
-    public bool NotCriticalToMakeChanges(TypeDefinition typeDefinition)
+    public bool NotCriticalToMakeChanges(TypeDefinition type)
     {
-        if (m_TypeDefCriticalInterfacesCriticalAnalyzer.NotCriticalToMakeChanges(typeDefinition) == false)
+        if (m_CriticalInterfacesCriticalAnalyzer.NotCriticalToMakeChanges(type) == false)
         {
+            Console.WriteLine("-----------------------------------------------------> CRITICAL TO RENAME TYPE... SAD");
             return false;
         }
-        if (m_TypeDefCriticalBaseTypesCriticalAnalyzer.NotCriticalToMakeChanges(typeDefinition) == false)
+        if (m_CriticalBaseTypesCriticalAnalyzer.NotCriticalToMakeChanges(type) == false)
         {
+            Console.WriteLine("-----------------------------------------------------> CRITICAL TO RENAME BASE TYPE... SAD");
             return false;
         }
         return true;
     }
-    public bool NotCriticalToMakeChanges(MethodDefinition methodDefinition)
+    public bool NotCriticalToMakeChanges(MethodDefinition method)
     {
-        var criticalMethodNames = m_Configuration.GetCriticalMethods();
-        if (criticalMethodNames.Any(c => c.Equals(methodDefinition.Name)))
-        {
-            return false;
-        }
-        return true;
+        return m_CriticalMethodsCriticalAnalyzer.NotCriticalToMakeChanges(method);
     }
 }
