d59b75aac60cc6648c1e78a335db53c8fb7795d9
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2023-01-01 21:20:34 +0200

    Update MethodImplAttributeResolver.cs

--- src/BitMono.Core/Protecting/Resolvers/MethodImplAttributeResolver.cs
+++ src/BitMono.Core/Protecting/Resolvers/MethodImplAttributeResolver.cs
@@ -4,11 +4,15 @@ public class MethodImplAttributeResolver : AttributeResolver
 {
     private readonly IConfiguration m_Configuration;
     private readonly IAttemptAttributeResolver m_AttemptAttributeResolver;
+    private readonly string m_AttributeNamespace;
+    private readonly string m_AttributeName;
 
     public MethodImplAttributeResolver(IBitMonoObfuscationConfiguration configuration, IAttemptAttributeResolver attemptAttributeResolver) 
     {
         m_Configuration = configuration.Configuration;
         m_AttemptAttributeResolver = attemptAttributeResolver;
+        m_AttributeNamespace = typeof(MethodImplAttribute).Namespace;
+        m_AttributeName = nameof(MethodImplAttribute);
     }
 
     public override bool Resolve([AllowNull] string feature, IHasCustomAttribute from, [AllowNull] out CustomAttributeResolve attributeResolve)
@@ -18,7 +22,7 @@ public class MethodImplAttributeResolver : AttributeResolver
         {
             return false;
         }
-        if (m_AttemptAttributeResolver.TryResolve(from, typeof(MethodImplAttribute), out Dictionary<string, CustomAttributeResolve> keyValuePairs) == false)
+        if (m_AttemptAttributeResolver.TryResolve(from, m_AttributeNamespace, m_AttributeName, out Dictionary<string, CustomAttributeResolve> keyValuePairs) == false)
         {
             return false;
         }
