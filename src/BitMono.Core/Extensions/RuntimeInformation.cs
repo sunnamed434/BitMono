ed11654573c983614ee49c694679e7ac7072c3b1
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2023-01-02 22:13:44 +0200

    Create RuntimeInformation.cs

---
+++ src/BitMono.Core/Extensions/RuntimeInformation.cs
@@ -0,0 +1,24 @@
+﻿namespace BitMono.Core.Extensions;
+
+public class RuntimeInformation
+{
+    [AllowNull]
+    public Version DotnetFrameworkVersion { get; set; }
+    [AllowNull]
+    public OperatingSystem OperatingSystem { get; set; }
+    [AllowNull]
+    public int Bits { get; set; }
+    [AllowNull]
+    public bool HasMono { get; set; }
+    [AllowNull]
+    public Type MonoType { get; set; }
+    [AllowNull]
+    public string MonoDisplayName { get; set; }
+
+    public override string ToString()
+    {
+        return string.Format("Running on {0}, {1}, x{2} bits", OperatingSystem, HasMono == false
+            ? ".NET Framework v" + DotnetFrameworkVersion
+            : MonoDisplayName, Bits);
+    }
+}
