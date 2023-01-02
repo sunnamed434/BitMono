226700c8aec68f5aff21d201eaa3af2f73df9756
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2023-01-02 22:14:54 +0200

    Update ProtectionContextCreator.cs

--- src/BitMono.Obfuscation/ProtectionContextCreator.cs
+++ src/BitMono.Obfuscation/ProtectionContextCreator.cs
@@ -2,30 +2,22 @@
 
 public class ProtectionContextCreator
 {
-    private readonly ModuleCreationResult m_ModuleCreationResult;
-    private readonly ModuleDefinition m_RuntimeModuleDefinition;
-    private readonly BitMonoContext m_Context;
-
-    public ProtectionContextCreator(
-        ModuleCreationResult moduleCreationResult, 
-        ModuleDefinition runtimeModuleDefinition, 
-        BitMonoContext context)
-    {
-        m_ModuleCreationResult = moduleCreationResult;
-        m_RuntimeModuleDefinition = runtimeModuleDefinition;
-        m_Context = context;
-    }
+    public ModuleCreationResult ModuleCreationResult { get; set; }
+    public ModuleDefinition RuntimeModuleDefinition { get; set; }
+    public BitMonoContext BitMonoContext { get; set; }
+    public CancellationToken CancellationToken { get; set; }
 
     public ProtectionContext Create()
     {
         return new ProtectionContext
         {
-            Module = m_ModuleCreationResult.Module,
-            RuntimeModule = m_RuntimeModuleDefinition,
-            ModuleReaderParameters = m_ModuleCreationResult.ModuleReaderParameters,
-            PEImageBuilder = m_ModuleCreationResult.PEImageBuilder,
-            RuntimeImporter = new ReferenceImporter(m_RuntimeModuleDefinition),
-            BitMonoContext = m_Context,
+            Module = ModuleCreationResult.Module,
+            RuntimeModule = RuntimeModuleDefinition,
+            ModuleReaderParameters = ModuleCreationResult.ModuleReaderParameters,
+            PEImageBuilder = ModuleCreationResult.PEImageBuilder,
+            RuntimeImporter = new ReferenceImporter(RuntimeModuleDefinition),
+            BitMonoContext = BitMonoContext,
+            CancellationToken = CancellationToken
         };
     }
 }
