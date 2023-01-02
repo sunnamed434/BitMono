e4917b25690c67d5e61905f02d5ad6b61f59a2a9
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2023-01-02 22:13:09 +0200

    Create IInvokablePipeline.cs

---
+++ src/BitMono.API/Protecting/Pipeline/IInvokablePipeline.cs
@@ -0,0 +1,10 @@
+﻿namespace BitMono.API.Protecting.Pipeline;
+
+public interface IInvokablePipeline
+{
+    bool Succeed { get; }
+    ProtectionContext Context { get; }
+    Action OnFail { get; set; }
+
+    Task InvokeAsync(Func<ProtectionContext, Task<bool>> func);
+}
