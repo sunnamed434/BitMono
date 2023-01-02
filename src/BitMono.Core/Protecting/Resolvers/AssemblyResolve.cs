fe5ece0396684b5c0fa30b6a641ec2c887a500e2
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2023-01-02 22:17:55 +0200

    Create AssemblyResolve.cs

---
+++ src/BitMono.Core/Protecting/Resolvers/AssemblyResolve.cs
@@ -0,0 +1,8 @@
+﻿namespace BitMono.Core.Protecting.Resolvers;
+
+public class AssemblyResolve
+{
+    public HashSet<AssemblyReference> ResolvedReferences { get; set; }
+    public HashSet<AssemblyReference> FailedToResolveReferences { get; set; }
+    public bool Succeed { get; set; }
+}
