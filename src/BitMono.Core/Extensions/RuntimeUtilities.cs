bb1bc658ad7c3b0ad2a36f27093a1be5f3b57e85
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2023-01-02 22:13:48 +0200

    Create RuntimeUtilities.cs

---
+++ src/BitMono.Core/Extensions/RuntimeUtilities.cs
@@ -0,0 +1,37 @@
+﻿namespace BitMono.Core.Extensions;
+
+public static class RuntimeUtilities
+{
+    private static RuntimeInformation _lastRuntimeInformation;
+
+    public static RuntimeInformation GetFrameworkInformation()
+    {
+        if (_lastRuntimeInformation != null)
+        {
+            return _lastRuntimeInformation;
+        }
+        var dotnetFrameworkVersion = Environment.Version;
+        var operatingSystem = Environment.OSVersion;
+        var bits = IntPtr.Size * 8;
+        var monoType = Type.GetType(KnownMonoRuntimes.TypeName);
+        var hasMono = monoType != null;
+        string monoDisplayName = null;
+        if (hasMono)
+        {
+            var displayName = monoType.GetMethod(KnownMonoRuntimes.GetDisplayName, BindingFlags.NonPublic | BindingFlags.Static);
+            if (displayName != null)
+            {
+                monoDisplayName = displayName.ToString();
+            }
+        }
+        return _lastRuntimeInformation = new RuntimeInformation
+        {
+            OperatingSystem = operatingSystem,
+            DotnetFrameworkVersion = dotnetFrameworkVersion,
+            Bits = bits,
+            HasMono = hasMono, 
+            MonoType = monoType,
+            MonoDisplayName = monoDisplayName
+        };
+    }
+}
