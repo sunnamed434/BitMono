a0ff601efe21f975a4e99860470b027cc5ca807f
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2023-01-02 22:19:09 +0200

    Create AntiDecompiler.cs

---
+++ src/BitMono.Protections/AntiDecompiler.cs
@@ -0,0 +1,24 @@
+﻿namespace BitMono.Protections;
+
+public class AntiDecompiler : IHasPipeline
+{
+    public IEnumerable<IPhaseProtection> PopulatePipeline()
+    {
+        yield return new AntiDnSpyAnalyzer();
+    }
+}
+[ProtectionName(nameof(AntiDnSpyAnalyzer))]
+public class AntiDnSpyAnalyzer : IPhaseProtection
+{
+    public Task ExecuteAsync(ProtectionContext context, ProtectionParameters parameters, CancellationToken cancellationToken = default)
+    {
+        foreach (var type in parameters.Targets.OfType<TypeDefinition>())
+        {
+            if (type.IsNested)
+            {
+                type.Attributes = TypeAttributes.Sealed | TypeAttributes.ExplicitLayout;
+            }
+        }
+        return Task.CompletedTask;
+    }
+}
