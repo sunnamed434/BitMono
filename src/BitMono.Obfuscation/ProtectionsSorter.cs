f9e47871d7517f610ec87295dc50872240a5e35f
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2023-01-03 21:10:11 +0200

    Update ProtectionsSorter.cs

--- src/BitMono.Obfuscation/ProtectionsSorter.cs
+++ src/BitMono.Obfuscation/ProtectionsSorter.cs
@@ -1,50 +1,40 @@
-﻿using BitMono.Core.Extensions;
-
-namespace BitMono.Obfuscation;
+﻿namespace BitMono.Obfuscation;
 
 public class ProtectionsSorter
 {
     private readonly ObfuscationAttributeResolver m_ObfuscationAttributeResolver;
     private readonly AssemblyDefinition m_Assembly;
-    private readonly ILogger m_Logger;
 
-    public ProtectionsSorter(
-        ObfuscationAttributeResolver obfuscationAttributeResolver,
-        AssemblyDefinition assembly,
-        ILogger logger)
+    public ProtectionsSorter(ObfuscationAttributeResolver obfuscationAttributeResolver, AssemblyDefinition assembly)
     {
         m_ObfuscationAttributeResolver = obfuscationAttributeResolver;
         m_Assembly = assembly;
-        m_Logger = logger.ForContext<ProtectionsSorter>();
     }
 
     public ProtectionsSort Sort(List<IProtection> protections, IEnumerable<ProtectionSettings> protectionSettings)
     {
-        var protectionsResolve = new ProtectionsResolver(protections, protectionSettings, m_Logger).Sort();
+        var protectionsResolve = new ProtectionsResolver(protections, protectionSettings).Sort();
         var obfuscationAttributeProtections = protectionsResolve.FoundProtections.Where(p => m_ObfuscationAttributeResolver.Resolve(p.GetName(), m_Assembly) == true);
         var deprecatedProtections = protectionsResolve.FoundProtections.Where(p => p.GetType().GetCustomAttribute<ObsoleteAttribute>(false) != null);
-        var sortedProtections = protectionsResolve.FoundProtections
-            .Except(obfuscationAttributeProtections)
+        var sortedProtections = protectionsResolve.FoundProtections.Except(obfuscationAttributeProtections)
             .Except(deprecatedProtections);
 
+        var pipelineProtections = sortedProtections.Where(p => p is IPipelineProtection)
+            .Cast<IPipelineProtection>();
         var packers = sortedProtections.Where(p => p is IPacker)
             .Cast<IPacker>();
-        var pipelineProtections = sortedProtections.Where(p => p is IHasPipeline)
-            .Cast<IHasPipeline>();
 
-        sortedProtections = sortedProtections
-            .Except(packers);
+        sortedProtections = sortedProtections.Except(packers).Except(pipelineProtections);
 
         var hasProtections = sortedProtections.Any();
         return new ProtectionsSort
         {
-            FoundProtections = protectionsResolve.FoundProtections,
+            ProtectionsResolve = protectionsResolve,
             SortedProtections = sortedProtections,
-            Packers = packers,
-            DeprecatedProtections = deprecatedProtections,
-            DisabledProtections = protectionsResolve.DisabledProtections,
             Pipelines = pipelineProtections,
+            Packers = packers,
             ObfuscationAttributeExcludeProtections = obfuscationAttributeProtections,
+            DeprecatedProtections = deprecatedProtections,
             HasProtections = hasProtections
         };
     }
