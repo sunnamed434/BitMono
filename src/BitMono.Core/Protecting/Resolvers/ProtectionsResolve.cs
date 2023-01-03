fd4a42154bdf34fbe234b1a5bd428ccea8e67626
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2023-01-03 21:10:32 +0200

    Update ProtectionsResolve.cs

--- src/BitMono.Core/Protecting/Resolvers/ProtectionsResolve.cs
+++ src/BitMono.Core/Protecting/Resolvers/ProtectionsResolve.cs
@@ -4,4 +4,5 @@ public class ProtectionsResolve
 {
     public List<IProtection> FoundProtections { get; set; }
     public List<string> DisabledProtections { get; set; }
+    public List<string> UnknownProtections { get; set; }
 }
