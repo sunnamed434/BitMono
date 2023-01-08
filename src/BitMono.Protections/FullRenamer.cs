a5e17bd9e43ff612e87a8101b5565121df73a720
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2023-01-08 16:20:56 +0200

    Update FullRenamer.cs

--- src/BitMono.Protections/FullRenamer.cs
+++ src/BitMono.Protections/FullRenamer.cs
@@ -13,23 +13,23 @@ public class FullRenamer : IProtection
     public Task ExecuteAsync(ProtectionContext context, ProtectionParameters parameters)
     {
         var moduleType = context.Module.GetOrCreateModuleType();
-        foreach (var type in parameters.Targets.OfType<TypeDefinition>())
+        foreach (var type in parameters.Members.OfType<TypeDefinition>())
         {
             if (type != moduleType)
             {
                 m_Renamer.Rename(type);
-                foreach (var field in type.Fields.ToArray())
+                foreach (var field in type.Fields)
                 {
                     m_Renamer.Rename(field);
                 }
-                foreach (var method in type.Methods.ToArray())
+                foreach (var method in type.Methods)
                 {
                     if (method.IsConstructor == false && method.IsVirtual == false)
                     {
                         m_Renamer.Rename(method);
                         if (method.HasParameters())
                         {
-                            foreach (var parameter in method.Parameters.ToArray())
+                            foreach (var parameter in method.Parameters)
                             {
                                 m_Renamer.Rename(parameter.Definition);
                             }
