3cc9546f32276a2240ef503de85135348bec1497
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2023-01-08 16:19:40 +0200

    Add RuntimeImplementations class

---
+++ src/BitMono.Core/Protecting/RuntimeImplementations.cs
@@ -0,0 +1,17 @@
+﻿namespace BitMono.Core.Protecting;
+
+public class RuntimeImplementations
+{
+    public RuntimeImplementations(IRenamer renamer, MscorlibInjector mscorlibInjector, CustomInjector customInjector)
+    {
+        Renamer = renamer;
+        MscorlibInjector = mscorlibInjector;
+        CustomInjector = customInjector;
+        Random = new Random();
+    }
+
+    public IRenamer Renamer { get; }
+    public MscorlibInjector MscorlibInjector { get; }
+    public CustomInjector CustomInjector { get; }
+    public Random Random { get; }
+}
