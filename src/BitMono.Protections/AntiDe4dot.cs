baa18586bc457550e844cf1c9245ae8a47b04516
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2023-01-08 16:21:21 +0200

    Fix AntiDe4dot Protection

--- src/BitMono.Protections/AntiDe4dot.cs
+++ src/BitMono.Protections/AntiDe4dot.cs
@@ -2,31 +2,31 @@
 
 public class AntiDe4dot : IProtection
 {
-    private readonly IInjector m_Injector;
+    private readonly CustomInjector m_Injector;
 
-    public AntiDe4dot(IInjector injector)
+    public AntiDe4dot(CustomInjector injector)
     {
         m_Injector = injector;
     }
 
     public Task ExecuteAsync(ProtectionContext context, ProtectionParameters parameters)
     {
-        m_Injector.InjectAttributeWithContent(context.Module, "SmartAssembly.Attributes", "PoweredBy", string.Empty);
-        m_Injector.InjectAttributeWithContent(context.Module, "Xenocode.Client.Attributes.AssemblyAttributes", "PoweredBy", string.Empty);
-        m_Injector.InjectAttributeWithContent(context.Module, string.Empty, "ObfuscatedByGoliath", string.Empty);
-        m_Injector.InjectAttributeWithContent(context.Module, "SecureTeam.Attributes", "ObfuscatedByAgileDotNet", string.Empty);
-        m_Injector.InjectAttributeWithContent(context.Module, string.Empty, "TrinityObfuscator", string.Empty);
-        m_Injector.InjectAttributeWithContent(context.Module, "SecureTeam.Attributes", "ObfuscatedByCliSecure", string.Empty);
-        m_Injector.InjectAttributeWithContent(context.Module, string.Empty, "ZYXDNGuarder", string.Empty);
-        m_Injector.InjectAttributeWithContent(context.Module, string.Empty, "BabelObfuscator", string.Empty);
-        m_Injector.InjectAttributeWithContent(context.Module, string.Empty, "BabelObfuscator", string.Empty);
-        m_Injector.InjectAttributeWithContent(context.Module, string.Empty, "Dotfuscator", string.Empty);
-        m_Injector.InjectAttributeWithContent(context.Module, string.Empty, "Centos", string.Empty);
-        m_Injector.InjectAttributeWithContent(context.Module, string.Empty, "ConfusedBy", string.Empty);
-        m_Injector.InjectAttributeWithContent(context.Module, "NineRays.Obfuscator", "Evaluation", string.Empty);
-        m_Injector.InjectAttributeWithContent(context.Module, "CryptoObfuscator", "ProtectedWithCryptoObfuscator", string.Empty);
-        m_Injector.InjectAttributeWithContent(context.Module, string.Empty, "();\u0009", string.Empty);
-        m_Injector.InjectAttributeWithContent(context.Module, string.Empty, "EMyPID_8234_", string.Empty);
+        m_Injector.InjectAttribute(context.Module, "SmartAssembly.Attributes", "PoweredBy", string.Empty);
+        m_Injector.InjectAttribute(context.Module, "Xenocode.Client.Attributes.AssemblyAttributes", "PoweredBy", string.Empty);
+        m_Injector.InjectAttribute(context.Module, string.Empty, "ObfuscatedByGoliath", string.Empty);
+        m_Injector.InjectAttribute(context.Module, "SecureTeam.Attributes", "ObfuscatedByAgileDotNet", string.Empty);
+        m_Injector.InjectAttribute(context.Module, string.Empty, "TrinityObfuscator", string.Empty);
+        m_Injector.InjectAttribute(context.Module, "SecureTeam.Attributes", "ObfuscatedByCliSecure", string.Empty);
+        m_Injector.InjectAttribute(context.Module, string.Empty, "ZYXDNGuarder", string.Empty);
+        m_Injector.InjectAttribute(context.Module, string.Empty, "BabelObfuscator", string.Empty);
+        m_Injector.InjectAttribute(context.Module, string.Empty, "BabelObfuscator", string.Empty);
+        m_Injector.InjectAttribute(context.Module, string.Empty, "Dotfuscator", string.Empty);
+        m_Injector.InjectAttribute(context.Module, string.Empty, "Centos", string.Empty);
+        m_Injector.InjectAttribute(context.Module, string.Empty, "ConfusedBy", string.Empty);
+        m_Injector.InjectAttribute(context.Module, "NineRays.Obfuscator", "Evaluation", string.Empty);
+        m_Injector.InjectAttribute(context.Module, "CryptoObfuscator", "ProtectedWithCryptoObfuscator", string.Empty);
+        m_Injector.InjectAttribute(context.Module, string.Empty, "();\u0009", string.Empty);
+        m_Injector.InjectAttribute(context.Module, string.Empty, "EMyPID_8234_", string.Empty);
         return Task.CompletedTask;
     }
 }
