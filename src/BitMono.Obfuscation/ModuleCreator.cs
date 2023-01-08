9e7e423c8b73305c59f370633764dd397261c056
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2023-01-08 16:21:26 +0200

    Update ModuleCreator.cs

--- src/BitMono.Obfuscation/ModuleCreator.cs
+++ src/BitMono.Obfuscation/ModuleCreator.cs
@@ -1,30 +1,33 @@
 ﻿public class ModuleCreator : IModuleCreator
 {
     private readonly byte[] m_Bytes;
+    private readonly IErrorListener m_ErrorListener;
 
-    public ModuleCreator(byte[] bytes)
+    public ModuleCreator(byte[] bytes, IErrorListener errorListener)
     {
         m_Bytes = bytes;
+        m_ErrorListener = errorListener;
     }
 
     public ModuleCreationResult Create()
     {
-        var moduleReaderParameters = new ModuleReaderParameters(EmptyErrorListener.Instance);
+        var moduleReaderParameters = new ModuleReaderParameters(m_ErrorListener);
         var module = SerializedModuleDefinition.FromBytes(m_Bytes, moduleReaderParameters);
         var managedPEImageBuilder = new ManagedPEImageBuilder(MetadataBuilderFlags.PreserveAll);
 
-        var isx86 = module.MachineType == MachineType.I386; 
-        if (isx86)
+        module.Attributes &= ~DotNetDirectoryFlags.ILOnly;
+
+        var hasX86 = module.MachineType == MachineType.I386; 
+        if (hasX86)
         {
             module.PEKind = OptionalHeaderMagic.PE32;
             module.MachineType = MachineType.I386;
-            module.IsBit32Required = true;
+            module.Attributes |= DotNetDirectoryFlags.Bit32Required;
         }
         else
         {
             module.PEKind = OptionalHeaderMagic.PE32Plus;
             module.MachineType = MachineType.Amd64;
-            module.IsBit32Required = false;
         }
 
         return new ModuleCreationResult
