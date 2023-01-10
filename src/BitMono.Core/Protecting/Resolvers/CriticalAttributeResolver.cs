a7cad2976e12183f5ff2c5e3628469b183089ca4
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2023-01-10 18:38:48 +0200

    Create CriticalAttributeResolver.cs

---
+++ src/BitMono.Core/Protecting/Resolvers/CriticalAttributeResolver.cs
@@ -0,0 +1,30 @@
+﻿namespace BitMono.Core.Protecting.Resolvers;
+
+public class CriticalAttributeResolver : AttributeResolver
+{
+    private readonly IConfiguration m_Configuration;
+    private readonly AttemptAttributeResolver m_AttemptAttributeResolver;
+
+    public CriticalAttributeResolver(IBitMonoCriticalsConfiguration configuration, AttemptAttributeResolver attemptAttributeResolver)
+    {
+        m_Configuration = configuration.Configuration;
+        m_AttemptAttributeResolver = attemptAttributeResolver;
+    }
+
+    public override bool Resolve([AllowNull] string feature, IHasCustomAttribute from, [AllowNull] out CustomAttributeResolve attributeResolve)
+    {
+        attributeResolve = null;
+        if (m_Configuration.GetValue<bool>("UseCriticalAttributes") == false)
+        {
+            return false;
+        }
+        foreach (var criticalAttribute in m_Configuration.GetCriticalAttributes())
+        {
+            if (m_AttemptAttributeResolver.TryResolve(from, criticalAttribute.Namespace, criticalAttribute.Name, out attributeResolve))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
