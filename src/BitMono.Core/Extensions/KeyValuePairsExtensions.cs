b1c1a7e2dfa1794629d0566201b86892722b9711
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2023-01-10 18:38:38 +0200

    Create KeyValuePairsExtensions.cs

---
+++ src/BitMono.Core/Extensions/KeyValuePairsExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace BitMono.Core.Extensions;
+
+public static class KeyValuePairsExtensions
+{
+    public static bool TryGetValueOrDefault(this Dictionary<string, object> source, string key, bool defaultValue = false)
+    {
+        var value = defaultValue;
+        if (source.TryGetValue(key, out var valueValue))
+        {
+            if (valueValue is bool resolveValue)
+            {
+                value = resolveValue;
+            }
+        }
+        return value;
+    }
+}
