3da24d29ef3088d7973106e6072802b068400ef5
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2023-01-10 12:27:30 +0200

    Delete ModuleCreationResult.cs

--- src/BitMono.Obfuscation.API/ModuleCreationResult.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BitMono.Obfuscation.API;
-
-public class ModuleCreationResult
-{
-    public ModuleDefinition Module { get; set; }
-    public ModuleReaderParameters ModuleReaderParameters { get; set; }
-    public IPEImageBuilder PEImageBuilder { get; set; }
-}
