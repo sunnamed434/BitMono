c5c50afb2c7955a17fc85a931eff3bf7eb97e416
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2023-01-10 12:28:01 +0200

    Create BitMonoContextFactory.cs

---
+++ src/BitMono.Obfuscation/BitMonoContextFactory.cs
@@ -0,0 +1,24 @@
+﻿namespace BitMono.Obfuscation;
+
+public class BitMonoContextFactory
+{
+    private readonly IDependenciesDataResolver m_DependenciesDataResolver;
+    private readonly IConfiguration m_Configuration;
+
+    public BitMonoContextFactory(IDependenciesDataResolver dependenciesDataResolver, IBitMonoObfuscationConfiguration configuration)
+    {
+        m_DependenciesDataResolver = dependenciesDataResolver;
+        m_Configuration = configuration.Configuration;
+    }
+
+    public BitMonoContext Create(string outputDirectoryName, string fileName)
+    {
+        return new BitMonoContext
+        {
+            OutputDirectoryName = outputDirectoryName,
+            DependenciesData = m_DependenciesDataResolver.Resolve(),
+            Watermark = m_Configuration.GetValue<bool>(nameof(Shared.Models.Obfuscation.Watermark)),
+            FileName = fileName,
+        };
+    }
+}
