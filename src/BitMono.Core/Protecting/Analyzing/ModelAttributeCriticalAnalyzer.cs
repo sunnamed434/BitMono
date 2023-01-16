9f4f40f42843938a2e64d38ad1284ecc8a7648f8
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2023-01-16 20:24:35 +0200

    Remove no need exception

--- src/BitMono.Core/Protecting/Analyzing/ModelAttributeCriticalAnalyzer.cs
+++ src/BitMono.Core/Protecting/Analyzing/ModelAttributeCriticalAnalyzer.cs
@@ -24,8 +24,6 @@ public class ModelAttributeCriticalAnalyzer : ICriticalAnalyzer<IHasCustomAttrib
                 return false;
             }
         }
-
-        throw new ArgumentException("AAAAAA");
         return true;
     }
 }
