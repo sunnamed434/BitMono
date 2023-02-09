a72c5ff967eabde099a26314ad7dedf9b6b3cb35
0x59R11 <41921371+0x59R11@users.noreply.github.com>
2023-02-09 21:29:17 +0200

    Add protection

---
+++ src/BitMono.Protections/MethodArgsToArglist.cs
@@ -0,0 +1,11 @@
+using System.Reflection.Emit;
+
+namespace BitMono.Protections;
+
+public class MethodArgsToArglist : IProtection
+{
+    public Task ExecuteAsync(ProtectionContext context, ProtectionParameters parameters)
+    {
+        
+    }
+}
