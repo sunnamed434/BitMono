5932ee7746455707800e51f529e0ba5f58676b38
sunnamed434 <cetnikletsplay@gmail.com>
2023-07-19 22:05:21 +0300

    Fix bug when bitmono "crashes" on launch

--- src/BitMono.Shared/Models/CriticalsSettings.cs
+++ src/BitMono.Shared/Models/CriticalsSettings.cs
@@ -1,5 +1,6 @@
 namespace BitMono.Shared.Models;
 
+[NullGuard(ValidationFlags.NonPublic)]
 public class CriticalsSettings
 {
     public bool UseCriticalAttributes { get; set; }
@@ -8,12 +9,12 @@ public class CriticalsSettings
     public bool UseCriticalBaseTypes { get; set; }
     public bool UseCriticalMethodsStartsWith { get; set; }
     public bool UseCriticalMethods { get; set; }
-#pragma warning disable CS8618
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     public List<CriticalAttribute> CriticalAttributes { get; set; }
     public List<CriticalAttribute> CriticalModelAttributes { get; set; }
     public List<string> CriticalInterfaces { get; set; }
     public List<string> CriticalBaseTypes { get; set; }
     public List<string> CriticalMethodsStartsWith { get; set; }
     public List<string> CriticalMethods { get; set; }
-#pragma warning restore CS8618
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 }
