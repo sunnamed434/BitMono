c656d04b080aae9f66d4b83fd00088f31514b76d
sunnamed434 <cetnikletsplay@gmail.com>
2023-10-21 20:32:10 +0300

    Add more info for RuntimeMonikerMonoAttribute

--- src/BitMono.Core/Attributes/RuntimeMonikerMonoAttribute.cs
+++ src/BitMono.Core/Attributes/RuntimeMonikerMonoAttribute.cs
@@ -1,5 +1,10 @@
 namespace BitMono.Core.Attributes;
 
+/// <summary>
+/// Represents a mechanism that specifies the runtime moniker of the protection as a <b>Mono</b>.
+/// <remarks>i.e if you see this attribute on protection then only specified attributes are the supported runtime monikers for the protection.
+/// If you don't see any of the attributes then it works everywhere, also, users will get a message via <see cref="RuntimeMonikerAttribute.GetMessage"/></remarks>
+/// </summary>
 [AttributeUsage(AttributeTargets.Class, Inherited = false)]
 public class RuntimeMonikerMonoAttribute : RuntimeMonikerAttribute
 {
