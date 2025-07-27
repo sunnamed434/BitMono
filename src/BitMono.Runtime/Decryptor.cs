4a43a8a9268f95bed63fda3afdfa2298cac6482b
sunnamed434 <cetnikletsplay@gmail.com>
2025-07-27 16:48:45 +0300

    Suppress Rijndael deprecation warnings

--- src/BitMono.Runtime/Decryptor.cs
+++ src/BitMono.Runtime/Decryptor.cs
@@ -7,11 +7,15 @@ public struct Decryptor
         byte[]? decryptedBytes = null;
         using (var memoryStream = new MemoryStream())
         {
+#pragma warning disable SYSLIB0022
             using (var aes = new RijndaelManaged())
+#pragma warning restore SYSLIB0022
             {
                 aes.KeySize = 256;
                 aes.BlockSize = 128;
+#pragma warning disable SYSLIB0041
                 var key = new Rfc2898DeriveBytes(cryptKeyBytes, saltBytes, 1000);
+#pragma warning restore SYSLIB0041
                 aes.Key = key.GetBytes(aes.KeySize / 8);
                 aes.IV = key.GetBytes(aes.BlockSize / 8);
                 aes.Mode = CipherMode.CBC;
--- src/BitMono.Runtime/Encryptor.cs
+++ src/BitMono.Runtime/Encryptor.cs
@@ -8,11 +8,15 @@ public struct Encryptor
         byte[]? encryptedBytes = null;
         using (var memoryStream = new MemoryStream())
         {
+#pragma warning disable SYSLIB0022
             using (var aes = new RijndaelManaged())
+#pragma warning restore SYSLIB0022
             {
                 aes.KeySize = 256;
                 aes.BlockSize = 128;
+#pragma warning disable SYSLIB0041
                 var key = new Rfc2898DeriveBytes(saltBytes, cryptKeyBytes, 1000);
+#pragma warning restore SYSLIB0041
                 aes.Key = key.GetBytes(aes.KeySize / 8);
                 aes.IV = key.GetBytes(aes.BlockSize / 8);
                 aes.Mode = CipherMode.CBC;
